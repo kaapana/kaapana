b8a16e0f0a4d0cab4e1153a9d85d4cc16d29b10d
Klaus Kades <k.kades@dkfz.de>
2020-10-01 12:55:51 +0200

    added clean up dag that once triggered cleans up everything older than 60 days

---
+++ workflows/airflow-components/dags/dag_clean_up_expired_workflow_data.py
@@ -0,0 +1,37 @@
+from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.utils.dates import days_ago
+from airflow.utils.trigger_rule import TriggerRule
+
+from datetime import timedelta
+
+from airflow.models import DAG
+
+from kaapana.operators.LocalCleanUpExpiredWorkflowDataOperator import LocalCleanUpExpiredWorkflowDataOperator
+
+from datetime import timedelta
+from datetime import datetime
+
+import os
+
+log = LoggingMixin().log
+
+dag_info = {
+    "visible": True,
+}
+
+args = {
+    'owner': 'airflow',
+    'start_date': days_ago(0),
+    'retries': 0,
+    'dag_info': dag_info,
+    'retry_delay': timedelta(seconds=30)
+}
+
+dag = DAG(
+    dag_id='clean-up-expired-workflow-data',
+    default_args=args,
+    schedule_interval=None)
+
+clean_up = LocalCleanUpExpiredWorkflowDataOperator(dag=dag, expired_period=timedelta(days=30))
+
+clean_up

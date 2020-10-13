6cdd05086a8228d5b193db3697cd93272f7cdbab
Klaus Kades <k.kades@dkfz.de>
2020-10-13 11:03:07 +0200

    uncommented testing

--- workflows/docker-container/dcmqi/files/convert.sh
+++ workflows/docker-container/dcmqi/files/convert.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-tail -f /dev/null
-# case "$DCMQI_COMMAND" in
-#     'itkimage2segimage')
-#         echo "COMMAND: $DCMQI_COMMAND"
-#         python3 -u itkimage2segimage.py
-#     ;;
-#     'segimage2itkimage')
-#         echo "COMMAND: $DCMQI_COMMAND"
-#         python3 -u segimage2itkimage.py
-#     ;;
-#     'tid1500writer')
-#         echo "COMMAND: $DCMQI_COMMAND"
-#         python3 -u tid1500writer.py
-#     ;;
-#     *)
-#         echo $"Usage: $0 {itkimage2segimage|segimage2itkimage|tid1500writer}"
-#         exit 1
-# esac
+case "$DCMQI_COMMAND" in
+    'itkimage2segimage')
+        echo "COMMAND: $DCMQI_COMMAND"
+        python3 -u itkimage2segimage.py
+    ;;
+    'segimage2itkimage')
+        echo "COMMAND: $DCMQI_COMMAND"
+        python3 -u segimage2itkimage.py
+    ;;
+    'tid1500writer')
+        echo "COMMAND: $DCMQI_COMMAND"
+        python3 -u tid1500writer.py
+    ;;
+    *)
+        echo $"Usage: $0 {itkimage2segimage|segimage2itkimage|tid1500writer}"
+        exit 1
+esac

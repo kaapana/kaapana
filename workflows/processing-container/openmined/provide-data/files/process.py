6422a37d4acfbcffe79f1cd11df62e8750c4f97a
Marius <marius.kempf@dkfz-heidelberg.de>
2021-02-19 19:49:39 +0100

    data providing updated

--- workflows/processing-container/openmined/provide-data/files/process.py
+++ workflows/processing-container/openmined/provide-data/files/process.py
@@ -5,10 +5,12 @@ import syft as sy
 from syft.grid.clients.data_centric_fl_client import DataCentricFLClient
 import torch
 import torchvision
-from torchvision import transforms
 
+from torchvision import transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
+
+from utils.utilities import provide_dataset_transforms
 
 hook = sy.TorchHook(torch)
 
@@ -16,27 +18,20 @@ hook = sy.TorchHook(torch)
 path = os.path.join(os.environ["WORKFLOW_DIR"], os.environ['OPERATOR_IN_DIR'])
 
 # transformations
-transform_dict = {
-    'train': transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-        ]),
-    'test': transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-        ])}
+transform_dict = provide_dataset_transforms(dataset=os.environ['DATASET'])
 
 # Image folder
-dataset_train = ImageFolder(
+dataset = ImageFolder(
     root=os.path.join(path, 'train'),
     transform=transform_dict['train'])
 
 # data loader
-n_samples = len(dataset_train)
-train_dataloader = DataLoader(dataset_train,batch_size=n_samples, shuffle=True, num_workers=0)
-dataiter = iter(train_dataloader)
+n_samples = len(dataset)
+print(f'Total dataset size: {n_samples}')
+dataloader = DataLoader(dataset, batch_size=n_samples, shuffle=True, num_workers=0)
 
 # get images & targets
+dataiter = iter(dataloader)
 images, targets = dataiter.next()
 
 # sending data to node
@@ -44,8 +39,8 @@ node_addr = 'http://{}:{}/'.format(os.environ['NODE_HOST'],os.environ['NODE_PORT
 node = DataCentricFLClient(hook, node_addr)
 
 print(f'Sending data to node {node_addr}')
-imgs_tag = images.tag('#X', '#mnist', '#dataset').describe('MNIST - images')
-targets_tag = targets.tag('#Y', '#mnist', '#dataset').describe('MNIST - targets')
+imgs_tag = images.tag('#X', f"#{os.environ['DATASET']}", '#dataset').describe(f"{os.environ['DATASET']} - images")
+targets_tag = targets.tag('#Y', f"#{os.environ['DATASET']}", '#dataset').describe(f"{os.environ['DATASET']} - targets")
 imgs_ptr, targets_ptr = imgs_tag.send(node), targets_tag.send(node)
 
 # sleeper

7ddc09e12cbab3c962842f2a651ff3b8730f520d
Marius <marius.kempf@dkfz-heidelberg.de>
2021-03-18 18:58:38 +0100

    new structure | waiting for node added

--- workflows/processing-container/openmined/provide-data/files/process.py
+++ workflows/processing-container/openmined/provide-data/files/process.py
@@ -14,43 +14,66 @@ from utils.utilities import provide_dataset_transforms
 
 hook = sy.TorchHook(torch)
 
-# define path
-path = os.path.join(os.environ["WORKFLOW_DIR"], os.environ['OPERATOR_IN_DIR'])
 
-# get dataset specific image transforms
-transform_dict = provide_dataset_transforms(dataset=os.environ['DATASET'])
+class Arguments():
+    def __init__(self):
+        self.host = os.getenv('NODE_HOST')
+        self.port = os.getenv('NODE_PORT', '5000')
+        self.node_addr = 'http://{}:{}/'.format(self.host, self.port)
 
-# Image folder
-dataset = ImageFolder(
-    root=os.path.join(path, 'train'),
-    transform=transform_dict['train'])
-print("Class encoding: {}".format(dataset.class_to_idx))
+        self.exp_tag = os.getenv('EXP_TAG', '#no-tag-given')
+        self.dataset = os.getenv('DATASET', 'mnist')
+        self.lifespan = int(os.getenv('LIFESPAN', '15'))
+        self.data_path = os.path.join(os.environ["WORKFLOW_DIR"], os.environ['OPERATOR_IN_DIR'])
 
-# data loader
-n_samples = len(dataset)
-print(f'Total dataset size: {n_samples}')
-dataloader = DataLoader(dataset, batch_size=n_samples, shuffle=True, num_workers=0)
 
-# get images & targets
-dataiter = iter(dataloader)
-images, targets = dataiter.next()
+def main(args):
 
-# sending data to node
-node_addr = 'http://{}:{}/'.format(os.environ['NODE_HOST'],os.environ['NODE_PORT'])
-node = DataCentricFLClient(hook, node_addr)
+    # get dataset specific image transforms
+    transform_dict = provide_dataset_transforms(dataset=args.dataset)
 
-print(f'Sending data to node {node_addr}')
-exp_tag = os.getenv('EXP_TAG', '#no-tag-given')
+    # Image folder
+    dataset = ImageFolder(
+        root=os.path.join(args.data_path, 'train'),
+        transform=transform_dict['train'])
+    print("Class encoding: {}".format(dataset.class_to_idx))
 
-imgs_tag = images.tag('#X', '#dataset', f"#{os.environ['DATASET']}", exp_tag).describe(
-    f"Images used for experiment {exp_tag} ({os.environ['DATASET']})")
+    # dataloader
+    n_samples = len(dataset)
+    print(f'Total dataset size: {n_samples}')
+    dataloader = DataLoader(
+        dataset,
+        batch_size=n_samples,
+        shuffle=True,
+        num_workers=0)
 
-targets_tag = targets.tag('#Y', '#dataset', f"#{os.environ['DATASET']}", exp_tag).describe(
-    f"Targets used for experiment {exp_tag} ({os.environ['DATASET']})")
+    # get images & targets
+    dataiter = iter(dataloader)
+    images, targets = dataiter.next()
 
-imgs_ptr, targets_ptr = imgs_tag.send(node), targets_tag.send(node)
+    # wait until node is available
+    available = False
+    while(not available):
+        try:
+            node = DataCentricFLClient(hook, args.node_addr)
+            available = True
+        except:
+            time.sleep(5)
+    
+    # send data to node
+    print(f'Sending data to node {args.node_addr}')
+    imgs_tag = images.tag('#X', '#dataset', f"#{args.dataset}", args.exp_tag).describe(
+        f"images used for experiment {args.exp_tag} ({args.dataset})")
+    targets_tag = targets.tag('#Y', '#dataset', f"#{args.dataset}", args.exp_tag).describe(
+        f"targets used for experiment {args.exp_tag} ({args.dataset})")
 
-# sleeper
-minutes = int(os.environ["LIFESPAN"])
-print(f'Providing data for {minutes} minutes ...')
-time.sleep(minutes * 60)
+    imgs_ptr, targets_ptr = imgs_tag.send(node), targets_tag.send(node)
+
+    # sleeper
+    print(f'Providing data for {args.lifespan} minutes ...')
+    time.sleep(args.lifespan * 60)
+
+
+if __name__ == "__main__":
+    args = Arguments()
+    main(args)

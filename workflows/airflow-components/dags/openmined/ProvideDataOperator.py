3478f51343862314195a7fcaef14aff53a80e120
Marius <marius.kempf@dkfz-heidelberg.de>
2021-03-19 15:24:49 +0100

    update data providing operators

--- workflows/airflow-components/dags/openmined/ProvideDataOperator.py
+++ workflows/airflow-components/dags/openmined/ProvideDataOperator.py
@@ -6,17 +6,30 @@ from datetime import timedelta
 from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator, default_registry, default_project
 from kaapana.blueprints.kaapana_global_variables import BATCH_NAME, WORKFLOW_DIR
 
-
 class OpenminedProvideDataOperator(KaapanaBaseOperator):
 
+    @staticmethod
+    def on_success(info_dict):
+        print("##################################################### on_success!")
+        pod_id = info_dict["ti"].task.kube_name
+        print("--> training ended, now delete pod {} !".format(pod_id))
+        KaapanaBaseOperator.pod_stopper.stop_pod_by_name(pod_id=pod_id)
+
+    def execute(self, context):
+        try:
+            KaapanaBaseOperator.execute(self, context)
+        except TypeError:
+            pass
+    
     def __init__(self,
                  dag,
-                 dataset=None,
-                 node_host=None,
-                 node_port=None,
+                 hostname=None,
+                 port=None,
                  lifespan=None,
+                 dataset=None,
+                 exp_tag=None,
                  env_vars=None,
-                 execution_timeout=timedelta(minutes=15),
+                 execution_timeout=timedelta(hours=6),
                  *args, **kwargs
                  ): 
 
@@ -24,9 +37,10 @@ class OpenminedProvideDataOperator(KaapanaBaseOperator):
             env_vars = {}
         
         envs = {
+            "HOSTNAME": str(hostname),
+            "PORT": str(port),
             "DATASET": str(dataset),
-            "NODE_HOST": str(node_host),
-            "NODE_PORT": str(node_port),
+            "EXP_TAG": str(exp_tag),
             "LIFESPAN": str(lifespan)
         }
 
@@ -34,12 +48,13 @@ class OpenminedProvideDataOperator(KaapanaBaseOperator):
 
         super().__init__(
             dag=dag,
-            name="openmined-provide-data",
+            name="data-provider",
             image="{}{}/openmined-provide-data:0.1.0-vdev".format(default_registry, default_project),
             image_pull_secrets=["registry-secret"],
             env_vars=env_vars,
             execution_timeout=execution_timeout,
+            on_success_callback=OpenminedProvideDataOperator.on_success,
             #image_pull_policy='Always',
-            #ram_mem_mb=10000,
+            #ram_mem_mb=5000,
             *args, **kwargs
             )

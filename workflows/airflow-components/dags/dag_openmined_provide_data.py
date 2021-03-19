10de2327df311bb7b9fc2b3dc91dcc51f5802ec4
Marius <marius.kempf@dkfz-heidelberg.de>
2021-03-19 19:16:06 +0100

    new sequence | final structure

--- workflows/airflow-components/dags/dag_openmined_provide_data.py
+++ workflows/airflow-components/dags/dag_openmined_provide_data.py
@@ -9,14 +9,9 @@ from kaapana.operators.LocalMinioOperator import LocalMinioOperator
 from kaapana.operators.LocalUnzipFileOperator import LocalUnzipFileOperator
 from kaapana.operators.LocalWorkflowCleanerOperator import LocalWorkflowCleanerOperator
 
+from openmined.RunNodeOperator import RunNodeOperator
 from openmined.ProvideDataOperator import OpenminedProvideDataOperator
 
-
-DATASET = 'mnist' #'xray'
-NODE_HOST = '10.128.129.6'
-NODE_PORT = '5000'
-FOLDER_DATASPLIT = 'MNIST-split' #'XRAY-split'
-LIFESPAN = '15'
 
 log = LoggingMixin().log
 
@@ -36,9 +31,39 @@ dag = DAG(
     max_active_runs=5
     )
 
-get_object_from_minio = LocalMinioOperator(dag=dag, action='get', bucket_name='openmined-data', action_operator_dirs=[FOLDER_DATASPLIT], operator_out_dir=FOLDER_DATASPLIT)
-unzip_files = LocalUnzipFileOperator(dag=dag, input_operator=get_object_from_minio, operator_in_dir=FOLDER_DATASPLIT)
-data2node = OpenminedProvideDataOperator(dag=dag, input_operator=unzip_files, node_host=NODE_HOST, node_port=NODE_PORT, dataset=DATASET, lifespan=LIFESPAN)
+get_object_from_minio = LocalMinioOperator(
+    dag=dag,
+    action='get',
+    bucket_name='openmined-data',
+    action_operator_dirs=['MNIST-split'],
+    operator_out_dir='MNIST-split'
+    )
+
+unzip_files = LocalUnzipFileOperator(
+    dag=dag,
+    operator_in_dir='MNIST-split'
+    )
+
+run_node = RunNodeOperator(
+    dag=dag, 
+    operator_in_dir='./',
+    release_name='openmined-node',
+    global_id=None,     #i.e. 'co',
+    hostname=None,      #i.e. '10.128.129.6',
+    port='5000',
+    grid_network=None   #i.e. '10.128.129.76:7000'
+    )
+
+data2node = OpenminedProvideDataOperator(
+    dag=dag,
+    input_operator=unzip_files,
+    hostname=None,      #i.e. '10.128.129.6,
+    port='5000',
+    lifespan='15',
+    dataset='mnist',
+    exp_tag="#no-tag-given"
+    )
+
 clean = LocalWorkflowCleanerOperator(dag=dag)
 
-get_object_from_minio >> unzip_files >> data2node >> clean
+get_object_from_minio >> unzip_files >> [run_node, data2node] >> clean

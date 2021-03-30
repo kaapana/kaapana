6449be828ada579073ca0721a8197db6d2c767ed
Marius <marius.kempf@dkfz-heidelberg.de>
2021-03-30 15:06:21 +0200

    added dag to reload workflows model directory from minio

---
+++ workflows/airflow-components/dags/dag_service_reload_models_dir.py
@@ -0,0 +1,42 @@
+from datetime import timedelta
+
+from airflow.models import DAG
+from airflow.utils.dates import days_ago
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.utils.log.logging_mixin import LoggingMixin
+
+from kaapana.operators.LocalMinioOperator import LocalMinioOperator
+from kaapana.operators.LocalWorkflowCleanerOperator import LocalWorkflowCleanerOperator
+
+
+log = LoggingMixin().log
+
+args = {
+    'ui_visible': False,
+    'owner': 'kaapana',
+    'start_date': days_ago(0),
+    'retries': 2,
+    'retry_delay': timedelta(seconds=30),
+}
+
+dag = DAG(
+    dag_id='service-reload-models-dir',
+    default_args=args,
+    schedule_interval=None,
+    concurrency=10,
+    max_active_runs=5
+    )
+
+reload_models_from_minio = LocalMinioOperator(
+    dag=dag,
+    action='get',
+    run_dir='/models',
+    bucket_name='models'
+    )
+
+cleanup = LocalWorkflowCleanerOperator(
+    dag=dag,
+    clean_workflow_dir=True
+    )
+ 
+reload_models_from_minio >> cleanup

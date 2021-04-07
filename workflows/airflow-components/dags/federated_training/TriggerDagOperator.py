e71556b33937d0a9fa03f4cf01b9205c187ba32b
Marius <marius.kempf@dkfz-heidelberg.de>
2021-04-07 14:25:33 +0200

    pretty print of rest-call

--- workflows/airflow-components/dags/federated_training/TriggerDagOperator.py
+++ workflows/airflow-components/dags/federated_training/TriggerDagOperator.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 from kaapana.operators.KaapanaPythonBaseOperator import KaapanaPythonBaseOperator, rest_self_udpate
-
+from kaapana.blueprints.kaapana_global_variables import BATCH_NAME, WORKFLOW_DIR
 
 class TriggerDagOperator(KaapanaPythonBaseOperator):
     
@@ -11,12 +11,12 @@ class TriggerDagOperator(KaapanaPythonBaseOperator):
     def start(self, ds, **kwargs):
 
         if self.dag_host is None:
-            url = 'http://airflow-service.flow.svc:8080/flow/kaapana/api/trigger/{}'.format(self.dag_name)
+            url = f'http://airflow-service.flow.svc:8080/flow/kaapana/api/trigger/{self.dag_name}'
         else:
-            url = 'https://{}/flow/kaapana/api/trigger/{}'.format(self.dag_host, self.dag_name)
+            url = f'https://{self.dag_host}/flow/kaapana/api/trigger/{self.dag_name}'
 
         print('Calling DAG: {}'.format(url))
-        print('Rest call: {}'.format(self.rest_call))
+        print('REST CALL: ', json.dumps(self.rest_call, indent=4))
 
         r = requests.post(url, json=self.rest_call, verify=False)
         print(r.json())

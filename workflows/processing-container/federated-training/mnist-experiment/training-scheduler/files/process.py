ddee0362049ec8cd0f41e4f6e2ab9282d2c141a7
Marius <marius.kempf@dkfz-heidelberg.de>
2021-04-13 13:20:13 +0200

    implemented smarter solution to wait for models & reload them | using Minio Api

--- workflows/processing-container/federated-training/mnist-experiment/training-scheduler/files/process.py
+++ workflows/processing-container/federated-training/mnist-experiment/training-scheduler/files/process.py
@@ -4,6 +4,7 @@ import json
 import shutil
 import requests
 import datetime
+from minio import Minio
 
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -31,12 +32,15 @@ class Arguments():
         self.model_cache = os.path.join(os.environ['WORKFLOW_DIR'], 'cache')
         if not os.path.exists(self.model_cache):
             os.makedirs(self.model_cache)
+
+        self.minio_access_key = os.environ.get('MINIOUSER')
+        self.minio_secret_key = os.environ.get('MINIOPASSWORD')
         
         self.procedure = os.getenv('PROCEDURE', 'avg')
-        self.scheduler = os.getenv('SCHEDULER', '10.128.129.76')
+        self.scheduler = os.getenv('SCHEDULER')
         self.participants = json.loads(os.environ['PARTICIPANTS'])
         self.apply_tests = True
-        self.checking_interval = 0.25 # minutes
+        self.check_minio_interval = 0.25 # minutes
         
         self.epochs = 1
         self.lr = 0.1
@@ -73,7 +77,56 @@ def test(model, dataloader_test, device):
         100. * correct / len(dataloader_test.dataset)))
 
 
-def apply_minio_action(action='put', run_dir='/data', bucket_name='federated-training', action_operator_dirs=['cache'], wait=True):
+def check_minio_and_load_models(participants, args, minio_host='minio-service.store.svc', minio_port='9000', session_token=None):
+    """Checks if remotely trained model(s) arrived in Minio - if yes, starts dag to lead them to Airflow"""
+
+    minioClient = Minio(
+        minio_host + ":" + minio_port,
+        access_key=args.minio_access_key,
+        secret_key=args.minio_secret_key,
+        session_token=session_token,
+        secure=False
+    )
+    bucket_name = 'federated-exp-mnist'
+    folder = 'cache/'
+    response = minioClient.list_objects(bucket_name, prefix=folder)
+    content = [element.object_name for element in response]
+    
+    model_list = [f'cache/model_checkpoint_from_{participant}.pt' for participant in participants]
+    available = [model_file_path in content for model_file_path in model_list]
+    
+    if not all(available):
+        return False
+    else:
+        # trigger minio service dag
+        print('Triggering Minio service dag to load model(s) to Airflow directory')
+        apply_local_minio_action(action='get', run_dir='/data/{}'.format(args.run_id), action_operator_dirs=['cache'], wait=False)
+        
+        # wait until model(s) are loaded
+        while not all([os.path.isfile(os.path.join(args.workflow_dir, model_file_path)) for model_file_path in model_list]):
+            time.sleep(10) # seconds
+        
+        print('Recieved model(s) from particpant(s) ({})'.format(participants))
+        return True
+
+
+def model_to_minio(model, optimizer, action_operator_dirs=['model']):
+    """Saves model with optimizer and puts them to Minio"""
+    if not os.path.isdir(args.model_dir):
+        os.mkdir(args.model_dir)    
+    
+    checkpoint = {
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict()
+    }
+
+    torch.save(checkpoint, os.path.join(args.model_dir, 'model_checkpoint.pt'))
+
+    run_dir = '/data/{}'.format(args.run_id)
+    apply_local_minio_action(action='put', run_dir=run_dir, action_operator_dirs=action_operator_dirs, wait=True)
+
+
+def apply_local_minio_action(action='put', run_dir='/data', bucket_name='federated-exp-mnist', action_operator_dirs=['cache'], wait=True):
     """Calls service dag to apply action Minio/Airflow /data/model"""
     rest_call = {
         'rest_call': {
@@ -94,38 +147,10 @@ def apply_minio_action(action='put', run_dir='/data', bucket_name='federated-tra
         time.sleep(60*0.5)
 
 
-def model_to_minio(model, optimizer, action_operator_dirs=['model']):
-    """Saves model with optimizer and puts them to Minio"""
-    if not os.path.isdir(args.model_dir):
-        os.mkdir(args.model_dir)    
-    
-    checkpoint = {
-        'model': model.state_dict(),
-        'optimizer': optimizer.state_dict()
-    }
-
-    torch.save(checkpoint, os.path.join(args.model_dir, 'model_checkpoint.pt'))
-
-    run_dir = '/data/{}'.format(args.run_id)
-    apply_minio_action(action='put', run_dir=run_dir, action_operator_dirs=action_operator_dirs, wait=True)
-
-
 def trigger_training_dag(participant, args):
     """Triggers Dag on training participant to train on its data"""
 
     dag_name = 'federated-training-mnist'
-    
-    # api call given to operator in remote dag
-    recall = {
-        'rest_call': {
-            'global': {},
-            'operators': {
-                'minio-actions-get': {
-                    'action': 'get',
-                    'run_dir': '/data/{}'.format(args.run_id),
-                    'bucket_name': 'federated-exp-mnist',
-                    'action_operator_dirs': ['cache']
-                }}}}
     
     # actual call to trigger dag
     rest_call = {
@@ -135,10 +160,7 @@ def trigger_training_dag(participant, args):
                 'minio_host': args.scheduler},
             'operators': {
                 'model-training': {
-                    'host_ip': participant},
-                'trigger-dag': {
-                    'dag_host': args.scheduler,
-                    'rest_call': recall }}}}
+                    'host_ip': participant}}}}
 
     url = 'https://{}/flow/kaapana/api/trigger/{}'.format(participant, dag_name)
     r = requests.post(url=url, json=rest_call, verify=False)
@@ -151,6 +173,60 @@ def average_model_state_dicts(state_dicts):
     for key in state_dicts[0]:
         model_sd_avg[key] = sum([state_dict[key] for state_dict in state_dicts]) / len(state_dicts)
     return model_sd_avg
+
+
+def averaging_training(args):
+    """Logic for federated averaging"""
+
+    # prepare dataloader if central testing is wanted
+    if args.apply_tests:
+        dataloader_test = prepare_dataloader_test(args)
+
+    print('#'*10, 'TRAINING START - MODEL AVERAGING', '#'*10)
+    print('#'*10, 'Using following participants: {}'.format(args.participants))
+
+    for epoch in range(0, args.epochs):
+        print('#'*10, 'EPOCH {}'.format(epoch))
+        
+        # Clear airflow directories
+        shutil.rmtree(args.model_dir)
+        shutil.rmtree(args.model_cache)
+        
+        # trigger remote dags on participants
+        print('#'*10, 'Distribute model to participants: {}'.format(args.participants))
+        for participant in args.participants:
+            trigger_training_dag(participant, args)
+        
+        # wait until all models were send back to scheduler        
+        print('Keep waiting for models...')
+        while not check_minio_and_load_models(args.participants, args):
+            time.sleep(60*args.check_minio_interval)
+
+        # load state dicts & create new avg model
+        model_file_list = [os.path.join(args.model_cache, f'model_checkpoint_from_{participant}.pt') for participant in args.participants]
+        model_state_dicts = [torch.load(model)['model'] for model in model_file_list]
+        sd_avg = average_model_state_dicts(model_state_dicts)
+        
+        model_avg = ClassifierMNIST()
+        model_avg.load_state_dict(sd_avg)
+
+        optimizer = torch.optim.SGD(model_avg.parameters(), lr=args.lr)
+        optimizer.load_state_dict(
+            torch.load(model_file_list[0])['optimizer']
+        )
+
+        # test model on locally available test data
+        if args.apply_tests:
+            device = 'cpu'
+            print('Testing averaged model at current state using device: {}'.format(device))
+            test(model_avg, dataloader_test, device)
+        
+        # save new 'model_checkpoint.pt' to /model and put to local Minio
+        model_to_minio(model_avg, optimizer)
+
+        # clear local Minio model cache
+        apply_local_minio_action(action='remove', run_dir=args.workflow_dir, action_operator_dirs=['cache'], wait=False)
+        print('')
 
 
 def sequential_training(args):
@@ -175,20 +251,14 @@ def sequential_training(args):
 
             # trigger dag on participant - then wait until remote Dag recieved model
             trigger_training_dag(participant, args)
-            time.sleep(60*1)
 
-            # file path for next model checkpoint
-            model_fp = os.path.join(args.model_cache, 'model_checkpoint_from_{}.pt'.format(participant))
-            
             # wait + check if new model arrived in cache folder
             print('Keep waiting for model...')
-            while not os.path.isfile(model_fp):
-                time.sleep(60*args.checking_interval)
-            
-            print('Model recieved from participant {}'.format(participant))
-            
+            while not check_minio_and_load_models([participant], args):
+                time.sleep(60*args.check_minio_interval)
+                        
             # load model and optimizer
-            checkpoint = torch.load(model_fp)
+            checkpoint = torch.load(os.path.join(args.model_cache, 'model_checkpoint_from_{}.pt'.format(participant)))
 
             model = ClassifierMNIST()
             model.load_state_dict(checkpoint['model'])
@@ -205,65 +275,8 @@ def sequential_training(args):
             model_to_minio(model, optimizer)
 
             # clear model cache
-            apply_minio_action(action='remove', action_operator_dirs=['cache'], wait=False)
+            apply_local_minio_action(action='remove', action_operator_dirs=['cache'], wait=False)
             print('')
-
-
-def averaging_training(args):
-    """Logic for federated averaging"""
-
-    # prepare dataloader if central testing is wanted
-    if args.apply_tests:
-        dataloader_test = prepare_dataloader_test(args)
-
-    print('#'*10, 'TRAINING START - MODEL AVERAGING', '#'*10)
-    print('#'*10, 'Using following participants: {}'.format(args.participants))
-
-    for epoch in range(0, args.epochs):
-        print('#'*10, 'EPOCH {}'.format(epoch))
-        
-        # Clear airflow directories
-        shutil.rmtree(args.model_dir)
-        shutil.rmtree(args.model_cache)
-        
-        # trigger remote dags on participants
-        print('#'*10, 'Distribute model to participants: {}'.format(args.participants))
-        for participant in args.participants:
-            trigger_training_dag(participant, args)
-
-        model_file_list = [os.path.join(args.model_cache, f'model_checkpoint_from_{participant}.pt') for participant in args.participants]
-        
-        # wait until all models were send back to scheduler        
-        print('Keep waiting for models...')
-        while not all([os.path.isfile(model_fb) for model_fb in model_file_list]):
-            time.sleep(60*args.checking_interval)
-
-        print('Recieved models from all particpants ({})'.format(args.participants))
-
-        # load state dicts & create new avg model
-        model_state_dicts = [torch.load(model)['model'] for model in model_file_list]
-        sd_avg = average_model_state_dicts(model_state_dicts)
-        
-        model_avg = ClassifierMNIST()
-        model_avg.load_state_dict(sd_avg)
-
-        optimizer = torch.optim.SGD(model_avg.parameters(), lr=args.lr)
-        optimizer.load_state_dict(
-            torch.load(model_file_list[0])['optimizer']
-        )
-
-        # test model on locally available test data
-        if args.apply_tests:
-            device = 'cpu'
-            print('Testing averaged model at current state using device: {}'.format(device))
-            test(model_avg, dataloader_test, device)
-        
-        # save new 'model_checkpoint.pt' to /model and put to local Minio
-        model_to_minio(model_avg, optimizer)
-
-        # clear local Minio model cache
-        apply_minio_action(action='remove', run_dir=args.workflow_dir, action_operator_dirs=['cache'], wait=False)
-        print('')
 
 
 def main(args):

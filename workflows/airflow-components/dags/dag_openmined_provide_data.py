f89d867b2d5d386b3ce11b1bff53b0a2d1023b23
Marius <marius.kempf@dkfz-heidelberg.de>
2021-04-15 18:04:14 +0200

    added openminend dags and operators

---
+++ workflows/airflow-components/dags/dag_openmined_provide_data.py
@@ -0,0 +1,69 @@
+from datetime import timedelta
+
+from airflow.models import DAG
+from airflow.utils.dates import days_ago
+from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.utils.trigger_rule import TriggerRule
+
+from kaapana.operators.LocalMinioOperator import LocalMinioOperator
+from kaapana.operators.LocalUnzipFileOperator import LocalUnzipFileOperator
+from kaapana.operators.LocalWorkflowCleanerOperator import LocalWorkflowCleanerOperator
+
+from openmined.RunNodeOperator import RunNodeOperator
+from openmined.ProvideDataOperator import OpenminedProvideDataOperator
+
+
+log = LoggingMixin().log
+
+args = {
+    'ui_visible': False,
+    'owner': 'kaapana',
+    'start_date': days_ago(0),
+    'retries': 0,
+    'retry_delay': timedelta(seconds=30),
+}
+
+dag = DAG(
+    dag_id='openmined-provide-data',
+    default_args=args,
+    schedule_interval=None,
+    concurrency=10,
+    max_active_runs=5
+    )
+
+get_object_from_minio = LocalMinioOperator(
+    dag=dag,
+    action='get',
+    bucket_name='openmined-data',
+    action_operator_dirs=['MNIST-split'],
+    operator_out_dir='MNIST-split'
+    )
+
+unzip_files = LocalUnzipFileOperator(
+    dag=dag,
+    operator_in_dir='MNIST-split'
+    )
+
+run_node = RunNodeOperator(
+    dag=dag, 
+    operator_in_dir='./',
+    release_name='openmined-node',
+    global_id=None,         #i.e. 'co',
+    hostname=None,          #i.e. '10.128.129.6',
+    port='5000',
+    grid_network_url=None   #i.e. '10.128.129.76:7000'
+    )
+
+data2node = OpenminedProvideDataOperator(
+    dag=dag,
+    input_operator=unzip_files,
+    hostname=None,      #i.e. '10.128.129.6,
+    port='5000',
+    lifespan='15',
+    dataset='mnist',
+    exp_tag="#no-tag-given"
+    )
+
+clean = LocalWorkflowCleanerOperator(dag=dag)
+
+get_object_from_minio >> unzip_files >> [run_node, data2node] >> clean
---
+++ workflows/airflow-components/dags/dag_openmined_train_model.py
@@ -0,0 +1,44 @@
+from datetime import timedelta
+
+from airflow.models import DAG
+from airflow.utils.dates import days_ago
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.utils.log.logging_mixin import LoggingMixin
+
+from kaapana.operators.LocalMinioOperator import LocalMinioOperator
+from kaapana.operators.LocalUnzipFileOperator import LocalUnzipFileOperator
+
+from openmined.TrainModelOperator import OpenminedTrainModelOperator
+
+
+# grid information
+GRID_HOST = '10.128.129.76'
+GRID_PORT = '7000'
+
+# training/model hyperparameter
+MODEL = 'mnist'
+EPOCHS = 20
+BATCH_SIZE = 256
+LEARNING_RATE = 0.01
+
+log = LoggingMixin().log
+
+args = {
+    'ui_visible': False,
+    'owner': 'kaapana',
+    'start_date': days_ago(0),
+    'retries': 2,
+    'retry_delay': timedelta(seconds=30),
+}
+
+dag = DAG(
+    dag_id='openmined-train-model',
+    default_args=args,
+    schedule_interval=None,
+    concurrency=10,
+    max_active_runs=5
+    ) 
+
+train_model = OpenminedTrainModelOperator(dag=dag, grid_host=GRID_HOST, grid_port=GRID_PORT, model=MODEL, epochs=EPOCHS, batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE)
+
+train_model

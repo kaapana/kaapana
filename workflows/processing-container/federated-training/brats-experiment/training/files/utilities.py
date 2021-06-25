4908de3d1659d84fa03d4628a58ce2d965002286
Marius <marius.kempf@dkfz-heidelberg.de>
2021-06-25 16:03:25 +0200

    added util methods from main processing file

--- workflows/processing-container/federated-training/brats-experiment/training/files/utilities.py
+++ workflows/processing-container/federated-training/brats-experiment/training/files/utilities.py
@@ -1,4 +1,11 @@
+import os
 import numpy as np
+
+import torch
+
+from monai.networks.nets import UNet
+from monai.data import DataLoader
+from monai.apps import DecathlonDataset
 
 from monai.transforms import (
     AsChannelFirstd,
@@ -46,6 +53,7 @@ class ConvertToMultiChannelBasedOnBratsClassesd(MapTransform):
         return d
 
 
+### APPLIED TRANSOFRMATIONS ###
 TRAIN_TRANSFORM = Compose(
     [
         # load 4 Nifti images and stack them together
@@ -84,3 +92,90 @@ VAL_TRANSFORM = Compose(
         ToTensord(keys=["image", "label"]),
     ]
 )
+### APPLIED TRANSOFRMATIONS ###
+
+
+def prepare_data_loader(args):
+    print('Start preparing data loaders...')
+    
+    train_dataset = DecathlonDataset(
+        root_dir=args.root_dir,
+        task="Task01_BrainTumour",
+        transform=TRAIN_TRANSFORM,
+        section="training",
+        download=False,
+        num_workers=4,
+        cache_num=100,
+    )
+    train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=4)
+
+    val_dataset = DecathlonDataset(
+        root_dir=args.root_dir,
+        task="Task01_BrainTumour",
+        transform=VAL_TRANSFORM,
+        section="validation",
+        download=False,
+        num_workers=4,
+    )
+    val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False, num_workers=4)
+    print('Finished preparing data loaders...')
+
+    return train_loader, val_loader
+
+
+def prepare_model_and_optimizer(device):
+    """Loads model/optimizer received from scheduler
+    
+    IMPORTANT:
+    Send model to device before initializing optimizer (and loading state dict).
+    Otherwise, the training will fail due to cpu/gpu tensors.
+    See: https://discuss.pytorch.org/t/effect-of-calling-model-cuda-after-constructing-an-optimizer/15165/5
+    See: https://pytorch.org/tutorials/beginner/saving_loading_models.html#saving-loading-model-across-devices
+    --> not 100% sure whats happening exactly!
+    """
+
+    checkpoint = torch.load(os.path.join(args.model_dir, 'model_checkpoint.pt'), map_location="cuda:0")
+
+    model = UNet(
+        dimensions=3,
+        in_channels=4,
+        out_channels=3,
+        channels=(16, 32, 64, 128, 256),
+        strides=(2, 2, 2, 2),
+        num_res_units=2,
+    )
+    model.load_state_dict(checkpoint['model'])
+    
+    # send model to GPU
+    model.to(device)
+
+    optimizer = torch.optim.Adam(
+        model.parameters(), 1e-4, weight_decay=1e-5, amsgrad=True
+    ) # <-- values are overwritten in next step
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
+    return model, optimizer
+
+
+def save_model(args, model, optimizer, final=False):
+    """Saves model & optimizer as checkpoint either to send back """
+    
+    model_checkpoint = {
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict()
+    }
+    if final:
+        file_path = os.path.join(args.model_cache, 'model_checkpoint_from_{}.pt'.format(args.host_ip))
+    else:
+        file_path = os.path.join(args.checkpoints_dir, 'model_checkpoint_from_{}_round_{}.pt'.format(args.host_ip, args.fed_round))
+    torch.save(model_checkpoint, file_path)
+
+
+def check_for_cuda():
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+        print('Using device: {}'.format(device))
+    else:
+        raise Exception ('Cuda is not available!')
+    return device
+

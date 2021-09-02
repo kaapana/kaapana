8c6bdb69d451568e49749afd7a3160d77e47e384
Jonas Scherer <jonas.scherer@dkfz-heidelberg.de>
2021-09-02 12:57:50 +0200

    Radiomics fix

--- workflows/airflow-components/dags/dag_nnunet_inference.py
+++ workflows/airflow-components/dags/dag_nnunet_inference.py
@@ -211,4 +211,5 @@ nrrd2dcmSeg_multi = Itk2DcmSegOperator(
 dcmseg_send_multi = DcmSendOperator(dag=dag, input_operator=nrrd2dcmSeg_multi)
 clean = LocalWorkflowCleanerOperator(dag=dag, clean_workflow_dir=True)
 
-get_input >> get_task_model >> dcm2nifti >> nnunet_predict >> nrrd2dcmSeg_multi >> dcmseg_send_multi >> clean
+get_task_model >> nnunet_predict
+get_input >> dcm2nifti >> nnunet_predict >> nrrd2dcmSeg_multi >> dcmseg_send_multi >> clean

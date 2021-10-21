d8340da0331981bd288b89d985fe47e190064e92
Philipp Schader <philipp.schader@dkfz-heidelberg.de>
2021-10-21 17:12:48 +0200

    Add slim viewer service

---
+++ services/store/slim-viewer/docker/local.js
@@ -0,0 +1,47 @@
+var hostname=window.location.href.split("//")[1].split("/")[0]
+console.log("Hostname: "+hostname)
+window.config = {
+  path: "/slim",
+  /** This is an array, but we'll only use the first entry for now */
+  servers: [
+    {
+      id: "local",
+      url: "https://"+hostname+"/dcm4chee-arc/aets/KAAPANA/rs",
+      write: true
+    }
+  ],
+  annotations: [
+    {
+      finding: {
+        value: '108369006',
+        schemeDesignator: 'SCT',
+        meaning: 'Tumor'
+      },
+      style: {
+        stroke: {
+          color: [251, 134, 4, 1],
+          width: 2
+        },
+        fill: {
+          color: [255, 255, 255, 0.2]
+        }
+      }
+    },
+    {
+      finding: {
+        value: '85756007',
+        schemeDesignator: 'SCT',
+        meaning: 'Tissue'
+      },
+      style: {
+        stroke: {
+          color: [51, 204, 51, 1],
+          width: 2
+        },
+        fill: {
+          color: [255, 255, 255, 0.2]
+        }
+      }
+    }
+  ]
+};

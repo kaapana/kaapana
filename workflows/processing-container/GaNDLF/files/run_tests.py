963b9410bf4cc95853a42bf9ce87df4c20a56bbd
s669m <santhosh.parampottupadam@dkfz.de>
2021-11-21 10:01:31 +0100

    adding processing files

--- workflows/processing-container/GaNDLF/files/run_tests.py
+++ workflows/processing-container/GaNDLF/files/run_tests.py
@@ -1,4 +1,8 @@
 import subprocess
+import sys
 print("calling sub process..!")
-process = subprocess.call(["/bin/bash", "-c","pytest > new_output.txt"],stdout=subprocess.PIPE, shell=False)
+#resp = subprocess.check_output(["/bin/bash", "-c","pytest > new_outputs.txt"], stderr=subprocess.STDOUT)
+resp = subprocess.check_output(["/bin/bash", "-c","pytest"], stderr=subprocess.STDOUT)
+print(resp)
+
 print("Completed!")

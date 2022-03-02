a23f0f2efcda49c79cdad215c68670b795682f86
Kaushal Parekh <kaushal.parekh@dkfz-heidelberg.de>
2022-03-02 12:29:05 +0100

    Add workflow for TFDA execution orchestrator

---
+++ workflows/airflow-components/dags/dag_tfda_execution_orchestrator.py
@@ -0,0 +1,54 @@
+from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+from airflow.models import DAG
+from kaapana.operators.LocalCreateIsoInstanceOperator import LocalCreateIsoInstanceOperator
+from kaapana.operators.LocalInstallPlatformOnIsoEnvOperator import LocalInstallPlatformOnIsoEnvOperator
+from kaapana.operators.LocalCopyDataAndAlgoOperator import LocalCopyDataAndAlgoOperator
+from kaapana.operators.LocalRunAlgoSendResultOperator import LocalRunAlgoSendResultOperator
+from kaapana.operators.LocalDeleteIsoEnvOperator import LocalDeleteIsoEnvOperator
+from kaapana.operators.DcmSendOperator import DcmSendOperator
+from kaapana.operators.LocalWorkflowCleanerOperator import LocalWorkflowCleanerOperator
+
+log = LoggingMixin().log
+
+ae_title = "NONE"
+pacs_host = ""
+pacs_port = 11112
+
+args = {
+    'ui_visible': True,
+    'ui_forms': ui_forms,
+    'owner': 'kaapana',
+    'start_date': days_ago(0),
+    'retries': 0,
+    'retry_delay': timedelta(seconds=30)
+}
+
+dag = DAG(
+    dag_id='tfda-execution-orchestrator',
+    default_args=args,
+    concurrency=10,
+    max_active_runs=10,
+    schedule_interval=None
+)
+
+create_iso_env = LocalCreateIsoInstanceOperator(dag=dag)
+install_platform = LocalInstallPlatformOnIsoEnvOperator(dag=dag)
+copy_data_algo = LocalCopyDataAndAlgoOperator(dag=dag)
+run_algo_send_result = LocalRunAlgoSendResultOperator(dag=dag)
+delete_iso_inst = LocalDeleteIsoEnvOperator(dag=dag)
+# get_input = LocalGetInputDataOperator(dag=dag)
+# dcm_send = DcmSendOperator(
+#     dag=dag,
+#     input_operator=get_input,
+#     ae_title=ae_title,
+#     pacs_host=pacs_host,
+#     pacs_port=pacs_port,
+#     host_network=True,
+#     level='element'
+# )
+
+clean = LocalWorkflowCleanerOperator(dag=dag, clean_workflow_dir=True)
+
+create_iso_env >> install_platform >> copy_data_algo >> run_algo_send_result >> delete_iso_inst >> clean
---
+++ workflows/airflow-components/plugins/kaapana/operators/LocalCreateIsoInstanceOperator.py
@@ -0,0 +1,36 @@
+import os
+import glob
+import zipfile
+
+from kaapana.operators.KaapanaPythonBaseOperator import KaapanaPythonBaseOperator
+from kaapana.blueprints.kaapana_global_variables import BATCH_NAME, WORKFLOW_DIR
+
+
+class LocalCreateIsoInstanceOperator(KaapanaPythonBaseOperator):
+
+    def start(self, ds, **kwargs):
+        print("Starting an isolated environment...")
+        print(kwargs)
+
+        # run_dir = os.path.join(WORKFLOW_DIR, kwargs['dag_run'].run_id)
+        # batch_input_dir = os.path.join(run_dir, self.operator_in_dir)
+        # print('input_dir', batch_input_dir)
+
+        # batch_output_dir = os.path.join(run_dir, self.operator_out_dir)  # , project_name)
+        # if not os.path.exists(batch_output_dir):
+        #     os.makedirs(batch_output_dir)
+
+        # for file_path in glob.glob(os.path.join(batch_input_dir, '*.zip')):
+        #     with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        #         zip_ref.extractall(batch_output_dir)
+
+    def __init__(self,
+                 dag,
+                 *args, **kwargs):
+
+        super().__init__(
+            dag,
+            name="create-iso-inst",
+            python_callable=self.start,
+            *args, **kwargs
+        )

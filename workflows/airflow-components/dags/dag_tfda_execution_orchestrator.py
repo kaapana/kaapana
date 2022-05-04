12cf59292fd9053d8b32de22b62532a4c1365548
s669m <santhosh.parampottupadam@dkfz-heidelberg.de>
2022-05-04 12:41:19 +0200

    BugFix: localCopyDataAlgo invalid tar argument

--- workflows/airflow-components/dags/dag_tfda_execution_orchestrator.py
+++ workflows/airflow-components/dags/dag_tfda_execution_orchestrator.py
@@ -2,13 +2,27 @@ from airflow.utils.log.logging_mixin import LoggingMixin
 from airflow.utils.dates import days_ago
 from datetime import timedelta
 from airflow.models import DAG
-from tfda_execution_orchestrator.LocalCreateIsoInstanceOperator import LocalCreateIsoInstanceOperator
-from tfda_execution_orchestrator.LocalChangeIsoInstHostnameOperator import LocalChangeIsoInstHostnameOperator
-from tfda_execution_orchestrator.LocalInstallPlatformDepsOnIsoEnvOperator import LocalInstallPlatformDepsOnIsoEnvOperator
-from tfda_execution_orchestrator.LocalDeployPlatformOnIsoEnvOperator import LocalDeployPlatformOnIsoEnvOperator
-from tfda_execution_orchestrator.LocalCopyDataAndAlgoOperator import LocalCopyDataAndAlgoOperator
-from tfda_execution_orchestrator.LocalRunAlgoSendResultOperator import LocalRunAlgoSendResultOperator
-from tfda_execution_orchestrator.LocalDeleteIsoEnvOperator import LocalDeleteIsoEnvOperator
+from tfda_execution_orchestrator.LocalCreateIsoInstanceOperator import (
+    LocalCreateIsoInstanceOperator,
+)
+from tfda_execution_orchestrator.LocalChangeIsoInstHostnameOperator import (
+    LocalChangeIsoInstHostnameOperator,
+)
+from tfda_execution_orchestrator.LocalInstallPlatformDepsOnIsoEnvOperator import (
+    LocalInstallPlatformDepsOnIsoEnvOperator,
+)
+from tfda_execution_orchestrator.LocalDeployPlatformOnIsoEnvOperator import (
+    LocalDeployPlatformOnIsoEnvOperator,
+)
+from tfda_execution_orchestrator.LocalCopyDataAndAlgoOperator import (
+    LocalCopyDataAndAlgoOperator,
+)
+from tfda_execution_orchestrator.LocalRunAlgoSendResultOperator import (
+    LocalRunAlgoSendResultOperator,
+)
+from tfda_execution_orchestrator.LocalDeleteIsoEnvOperator import (
+    LocalDeleteIsoEnvOperator,
+)
 from kaapana.operators.DcmSendOperator import DcmSendOperator
 from kaapana.operators.LocalWorkflowCleanerOperator import LocalWorkflowCleanerOperator
 
@@ -19,27 +33,27 @@ pacs_host = ""
 pacs_port = 11112
 
 args = {
-    'ui_visible': True,
+    "ui_visible": True,
     # 'ui_forms': ui_forms,
-    'owner': 'kaapana',
-    'start_date': days_ago(0),
-    'retries': 0,
-    'retry_delay': timedelta(seconds=30)
+    "owner": "kaapana",
+    "start_date": days_ago(0),
+    "retries": 0,
+    "retry_delay": timedelta(seconds=30),
 }
 
 dag = DAG(
-    dag_id='tfda-execution-orchestrator',
+    dag_id="tfda-execution-orchestrator",
     default_args=args,
     concurrency=10,
     max_active_runs=10,
-    schedule_interval=None
+    schedule_interval=None,
 )
 
 create_iso_env = LocalCreateIsoInstanceOperator(dag=dag)
 # change_hostname = LocalChangeIsoInstHostnameOperator(dag=dag)
 # install_platform_dependencies = LocalInstallPlatformDepsOnIsoEnvOperator(dag=dag)
 # deploy_platform = LocalDeployPlatformOnIsoEnvOperator(dag=dag)
-copy_data_algo = LocalCopyDataAndAlgoOperator(dag=dag, tarball_file="tarball_name")
+copy_data_algo = LocalCopyDataAndAlgoOperator(dag=dag)
 run_algo_send_result = LocalRunAlgoSendResultOperator(dag=dag)
 delete_iso_inst = LocalDeleteIsoEnvOperator(dag=dag)
 

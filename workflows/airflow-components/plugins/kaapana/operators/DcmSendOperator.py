5df5b87789332c8214531e59e1319226a01cae62
Stefan Denner <stefan.denner@dkfz-heidelberg.de>
2022-05-16 18:13:34 +0200

    Operator docs: DcmSendOperator & LocalDicomSendOperator
    https://gitlab.hzdr.de/kaapana/kaapana/-/issues/103

--- workflows/airflow-components/plugins/kaapana/operators/DcmSendOperator.py
+++ workflows/airflow-components/plugins/kaapana/operators/DcmSendOperator.py
@@ -1,26 +1,41 @@
-import os
-import glob
-from datetime import timedelta
-import pydicom
+from datetime import timedelta, datetime
 
-from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator, default_registry, default_project
-from kaapana.blueprints.kaapana_global_variables import BATCH_NAME, WORKFLOW_DIR
+from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator, \
+    default_registry
 
 
 class DcmSendOperator(KaapanaBaseOperator):
+    """
+    Operator sends data to the platform.
+
+    This operator is used for sending data to the platform.
+    For dcmsend documentation please have a look at https://support.dcmtk.org/docs/dcmsend.html.
+    """
 
     def __init__(self,
-                dag,
-                name="dcmsend",
-                ae_title='NONE',
-                pacs_host='ctp-dicom-service.flow.svc',
-                pacs_port='11112',
-                env_vars=None,
-                level='element',
-                check_arrival=False,
-                execution_timeout=timedelta(minutes=60),
-                **kwargs
-                ):
+                 dag,
+                 name: str = "dcmsend",
+                 ae_title: str = 'NONE',
+                 pacs_host: str = 'ctp-dicom-service.flow.svc',
+                 pacs_port: str = '11112',
+                 env_vars=None,
+                 level: str = 'element',
+                 check_arrival: bool = False,
+                 execution_timeout: datetime = timedelta(minutes=60),
+                 **kwargs
+                 ):
+
+        """
+        :param ae_title: calling Application Entity (AE) title
+        :param pacs_host: Host of PACS
+        :param pacs_port: Port of PACS
+        :param env_vars: Environment variables
+        :param level: "element" or "batch"
+            If batch, an operator folder next to the batch folder with .dcm files is expected.
+            If element, *.dcm are expected in the corresponding operator with .dcm files is expected.'
+        :param check_arrival: Verifies if data transfer was successful
+        :param execution_timeout: timeout for connection requests
+        """
 
         if level not in ['element', 'batch']:
             raise NameError('level must be either "element" or "batch". \

4d965722a699c22159903d94c4eeb678f141504d
Jonas Scherer <jonas.scherer@dkfz-heidelberg.de>
2022-05-30 10:14:44 +0200

    version bump to v0.1.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'MIC @DKFZ'
 # The short X.Y version
 version = '0.1.3'
 # The full version, including alpha/beta/rc tags
-release = '0.1.3-beta'
+release = '0.1.3'
 
 
 # -- General configuration ---------------------------------------------------

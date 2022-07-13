314c6e5ce4c6c8f86ce1a4b61ada0e01021fcc5b
Stefan Denner <stefan.denner@dkfz-heidelberg.de>
2022-07-13 10:31:37 +0200

    - Operator docs: BprOperator (Body Part Prediction)
    https://gitlab.hzdr.de/kaapana/kaapana/-/issues/123

--- data-processing/processing-pipelines/bodypartregression/extension/docker/files/bodypartregression/BprOperator.py
+++ data-processing/processing-pipelines/bodypartregression/extension/docker/files/bodypartregression/BprOperator.py
@@ -1,8 +1,31 @@
-from kaapana.kubetools.resources import Resources as PodResources
-from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator, default_registry, default_platform_abbr, default_platform_version
 from datetime import timedelta
 
+from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator, \
+    default_registry, default_platform_abbr, default_platform_version
+
+
 class BprOperator(KaapanaBaseOperator):
+    """
+    Regresses the body part a given slice is from.
+
+    The Body Part Regression (BPR) operator translates the anatomy in a
+    radiologic volume into a machine-interpretable form. Each axial slice maps
+    to a slice score. The slice scores monotonously increase with patient
+    height.
+
+    Code: https://github.com/mic-dkfz/bodypartregression
+    Paper: https://arxiv.org/abs/2110.09148
+
+    **Inputs:**
+
+    * Data in nifti format
+
+    **Outputs:**
+
+    * Regresses a slice score for each given sample
+
+    """
+
     execution_timeout = timedelta(minutes=10)
     task_dict = {}
 
@@ -14,7 +37,7 @@ class BprOperator(KaapanaBaseOperator):
                  execution_timeout=execution_timeout,
                  **kwargs
                  ):
-        envs = { "STRINGIFY_JSON": str(stringify_json)}
+        envs = {"STRINGIFY_JSON": str(stringify_json)}
         env_vars.update(envs)
 
         super().__init__(
@@ -29,4 +52,4 @@ class BprOperator(KaapanaBaseOperator):
             gpu_mem_mb=5000,
             env_vars=env_vars,
             **kwargs
-        ) 
+        )

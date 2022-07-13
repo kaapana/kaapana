7765d4bff1751cd7b8f65ed95cc15baf6eef2e08
Klaus Kades <k.kades@dkfz.de>
2022-07-13 16:31:10 +0200

    removed too long logs for federated backend

--- services/applications/federated-backend/docker/files/app/main.py
+++ services/applications/federated-backend/docker/files/app/main.py
@@ -1,4 +1,5 @@
 import os
+import urllib3
 from fastapi import Depends, FastAPI, Request
 
 from .internal import admin
@@ -11,6 +12,7 @@ from app.utils import get_remote_updates
 
 models.Base.metadata.create_all(bind=engine)
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 app = FastAPI(openapi_prefix=os.getenv('APPLICATION_ROOT', ''))
 

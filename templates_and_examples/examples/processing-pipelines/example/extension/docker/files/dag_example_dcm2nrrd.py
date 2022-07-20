78a8abdcceb9ea1d04e079ae310a64797470908c
Lorenz Feineis <lorenz.feineis@dkfz-heidelberg.de>
2022-07-20 12:01:24 +0200

    Added the example processing-pipeline example.

---
+++ templates_and_examples/examples/processing-pipelines/example/extension/docker/files/dag_example_dcm2nrrd.py
@@ -0,0 +1,51 @@
+from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+from airflow.models import DAG
+from kaapana.operators.DcmConverterOperator import DcmConverterOperator
+from kaapana.operators.LocalGetInputDataOperator import LocalGetInputDataOperator
+from kaapana.operators.LocalWorkflowCleanerOperator import LocalWorkflowCleanerOperator
+from kaapana.operators.LocalMinioOperator import LocalMinioOperator
+
+
+log = LoggingMixin().log
+
+ui_forms = {
+    "workflow_form": {
+        "type": "object",
+        "properties": {
+            "single_execution": {
+                "title": "single execution",
+                "description": "Should each series be processed separately?",
+                "type": "boolean",
+                "default": False,
+                "readOnly": False,
+            }
+        }
+    }
+}
+
+args = {
+    'ui_forms': ui_forms,
+    'ui_visible': True,
+    'owner': 'kaapana',
+    'start_date': days_ago(0),
+    'retries': 0,
+    'retry_delay': timedelta(seconds=30)
+}
+
+dag = DAG(
+    dag_id='example-dcm2nrrd',
+    default_args=args,
+    schedule_interval=None
+    )
+
+
+get_input = LocalGetInputDataOperator(dag=dag)
+convert = DcmConverterOperator(dag=dag, input_operator=get_input, output_format='nrrd')
+put_to_minio = LocalMinioOperator(dag=dag, action='put', action_operators=[convert], file_white_tuples=('.nrrd'))
+clean = LocalWorkflowCleanerOperator(dag=dag,clean_workflow_dir=True)
+
+get_input >> convert >> put_to_minio >> clean
+
+

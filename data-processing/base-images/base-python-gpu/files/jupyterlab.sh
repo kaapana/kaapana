b109cffae949539548d971eef9834fb23b396cfc
Jonas Reinwald <jonas.reinwald@dkfz-heidelberg.de>
2022-07-29 14:22:13 +0200

    disable webpdf export in jupyterlab

--- data-processing/base-images/base-python-gpu/files/jupyterlab.sh
+++ data-processing/base-images/base-python-gpu/files/jupyterlab.sh
@@ -4,4 +4,7 @@ sed -i "s+^.*ServerApp.base_url.*$+c.ServerApp.base_url = '$INGRESS_PATH'+g" /ro
 sed -i "s+^.*ServerApp.root_dir.*$+c.ServerApp.root_dir = '/kaapanasrc'+g" /root/.jupyter/jupyter_lab_config.py
 sed -i "s+^.*ServerApp.quit_button.*$+c.ServerApp.quit_button = False+g" /root/.jupyter/jupyter_lab_config.py # hide quit button, as quitting the app causes the pod to shut down, triggering a kubernetes CrashLoopBackoff
 
+# disable the webpdf export option, as requirements it are currently not supported
+echo -e "\nc.WebPDFExporter.enabled = False" >>/root/.jupyter/jupyter_lab_config.py
+
 jupyter lab --ip=* --port=8888 --no-browser --allow-root

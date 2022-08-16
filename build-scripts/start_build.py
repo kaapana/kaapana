13dae3cf7d6c5b23e0b94ed1b52181db14a10597
Jonas Scherer <jonas.scherer@dkfz-heidelberg.de>
2022-08-16 09:14:39 +0200

    racoon build-script adjustments

--- build-scripts/start_build.py
+++ build-scripts/start_build.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     parser.add_argument("-c", "--config", dest="config_filepath", default=None, help="Path the the build-config.yaml")
     parser.add_argument("-u", "--username", dest="username", default=None, help="Username")
     parser.add_argument("-p", "--password", dest="password", default=None, required=False, help="Password")
-    parser.add_argument("-bo", "--build-only", dest="build_only", default=None, action='store_true', help="Just building the containers and charts -> no pushing")
+    parser.add_argument("-bo", "--build-only", dest="build_only", default=None, action="store_true", help="Just building the containers and charts -> no pushing")
     parser.add_argument("-oi", "--create-offline-installation", dest="create_offline_installation", default=None, help="Will create a docker dump, from which the platfrom can be deployed.")
     parser.add_argument("-pm", "--push-to-microk8s", dest="push_to_microk8s", default=None, help="Will create a docker dump, from which the platfrom can be deployed.")
     parser.add_argument("-ll", "--log-level", dest="log_level", default=None, help="Set log-level.")
@@ -30,6 +30,8 @@ if __name__ == '__main__':
     parser.add_argument("-bd", "--build-dir", dest="build_dir", default=None, help="Specify the main Kaapana repo-dir to build from.")
     parser.add_argument("-kd", "--kaapana-dir", dest="kaapana_dir", default=None, help="Specify the main Kaapana repo-dir to build from.")
     parser.add_argument("-es", "--external-sources", dest="external_source_dirs", default=None, help="External dirs to search for containers and charts.")
+    parser.add_argument("-nl", "--no-login", dest="no_login", default=False, action="store_true",help="Skipps the logins to the container registry (expects to be already logged in).")
+
     args = parser.parse_args()
 
     kaapana_dir = args.kaapana_dir if args.kaapana_dir != None else os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,7 +117,8 @@ if __name__ == '__main__':
     exit_on_error = args.exit_on_error if args.exit_on_error != None else conf_exit_on_error
     platform_filter = args.platform_filter.split(",") if args.platform_filter != None else conf_platform_filter
     skip_push_no_changes = args.skip_push_no_changes if args.skip_push_no_changes != None else conf_skip_push_no_changes
-
+    no_login = args.no_login
+    
     for external_source_dir in external_source_dirs:
         if not os.path.isdir(external_source_dir):
             logger.error("---------------------------------------------------------------------------------------------")
@@ -162,10 +165,11 @@ if __name__ == '__main__':
     logger.info(f"{create_offline_installation=}")
     logger.info(f"{build_installer_scripts=}")
     logger.info(f"{push_to_microk8s=}")
+    logger.info(f"{no_login=}")
     logger.info("")
     logger.info("-----------------------------------------------------------")
 
-    if not build_only:
+    if not build_only and not no_login:
         if registry_user is None:
             registry_user = os.getenv("REGISTRY_USER", None)
         if registry_pwd is None:
@@ -224,7 +228,7 @@ if __name__ == '__main__':
         enable_build=container_build,
         enable_push=containers_push,
     )
-    if not build_only:
+    if not build_only and not no_login:
         container_registry_login(username=registry_user, password=registry_pwd)
         helm_registry_login(username=registry_user, password=registry_pwd)
 

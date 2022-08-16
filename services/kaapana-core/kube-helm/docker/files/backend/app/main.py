1fa5115d8afefe34406a8fad5745946f51834613
Jonas Scherer <jonas.scherer@dkfz-heidelberg.de>
2022-08-16 14:21:17 +0200

    progress

--- services/kaapana-core/kube-helm/docker/files/backend/app/main.py
+++ services/kaapana-core/kube-helm/docker/files/backend/app/main.py
@@ -7,32 +7,25 @@ from repeat_timer import RepeatedTimer
 from utils import get_extensions_list, charts_cached, helm_search_repo
 import uvicorn
 from fastapi.templating import Jinja2Templates
+from os.path import basename, dirname, join
 
-app = FastAPI(openapi_prefix=os.getenv('APPLICATION_ROOT', ''))
+url_prefix = os.getenv('APPLICATION_ROOT', '')
+app = FastAPI(openapi_prefix=url_prefix)
 
-if __name__ == 'app.main':
+app.mount("/static", StaticFiles(directory=join(dirname(str(__file__)), "static")), name="static")
 
-    app.mount("/static", StaticFiles(directory="app/static"), name="static")
-    app.include_router(
-        router,
-    )
+app.include_router(
+    router,
+)
+if charts_cached == None:
+    helm_search_repo(keywords_filter=['kaapanaapplication', 'kaapanaworkflow'])
 
-    if charts_cached == None:
-        helm_search_repo(keywords_filter=['kaapanaapplication', 'kaapanaworkflow'])
+rt = RepeatedTimer(5, get_extensions_list)
 
-    rt = RepeatedTimer(5, get_extensions_list)
+# if __name__ == 'app.main':
 
 if __name__ == "__main__":
 
-    app.mount("/static", StaticFiles(directory="/home/jonas/projects/kaapana/services/kaapana-core/kube-helm/docker/files/backend/app/static"), name="static")
-    app.include_router(
-        router,
-    )
-
-    if charts_cached == None:
-        helm_search_repo(keywords_filter=['kaapanaapplication', 'kaapanaworkflow'])
-
-    rt = RepeatedTimer(5, get_extensions_list)
     uvicorn.run(app, host="0.0.0.0", port=5000)
 
 # @app.on_event("startup")

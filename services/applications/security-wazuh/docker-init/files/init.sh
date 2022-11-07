9cd532f2013c1506ee5aca534677d923787dc168
Jonas Reinwald <jonas.reinwald@dkfz-heidelberg.de>
2022-11-07 10:59:47 +0100

    add unfinished wazuh security extension

---
+++ services/applications/security-wazuh/docker-init/files/init.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# remove 
+
+if [ -d "./config" ]; then
+    rm -rf ./config
+fi
+mkdir ./config
+mv /tmp-config/* ./config 
+
+sed -i 's/opensearch.ssl.verificationMode: certificate/opensearch.ssl.verificationMode: none/' ./config/wazuh_dashboard/opensearch_dashboards.yml
+sed -i 's/server.ssl.enabled: true/server.ssl.enabled: false/' ./config/wazuh_dashboard/opensearch_dashboards.yml
+
+mkdir ./config/wazuh_indexer_ssl_certs
+mv /tmp-certificates/* ./config/wazuh_indexer_ssl_certs/
+
+# without write permissions for other users the wazuh-indexer container fails to start
+mkdir ./wazuh-indexer-data
+chmod -R 777 ./wazuh-indexer-data
+
+# curl <registry url for security extension>

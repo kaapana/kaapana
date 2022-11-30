d89444f20217c501181007b41d6743703f545a7b
Markus Bujotzek <markus.bujotzek@dkfz-heidelberg.de>
2022-11-30 16:26:45 +0100

    EMS working for local experiment execution, including delete, restart, abort for jobs and experiments

--- services/base/kaapana-backend/docker/files/app/experiments/schemas.py
+++ services/base/kaapana-backend/docker/files/app/experiments/schemas.py
@@ -48,7 +48,8 @@ class KaapanaInstance(KaapanaInstanceBase):
     allowed_datasets: str
     time_created: datetime.datetime
     time_updated: datetime.datetime
-
+    # involved_experiment_ids: List[int]
+    
     @validator('allowed_dags')
     def convert_allowed_dags(cls, v):
         return sorted(json.loads(v))
@@ -76,9 +77,8 @@ class JobBase(BaseModel):
     dag_id: str = None
     run_id: str = None
     description: str = None
-    external_job_id: int = None  # job_id on another system
-    addressed_kaapana_instance_name: str = None  # Remote Kaapana instance that is addressed, not external kaapana_instance_id!
-
+    external_job_id: int = None # job_id on another system
+    addressed_kaapana_instance_name: str = None # Remote Kaapana instance that is addressed, not external kaapana_instance_id!
 
 class Job(JobBase):
     id: int
@@ -101,15 +101,15 @@ class Job(JobBase):
     class Config:
         orm_mode = True
 
-
 class JobCreate(JobBase):
     conf_data: dict = {}
     kaapana_instance_id: int
+    # experiment_id: int = None
     username: str = None
 
 
 class JobUpdate(JobBase):
-    job_id: int
+    job_id: int   # not defined in model Experiment but still needed in client.py and crud.py
     # status: str
     # run_id: str = None
     # description: str = None
@@ -127,6 +127,7 @@ class FilterKaapanaInstances(BaseModel):
     remote: bool = True
     dag_id: str = None
     instance_names: List = []
+    experiment_name: str = None
 
 
 class JsonSchemaData(FilterKaapanaInstances):
@@ -139,6 +140,8 @@ class Identifier(BaseModel):
 
     class Config:
         orm_mode = True
+
+
 
 
 class CohortBase(BaseModel):
@@ -183,3 +186,55 @@ class Cohort(CohortBase):
 
     class Config:
         orm_mode = True
+
+class ExperimentBase(BaseModel):
+    experiment_name: str = None
+    experiment_status: str = None
+
+class Experiment(ExperimentBase):
+    id: int
+    username: str = None
+    status: str = None
+    time_created: datetime.datetime
+    time_updated: datetime.datetime
+    # experiment_jobs: List = []     # List[Job] = [], do NOT include or get recursion error when querying jobs from experiment via 
+    # involved_kaapana_instances: List[KaapanaInstance] = []
+    cohort_name: str = None
+
+    # comment or you will get "pydantic.error_wrappers.ValidationError: 1 validation error for Experiment; response -> experiment_jobs; the JSON object must be str, bytes or bytearray, not list (type=type_error)"
+    # @validator('experiment_jobs')
+    # def convert_experiment_jobs(cls, v):
+    #     return json.loads(v)
+
+    # # convert methods for time_created and time_updated lead to datetime being displayed as e.g. "1668590451.586321" instead of sth like "2022-11-21T07:35:49.839591+00:00"
+    # @validator('time_created')
+    # def convert_time_created(cls, v):
+    #     return datetime.datetime.timestamp(v)
+    # @validator('time_updated')
+    # def convert_time_updated(cls, v):
+    #     return datetime.datetime.timestamp(v)
+
+    class Config:           # makes Pydantic model compatible with sqlalchemy ORMs
+        orm_mode = True
+
+class ExperimentCreate(ExperimentBase):
+    username: str = None
+    kaapana_instance_id: int
+    experiment_jobs: List = []     # List[Job] = []
+    # involved_kaapana_instances: List[KaapanaInstance] = []
+    cohort_name: str = None
+
+class ExperimentUpdate(ExperimentBase):
+    experiment_id: int
+
+class ExperimentWithKaapanaInstance(Experiment):
+    kaapana_instance: KaapanaInstance = None
+
+class KaapanaInstanceWithExperiments(KaapanaInstance):
+    experiments: List[Experiment] = []
+
+class JobWithExperiment(Job):
+    experiment: Experiment = None
+
+class ExperimentWithJobs(Experiment):
+    jobs: List[Job] = []
---
+++ services/utils/base-landing-page/files/kaapana_app/src/components/ExperimentTable.vue
@@ -0,0 +1,245 @@
+<template>
+    <v-card>
+      <v-card-title>
+        <v-row>
+            <h2>Experiments</h2>
+        </v-row>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search for Experiment"
+          single-line
+          hide-details
+          class="mx-4"
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="experimentHeaders"
+        :items="filteredExperiments"
+        item-key="experiment_name"
+        class="elevation-1"
+        :search="search"
+        :expanded="expanded"
+        @click:row="expandRow"
+      >
+        <template v-slot:item.actions="{ item }">
+          <v-menu transition="scale-transition">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark v-bind="attrs" v-on="on" >
+                Action
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click='abortExperiment(item)' >
+                <v-list-item-title>Abort</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click='restartExperiment(item)' >
+                <v-list-item-title>Restart</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click='deleteExperiment(item)'>
+                <v-list-item-title>Delete</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </template>
+        <template #expanded-item="{headers,item}">
+          <td :colspan="headers.length">
+            <job-table v-if="jobsofExperiment" :jobs="jobsofExperiment" :remote="clientInstance.remote" @refreshView="refreshClient()"></job-table>
+          </td>
+        </template>
+      </v-data-table>
+    </v-card>
+</template>
+  
+<script>
+
+import kaapanaApiService from "@/common/kaapanaApi.service";
+import JobTable from "./JobTable.vue";
+
+export default {
+  name: 'ExperimentTable',
+
+  components: {
+    JobTable,
+  },
+
+  data () {
+    return {
+      search: '',
+      expanded: [],
+      experimentHeaders: [
+        {
+          text: 'Experiment Name',
+          align: 'start',
+          value: 'experiment_name',
+        },
+        { text: 'Cohort Name', value: 'cohort_name' },
+        { text: 'Created', value: 'time_created' },
+        { text: 'Updated', value: 'time_updated' },
+        { text: 'Username', value: 'username' },
+        { text: 'Executing Instance Name', value: 'kaapana_instance.instance_name' },
+        { text: 'Actions', value: 'actions', sortable: false, filterable: false},
+      ],
+      clientInstance: {},
+      clientExperiments: {},
+      clientJobs: [],
+      expandedExperiment: '',
+      jobsofExperiment: [],
+      abortID: '',
+      restartID: '',
+      deleteID: '',
+    }
+  },
+
+  mounted () {
+    this.refreshClient();
+  },
+
+  props: {
+    instance: {
+      type: Object,
+      required: true
+    },
+    experiments: {
+      type: Array,
+      required: true
+    },
+    remote: {
+      type: Boolean,
+      default: true
+    }
+  },
+
+  computed: {
+    filteredExperiments() {
+      if (this.experiments !== null) {
+        console.log("filteredExperiments: ", this.experiments, "expandedExperiment", this.expandedExperiment)
+        if (this.expandedExperiment !== null) {
+          this.getJobsOfExperiment(this.expandedExperiment.experiment_name)
+        }
+        return this.experiments;
+      } else {
+        return [];
+      }
+    },
+  },
+
+  methods: {
+    // General Methods
+    refreshClient() {
+      // console.log("refreshClient() in ExpeimentTable.vue")
+      // this.getExpandedExperiment()
+      this.getClientInstance()
+      this.getClientExperiments()
+      this.getClientJobs()
+      // this.getJobsOfExpandedExperiment()
+    },
+    expandRow(item) {
+        this.expanded = item === this.expanded[0] ? [] : [item]
+        // console.log("In method expandRow: item", item, "this.expanded", this.expanded)
+        this.expandedExperiment = item
+        this.getJobsOfExperiment(this.expandedExperiment.experiment_name)
+    },
+    abortExperiment(item) {
+        this.abortID = item.id
+        console.log("Abort Experiment:", this.abortID)
+        this.abortClientExperimentAPI(this.abortID, 'abort')
+    },
+    restartExperiment(item) {
+        this.restartID = item.id
+        console.log("Restart Experiment:", this.restartID)
+        this.restartClientExperimentAPI(this.restartID, 'scheduled')
+    },
+    deleteExperiment(item) {
+        this.deleteID = item.id
+        console.log("Delete Experiment:", this.deleteID, "Item:", item)
+        this.deleteClientExperimentAPI(this.deleteID)
+    },
+
+    // API Calls
+    getClientInstance() {
+      kaapanaApiService
+        .federatedClientApiGet("/client-kaapana-instance")
+        .then((response) => {
+          this.clientInstance = response.data;
+          // console.log("Client Instance in ExperimentTable", this.clientInstance)
+        })
+        .catch((err) => {
+          this.clientInstance = {}
+        });
+    },
+    getClientExperiments() {
+      kaapanaApiService
+        .federatedClientApiGet("/experiments",{
+        limit: 100,
+        }).then((response) => {
+          this.clientExperiments = response.data;
+          // console.log(this.clientExperiments)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getClientJobs() {
+      kaapanaApiService
+        .federatedClientApiGet("/jobs",{
+          limit: 100,
+        }).then((response) => {
+          this.clientJobs = response.data;
+          // console.log("Client Jobs in ExperimentTable", this.clientJobs)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getJobsOfExperiment(experiment_name) {
+        kaapanaApiService
+          .federatedClientApiGet("/experiment_jobs",{
+            experiment_name: experiment_name,
+            limit: 100,
+          }).then((response) => {
+            this.jobsofExperiment = response.data;
+            console.log("Get Jobs of Experiment", this.jobsofExperiment)
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+    },
+    deleteClientExperimentAPI(experiment_id) {
+        kaapanaApiService
+        .federatedClientApiDelete("/experiment",{
+            experiment_id,
+        }).then((response) => {
+            console.log("Experiment deleted")
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    },
+    restartClientExperimentAPI(experiment_id, experiment_status) {
+        kaapanaApiService
+        .federatedClientApiPut("/experiment",{
+            experiment_id,
+            experiment_status,
+        }).then((response) => {
+            console.log("Experiment restarted")
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    },
+    abortClientExperimentAPI(experiment_id, experiment_status) {
+        kaapanaApiService
+        .federatedClientApiPut("/experiment",{
+            experiment_id,
+            experiment_status,
+        }).then((response) => {
+            console.log("Experiment restarted")
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    },
+  }
+}
+</script>

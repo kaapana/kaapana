7eb3d5de2bf102fddb96a475a427208bf56c5cef
Ünal Akünal <uenal.akuenal@dkfz-heidelberg.de>
2023-01-02 17:11:54 +0100

    init chart & docker for maintenance-page-kaapana

---
+++ services/kaapana-admin/maintenance-page/docker/files/kaapana_app/src/common/auth.service.ts
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import request from '@/request'
+
+const AuthService = {
+  getToken() {
+    return new Promise((resolve, reject) => {
+      let oauthUrl = ''
+      if (Vue.config.productionTip === true) {
+        oauthUrl = '/oauth2/userinfo'
+      } else {
+        oauthUrl = '/jsons/testingAuthenticationToken.json'
+      }
+      request.get(oauthUrl).then((response: any) =>  {
+        resolve(response.data)
+      }).catch((error: any) => {
+        console.log('not token there', error)
+        reject(error)
+      })
+
+    })
+  },
+  logout() {
+    location.href = '/oauth2/sign_out?rd=/auth/realms/kaapana/protocol/openid-connect/logout'
+    //location.href = '/oauth/logout' // without redirect since redirect lead often to the error page and people had to renter the url
+  },
+  getFederatedHeaders() {
+    return new Promise((resolve, reject) => {
+      request.get('/kaapana-backend/client/client-kaapana-instance').then((response: any) =>  {
+        resolve({
+          FederatedAuthorization: response.data['token']
+        })
+      }).catch((error: any) => {
+        // console.log('not token there', error)
+        // reject(error)
+      })
+
+    })
+  }
+}
+export default AuthService
---
+++ services/kaapana-admin/maintenance-page/docker/files/kaapana_app/vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    transpileDependencies: ['vuetify'],
+    devServer: {
+        allowedHosts: "all"
+    }
+}

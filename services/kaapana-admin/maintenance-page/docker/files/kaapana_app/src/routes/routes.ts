1fcd86ff083ae911ca01da49cce0619468908640
Ünal Akünal <uenal.akuenal@dkfz-heidelberg.de>
2023-01-11 22:52:53 +0100

    maintenance-page: fix image name

--- services/kaapana-admin/maintenance-page/docker/files/kaapana_app/src/routes/routes.ts
+++ services/kaapana-admin/maintenance-page/docker/files/kaapana_app/src/routes/routes.ts
@@ -8,6 +8,15 @@ const routes = [
             isPublic: false,
         },
     },
+    {
+        name: 'home',
+        path: '/maintenance',
+        component: () => import('@/views/Extensions.vue'),
+        title: 'Extensions',
+        permissions: {
+            isPublic: false,
+        },
+    },
 ]
 
 export default routes

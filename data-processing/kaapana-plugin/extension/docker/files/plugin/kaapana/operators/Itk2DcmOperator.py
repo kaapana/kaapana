836da96d18d7256598d8aec8b037abf0f8f54724
jens <jens.beyermann@dkfz-heidelberg.de>
2023-01-19 15:55:47 +0100

    added Itk2DcmOperator.py

---
+++ data-processing/kaapana-plugin/extension/docker/files/plugin/kaapana/operators/Itk2DcmOperator.py
@@ -0,0 +1,24 @@
+from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator, default_registry, kaapana_build_version
+from datetime import timedelta
+
+class Itk2DcmOperator(KaapanaBaseOperator):
+
+    def __init__(self,
+                 dag,
+                 name=None,
+                 execution_timeout=timedelta(minutes=90),
+                 *args, **kwargs
+                 ) -> None:
+
+        name = name if name is not None else "itk2dcm-converter"
+
+        super().__init__(
+            dag=dag,
+            name=name,
+            image=f"{default_registry}/itk2dcm-dev:{kaapana_build_version}",
+            image_pull_policy="Always",
+            image_pull_secrets=["registry-secret"],
+            execution_timeout=execution_timeout,
+            *args,
+            **kwargs
+        )

6294c38f6bc4ba46bda76cee7ac958a66fe9db0e
Jonas Scherer <jonas.scherer@dkfz-heidelberg.de>
2023-01-27 17:04:07 +0100

    added user + access sub-path logging withing the auth-backend

--- services/base/auth-backend/docker/files/init.py
+++ services/base/auth-backend/docker/files/init.py
@@ -3,10 +3,13 @@ import os
 import logging
 
 auth_role_mapping_path = "/app/auth_role_mapping.json"
-global auth_role_mapping_dict, logger
+global auth_role_mapping_dict
+
+
+logger = logging.getLogger("gunicorn.access")
 
 def load_auth_config():
-    global auth_role_mapping_dict, logger
+    global auth_role_mapping_dict
 
     if not os.path.exists(auth_role_mapping_path):
         logger.error("")
@@ -27,6 +30,4 @@ def load_auth_config():
     logger.warn(json.dumps(auth_role_mapping_dict, indent=4))
     logger.warn("")
 
-
-logger = logging.getLogger(__name__)
 load_auth_config()
--- services/base/auth-backend/docker/files/main.py
+++ services/base/auth-backend/docker/files/main.py
@@ -7,20 +7,34 @@ app = FastAPI()
 
 @app.get("/auth-check",status_code=status.HTTP_200_OK)
 async def auth_check(request: Request,response: Response):
+    # for header, value in request.headers.items():
+    #     logger.warn(f"{header}:{value}")
     requested_prefix = request.headers.get('x-forwarded-prefix')
     if requested_prefix is None:
         requested_prefix = request.headers.get('x-forwarded-uri')
         
     if requested_prefix is None:
-        logger.info("No HTTP_X_FORWARDED_PREFIX could be identified within the request -> restricting access.")
         response.status_code = status.HTTP_403_FORBIDDEN
-        return "x-forwarded-prefix not found"
+        message = "No HTTP_X_FORWARDED_PREFIX could be identified within the request -> restricting access."
+        logger.warn(message)
+        return message
+
+    user_requesting = request.headers.get('x-forwarded-preferred-username')
+    if user_requesting is None:
+        user_requesting = request.headers.get('x-forwarded-preferred-username')
+        
+    if user_requesting is None:
+        response.status_code = status.HTTP_403_FORBIDDEN
+        message = "'x-forwarded-preferred-username' could be identified within the request -> restricting access."
+        logger.warn(message)
+        return message
 
     auth_groups = request.headers.get('x-forwarded-groups').split(",")
     if auth_groups is None:
-        logger.info("No x-forwarded-groups could be identified within the request -> restricting access.")
         response.status_code = status.HTTP_403_FORBIDDEN
-        return "x-forwarded-groups not found"
+        message = "No x-forwarded-groups could be identified within the request -> restricting access."
+        logger.warn("No x-forwarded-groups could be identified within the request -> restricting access.")
+        return message
     
     user_roles = []
     for group in auth_groups:
@@ -35,18 +49,22 @@ async def auth_check(request: Request,response: Response):
 
     if prefix_roles_allowed is None:
         response.status_code = status.HTTP_200_OK
-        return f"No role specified for prefix: {requested_prefix}"
+        message = f"No role specified for prefix: {requested_prefix}"
+        logger.warn(message)
+        return message
     
     else:
         for user_role in user_roles:
             if user_role in prefix_roles_allowed:
                 response.status_code = status.HTTP_200_OK
-                return f"User ({user_roles=}) has one of the allowed roles: {user_role} -> access granted"
+                message = f"User {user_requesting}: access granted for: {requested_prefix}"
+                logger.warn(message)
+                return message
         
         response.status_code = status.HTTP_403_FORBIDDEN
-        return f"User ({user_roles=}) has not one of the allowed roles: {prefix_roles_allowed} -> access denied"
-
-
+        message = f"User ({user_roles=}) has not one of the allowed roles: {prefix_roles_allowed} -> access denied"
+        logger.warn(message)
+        return message
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)

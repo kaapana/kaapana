236e964c214ad8d911d0ea4520845103ed6a7f4a
Jonas Scherer <jonas.scherer@dkfz-heidelberg.de>
2023-02-07 17:21:04 +0100

    no-vnc-base image fix

--- templates_and_examples/examples/processing-pipelines/otsus-method/extension/docker/files/dag_otsus_method.py
+++ templates_and_examples/examples/processing-pipelines/otsus-method/extension/docker/files/dag_otsus_method.py
@@ -71,7 +71,7 @@ generate_report = OtsusNotebookOperator(
     input_operator=otsus_method,
     # dev_server='jupyterlab',
     cmds=["/bin/bash"],
-    arguments=["/kaapanasrc/otsus_notebooks/run_otsus_report_notebook.sh"]
+    arguments=["/app/otsus_notebooks/run_otsus_report_notebook.sh"]
 )
 
 put_report_to_minio = LocalMinioOperator(dag=dag,

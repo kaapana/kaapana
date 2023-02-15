6016f7e409934e1e4545fd438ea639905be6b49c
jens <jens.beyermann@dkfz-heidelberg.de>
2023-02-15 14:22:27 +0100

    format update

--- data-processing/kaapana-plugin/processing-containers/itk2dcm/files/convert-itk2dcm.py
+++ data-processing/kaapana-plugin/processing-containers/itk2dcm/files/convert-itk2dcm.py
@@ -304,31 +304,6 @@ class Parser:
         cases = [f for f in Path(path).rglob("*") if (re.match(r'^(?!.*(?:seg|Seg|segmentation|Segmentation)).*$', str(f.name)) and re.search(r"[0-9]*.\.nii(.gz)?", str(f.name)) ) ]
         # segs = [f for f in Path(path).rglob("*") if re.search(r"[cC]ase[0-9]+\_[sS]eg(mentation)?\.nii(\.gz)", str(f.name)) and not re.search(f"_{path}_/[cC]ase\_out", str(f))]
         segs = [f for f in Path(path).rglob("*") if re.search(r"[sS]eg(mentation)?\.nii(\.gz)?", str(f.name))]
-        # cases.sort()
-        # segs.sort()
-        # if kwds.get("log") in ["Info", "Debug"]:
-        #     print("----cases----")
-        #     for x in cases: print(x)
-        #     print("----segs-----")
-        #     for x in segs: print(x)
-
-        # str_segs = [str(s) for s in segs]
-        # cases_with_segs = [c for c in cases if re.search(str(c).split(".")[0], "|".join(str_segs) )]
-        # cases_without_segs = [c for c in cases if c not in cases_with_segs]
-        # if len(cases_with_segs) > 0:
-
-        #     print(f"Found {len(cases_without_segs)} cases without segmentations. Theses cases will be ignored. For more info run script with log='Debug' mode.")
-        #     if kwds.get("log")=='Debug':
-        #         print("----Cases without segmentations:----")
-        #         for c in cases_without_segs:
-        #             print(c)
-        #         print("----")
-        
-        # cases_with_segs.sort()
-        # segs.sort()
-
-        # res = list(zip(cases_with_segs, segs))
-        # res.extend([(c, None) for c in cases_without_segs])
 
         return self.zip_cases_with_segs(cases, segs)
 

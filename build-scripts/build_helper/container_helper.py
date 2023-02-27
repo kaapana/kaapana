9e2cd54ce19d36b2ecbecdea56d7b94240dcea14
Jonas Scherer <jonas.scherer@dkfz-heidelberg.de>
2023-02-27 17:42:25 +0100

    image stats working

--- build-scripts/build_helper/container_helper.py
+++ build-scripts/build_helper/container_helper.py
@@ -74,29 +74,33 @@ def convert_size(size_string):
         pass
 
 
-def get_image_stats():
+def get_image_stats(version):
     images_stats = {}
-    command = [
-        f"{Container.container_engine} system df -v | grep {BuildUtils.platform_build_version} | awk -F ' ' '{{print $1,$2,$6,$7,$8,$9}}'"
-    ]
+    command = [f"{Container.container_engine} system df -v | grep {version}"]
     output = run(command, shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True, timeout=5)
     if output.returncode == 0:
         system_df_output = output.stdout.split("\n")
         for image_stats in system_df_output:
             if len(image_stats) == 0:
                 continue
-            image_name, image_tag, size, shared_size, unique_size, containers = image_stats.split(" ")
+            image_name, image_tag, image_hash, image_build_time, size, shared_size, unique_size, containers = [
+                x for x in image_stats.strip().split("  ") if x != ""
+            ]
             size = convert_size(size)
             shared_size = convert_size(shared_size)
             unique_size = convert_size(unique_size)
 
             images_stats[f"{image_name}:{image_tag}"] = {
                 "size": size,
-                "shared_size": shared_size,
                 "unique_size": unique_size,
-                "containers": containers,
+                "shared_size": shared_size,
+                "image_build_time": image_build_time,
+                "containers": int(containers.strip()),
             }
-    images_stats = {k: v for k, v in sorted(images_stats.items(), key=lambda item: item[1]["unique_size"],reverse=True)}
+
+    images_stats = {
+        k: v for k, v in sorted(images_stats.items(), key=lambda item: item[1]["size"], reverse=True)
+    }
     return images_stats
 
 

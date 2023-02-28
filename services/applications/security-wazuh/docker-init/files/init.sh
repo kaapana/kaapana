7b0ca119c37cbe3bfeb9d738069749abb4d884c8
Jonas Reinwald <jonas.reinwald@dkfz-heidelberg.de>
2023-02-28 17:22:14 +0100

    add wazuh agent config and change some values in general wazuh config

--- services/applications/security-wazuh/docker-init/files/init.sh
+++ services/applications/security-wazuh/docker-init/files/init.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# remove 
-
+# remove old config
 if [ -d "./config" ]; then
     rm -rf ./config
 fi
@@ -18,13 +17,30 @@ API_PW=`cat /wazuh_api_secret/password`
 sed -i "s/username: wazuh-wui/username: $API_USER/" ./config/wazuh_dashboard/wazuh.yml
 sed -i "s/password: \"MyS3cr37P450r.*-\"/password: \"$API_PW\"/" ./config/wazuh_dashboard/wazuh.yml
 
+# enable vulnerability detection
+sed -i -z "s/<vulnerability-detector>\(\s*\)<enabled>no/<vulnerability-detector>\1<enabled>yes/g" ./config/wazuh_cluster/wazuh_manager.conf
+#perl -i -pe 'BEGIN{undef $/;} s/<vulnerability-detector>\(.*\)<enabled>no/<vulnerability-detector>\1<enabled>yes/smg' ./config/wazuh_cluster/wazuh_manager.conf
+sed -i -z "s/<provider name=\"canonical\">\(\s*\)<enabled>no/<provider name=\"canonical\">\1<enabled>yes/g" ./config/wazuh_cluster/wazuh_manager.conf
+#perl -i -pe 'BEGIN{undef $/;} s/<provider name=\"canonical\">\(.*\)<enabled>no/<provider name=\"canonical\">\1<enabled>yes/smg' ./config/wazuh_cluster/wazuh_manager.conf
+sed -i -z "s/<provider name=\"redhat\">\(\s*\)<enabled>no/<provider name=\"redhat\">\1<enabled>yes/g" ./config/wazuh_cluster/wazuh_manager.conf
+#perl -i -pe 'BEGIN{undef $/;} s/<provider name=\"redhat\">\(.*\)<enabled>no/<provider name=\"redhat\">\1<enabled>yes/smg' ./config/wazuh_cluster/wazuh_manager.conf
+
+# set sca interval to 30min
+#perl -i -pe 'BEGIN{undef $/;} s/<sca>\(.*\)<interval>12h/<sca>\1<interval>30m/smg' ./config/wazuh_cluster/wazuh_manager.conf
+sed -i -z "s/<sca>\(.*\)<interval>12h/<sca>\1<interval>30m/g" ./config/wazuh_cluster/wazuh_manager.conf
+
 mkdir ./config/wazuh_indexer_ssl_certs
 mv /tmp-certificates/* ./config/wazuh_indexer_ssl_certs/
 
 # without write permissions for other users the wazuh-indexer container fails to start
-mkdir ./wazuh-indexer-data
+mkdir ./wazuh-indexer-data >/dev/null 2>&1
 chmod -R 777 ./wazuh-indexer-data
 
 # make the randomly generated api credentials available to security-pages
 cp /wazuh_api_secret/username /api_credentials/wazuh_username
 cp /wazuh_api_secret/password /api_credentials/wazuh_password
+
+# add shared agent config
+mkdir -p /wazuh-agent-config/shared/default/
+chmod -R 777 /wazuh-agent-config
+cp /shared_agent_config.xml /wazuh-agent-config/shared/default/agent.conf

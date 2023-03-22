c78a68077e9efd02cde1448e9bd70f11eda79c35
Kaushal Parekh <kaushal.parekh@dkfz-heidelberg.de>
2023-03-22 15:21:04 +0100

    Fix VM folder and add experiment name to results file

--- data-processing/processing-pipelines/tfda-execution-orchestrator/extension/docker/files/tfda_execution_orchestrator/algorithm_files/shell_workflow/test_shell_workflow/main.sh
+++ data-processing/processing-pipelines/tfda-execution-orchestrator/extension/docker/files/tfda_execution_orchestrator/algorithm_files/shell_workflow/test_shell_workflow/main.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 echo "Hello World"
-results_folder='/home/ubuntu/results'
-dir='/home/ubuntu/test-site-data'
+
+### Parsing command line arguments:
+usage="$(basename "$0")
+
+_Argument: --input [Data directory on the isolated VM]
+_Argument: --output [Results directory on the isolated VM]"
+
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+
+        -i|--input)
+            dir="$2"
+            echo -e "Path to data $dir";
+            shift # past argument
+            shift # past value
+        ;;
+
+        -p|--output)
+            results_folder="$2"
+            echo -e "Results path $results_folder";
+            shift # past argument
+            shift # past value
+        ;;
+
+    esac
+done
+
 
 if [ -z "$(ls -A $dir)" ]; then
   echo "Data bucket is empty" >> "$results_folder"/out.txt

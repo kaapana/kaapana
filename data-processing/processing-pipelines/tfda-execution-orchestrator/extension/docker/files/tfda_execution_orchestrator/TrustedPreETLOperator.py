a19559bf9ffa89a9860b40efc22ddf30b4a4f351
Kaushal Parekh <kaushal.parekh@dkfz-heidelberg.de>
2023-04-13 17:12:50 +0200

    Fix issues with proc container for managing iso instances

---
+++ data-processing/processing-pipelines/tfda-execution-orchestrator/extension/docker/files/tfda_execution_orchestrator/TrustedPreETLOperator.py
@@ -0,0 +1,39 @@
+import os
+import logging
+import requests
+import subprocess
+from subprocess import PIPE
+from pathlib import Path
+from airflow.exceptions import AirflowFailException
+from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator
+from kaapana.blueprints.kaapana_global_variables import DEFAULT_REGISTRY, KAAPANA_BUILD_VERSION
+from datetime import timedelta
+
+
+class TrustedPreETLOperator(KaapanaBaseOperator):
+    execution_timeout = timedelta(hours=10)
+
+    def __init__(self,
+                 dag,
+                 instanceState = "present",
+                 name = "create-iso-inst",
+                 env_vars={},
+                 execution_timeout=execution_timeout,
+                 **kwargs):
+        
+        envs = {
+
+        }
+        env_vars.update(envs)
+
+        super().__init__(
+            dag=dag,
+            image=f"{DEFAULT_REGISTRY}/pre-and-post-etl:{KAAPANA_BUILD_VERSION}",
+            name=name,
+            python_callable=self.start,
+            image_pull_secrets=["registry-secret"],
+            execution_timeout=execution_timeout,
+            env_vars=env_vars,
+            **kwargs
+        )
+        self.instanceState = instanceState

b0f8642dfa036a857708dcdf817d6098afc467f8
Stefan Denner <stefan.denner@dkfz-heidelberg.de>
2023-04-21 18:01:04 +0200

    Gallery View: introduced toggle to disable tag bar, once in selection mode

--- services/utils/base-landing-page/files/kaapana_app/src/store/modules/datasets.module.ts
+++ services/utils/base-landing-page/files/kaapana_app/src/store/modules/datasets.module.ts
@@ -1,16 +1,26 @@
 const defaults = {
   activeTags: [],
+  multiSelectKeyPressed: false,
+  selectedItems: [],
 };
 
 const state = Object.assign({}, defaults);
 
 const getters = {
-  activeTags: (state: any)  =>  state.activeTags
+  activeTags: (state: any) => state.activeTags,
+  multiSelectKeyPressed: (state: any) => state.multiSelectKeyPressed,
+  selectedItems: (state: any) => state.selectedItems,
 };
 
 const mutations = {
   setActiveTags(state: any, activeTags: any) {
     state.activeTags = activeTags;
+  },
+  setMultiSelectKeyPressed(state: any, multiSelectKeyPressed: any) {
+    state.multiSelectKeyPressed = multiSelectKeyPressed;
+  },
+  setSelectedItems(state: any, selectedItems: any) {
+    state.selectedItems = selectedItems;
   },
 };
 

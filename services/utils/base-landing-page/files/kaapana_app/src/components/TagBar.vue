df82d81f9696abfc4ec8bc43832c4dbf360fc916
Stefan Denner <stefan.denner@dkfz-heidelberg.de>
2023-04-21 14:22:13 +0200

    Gallery view: Rearrange of the Search UI.

--- services/utils/base-landing-page/files/kaapana_app/src/components/TagBar.vue
+++ services/utils/base-landing-page/files/kaapana_app/src/components/TagBar.vue
@@ -1,37 +1,44 @@
 <template>
   <!--  edit Mode-->
-  <v-row v-if="editMode" dense align="center" style="padding-top: 5px; padding-bottom: 5px">
+  <v-row v-if="editMode" dense align="center">
     <v-col cols="1" align="center">
       <v-icon>mdi-tag-outline</v-icon>
     </v-col>
-    <v-col cols="9">
+    <v-col cols="8" style="padding-top: 8px; padding-bottom: 11px">
       <v-combobox
-          label="Tags"
-          v-model="tags"
-          :items="availableTags"
-          chips
-          clearable
-          multiple
-          single-line
-          append-icon=""
-          deletable-chips
-          flat
-          small-chips
-          hide-details
-          dense
+        label="Tags"
+        v-model="tags"
+        :items="availableTags"
+        chips
+        clearable
+        multiple
+        single-line
+        append-icon=""
+        deletable-chips
+        flat
+        small-chips
+        hide-details
+        dense
       >
       </v-combobox>
     </v-col>
-    <v-col cols="1" align="center">
-      <v-checkbox
-          v-model="multiple"
-          label="Multiple"
-          dense
-          hide-details
-      ></v-checkbox>
+    <v-col cols="2" align="center" justify="center">
+      <v-switch
+        v-model="multiple"
+        label="Multiple"
+        dense
+        small
+        hide-details
+        style="margin-top:0"
+      ></v-switch>
     </v-col>
     <v-col cols="1" align="center">
-      <v-btn @click="editMode = !editMode" small icon :disabled="tags.length === 0">
+      <v-btn
+        @click="editMode = !editMode"
+        small
+        icon
+        :disabled="tags.length === 0"
+      >
         <v-icon>mdi-content-save</v-icon>
       </v-btn>
     </v-col>
@@ -43,13 +50,18 @@
     </v-col>
     <v-col cols="10" align="center">
       <v-chip-group
-          v-model="selection"
-          active-class="deep-purple--text text--accent-4"
-          :multiple="this.multiple"
-          @change="onChangeSelection"
-          dense
+        v-model="selection"
+        active-class="deep-purple--text text--accent-4"
+        :multiple="this.multiple"
+        @change="onChangeSelection"
+        dense
       >
-        <v-chip v-for="tag in tags" :key="tag" small :disabled="!settings.datasets.cardText">
+        <v-chip
+          v-for="tag in tags"
+          :key="tag"
+          small
+          :disabled="!settings.datasets.cardText"
+        >
           {{ tag }}
         </v-chip>
       </v-chip-group>
@@ -65,10 +77,8 @@
 <script>
 /* eslint-disable */
 
-
-import {loadValues} from "@/common/api.service";
-import {settings} from "@/static/defaultUIConfig";
-
+import { loadValues } from "@/common/api.service";
+import { settings } from "@/static/defaultUIConfig";
 
 export default {
   data() {
@@ -78,70 +88,83 @@ export default {
       availableTags: [],
       multiple: settings.datasets.tagBar.multiple,
       tags: settings.datasets.tagBar.tags,
-      settings: settings
+      settings: settings,
     };
   },
-  emits: ['selectedTags'],
+  emits: ["selectedTags"],
   mounted() {
-    this.settings = JSON.parse(localStorage['settings'])
-    this.multiple = this.settings.datasets.tagBar.multiple
-    this.tags = this.settings.datasets.tagBar.tags
+    this.settings = JSON.parse(localStorage["settings"]);
+    this.multiple = this.settings.datasets.tagBar.multiple;
+    this.tags = this.settings.datasets.tagBar.tags;
 
-    if (this.settings.datasets.tagBar.tags.length > 0)
-      this.editMode = false
+    if (this.settings.datasets.tagBar.tags.length > 0) this.editMode = false;
 
-    loadValues({}, 'Tags')
-        .then(res => this.availableTags = ('items' in res.data ? res.data['items'].map(i => i['value']) : []))
+    loadValues({}, "Tags").then(
+      (res) =>
+        (this.availableTags =
+          "items" in res.data ? res.data["items"].map((i) => i["value"]) : [])
+    );
 
-    window.addEventListener("keypress", event => this.keypressListener(event));
+    window.addEventListener("keypress", (event) =>
+      this.keypressListener(event)
+    );
   },
   beforeDestroy() {
-    window.removeEventListener('keypress', event => this.keypressListener(event))
+    window.removeEventListener("keypress", (event) =>
+      this.keypressListener(event)
+    );
   },
   methods: {
     onChangeSelection(e) {
       if (this.multiple) {
-        this.$emit('selectedTags', this.selection.map(i => this.tags[i]) || [])
+        this.$emit(
+          "selectedTags",
+          this.selection.map((i) => this.tags[i]) || []
+        );
       } else {
-        this.$emit('selectedTags', this.selection !== undefined ? [this.tags[this.selection]] : [])
+        this.$emit(
+          "selectedTags",
+          this.selection !== undefined ? [this.tags[this.selection]] : []
+        );
       }
     },
     keypressListener(e) {
-      const keyCode = String.fromCharCode(e.keyCode)
-      if (this.editMode || !Number.isInteger(Number(keyCode)))
-        return
-      const n = parseInt(keyCode) - 1
+      const keyCode = String.fromCharCode(e.keyCode);
+      if (this.editMode || !Number.isInteger(Number(keyCode))) return;
+      const n = parseInt(keyCode) - 1;
       if (n >= 0 && n < this.tags.length) {
         if (this.multiple) {
-          if (this.selection.filter(t => t === n).length > 0) {
+          if (this.selection.filter((t) => t === n).length > 0) {
             // already selected
-            this.selection = this.selection.filter(t => t !== n)
+            this.selection = this.selection.filter((t) => t !== n);
           } else {
             // not selected yet -> add to selection
-            this.selection.push(n)
+            this.selection.push(n);
           }
-          this.$emit('selectedTags', this.selection.map(i => this.tags[i]))
+          this.$emit(
+            "selectedTags",
+            this.selection.map((i) => this.tags[i])
+          );
         } else {
-          this.selection = n
-          this.$emit('selectedTags', [this.tags[this.selection]])
+          this.selection = n;
+          this.$emit("selectedTags", [this.tags[this.selection]]);
         }
       }
-    }
+    },
   },
   watch: {
     multiple() {
-      this.selection = this.multiple ? [] : null
-      const settings = JSON.parse(localStorage['settings'])
-      settings.datasets.tagBar.multiple = this.multiple
-      localStorage['settings'] = JSON.stringify(settings)
+      this.selection = this.multiple ? [] : null;
+      const settings = JSON.parse(localStorage["settings"]);
+      settings.datasets.tagBar.multiple = this.multiple;
+      localStorage["settings"] = JSON.stringify(settings);
     },
     tags() {
-      const settings = JSON.parse(localStorage['settings'])
-      settings.datasets.tagBar.tags = this.tags
-      localStorage['settings'] = JSON.stringify(settings)
-    }
-  }
+      const settings = JSON.parse(localStorage["settings"]);
+      settings.datasets.tagBar.tags = this.tags;
+      localStorage["settings"] = JSON.stringify(settings);
+    },
+  },
 };
 </script>
-<style scoped>
-</style>
+<style scoped></style>

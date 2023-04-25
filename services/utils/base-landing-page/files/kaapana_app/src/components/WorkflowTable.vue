c53263108479fa968954755dcc4ff4284019960c
Markus Bujotzek <markus.bujotzek@dkfz-heidelberg.de>
2023-04-25 10:57:34 +0200

    redirect to airflow from WorkflowTable

--- services/utils/base-landing-page/files/kaapana_app/src/components/WorkflowTable.vue
+++ services/utils/base-landing-page/files/kaapana_app/src/components/WorkflowTable.vue
@@ -7,8 +7,18 @@
       <v-col align="right">
         <v-tooltip bottom>
           <template v-slot:activator="{ on, attrs }">
-            <v-btn v-on="on" @click='refreshClient()' small icon>
-              <v-icon color="primary" large class="mx-2" dark>mdi-refresh</v-icon> 
+            <v-btn class="pa-6" v-on="on" @click='redirectToAirflow()' small icon>
+              <v-icon color="primary" large dark>
+                mdi-chart-timeline-variant
+              </v-icon> 
+            </v-btn> 
+          </template>
+          <span>redirect to Airflow worklfow engine</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn class="pa-6" v-on="on" @click='refreshClient()' small icon>
+              <v-icon color="primary" large  dark>mdi-refresh</v-icon> 
             </v-btn> 
           </template>
           <span>refresh workflow list</span>
@@ -156,11 +166,13 @@ data () {
     hover: false,
     activateAddRemote: false,
     shouldExpand: true,
+    localInstance: {},
   }
 },
 
 mounted () {
   this.refreshClient();
+  this.getLocalInstance();
 },
 
 props: {
@@ -217,6 +229,10 @@ methods: {
       count: states.filter(_state => _state === state).length
     }))
   },
+  redirectToAirflow() {
+    const airflow_url = this.localInstance.protocol + "://" + this.localInstance.host + "/flow/home"
+    window.open(airflow_url, "_blank", "noreferrer")
+  },
   startWorkflowManually(item) {
     this.shouldExpand = false
     this.manual_startID = item.workflow_id,
@@ -243,24 +259,35 @@ methods: {
   },
 
   // API Calls
+  getLocalInstance() {
+    kaapanaApiService
+      .federatedClientApiGet("/kaapana-instance")
+      .then((response) => {
+        this.localInstance = response.data;
+        console.log("this.localInstance: ", this.localInstance)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
   getJobsOfWorkflow(workflow_name) {
-      kaapanaApiService
-        .federatedClientApiGet("/jobs",{
-          workflow_name: workflow_name,
-          limit: 100,
-        }).then((response) => {
-          if (this.expanded.length > 0) {
-            this.jobsofExpandedWorkflow = response.data;
-          } else {
-            this.jobsofWorkflows = response.data;
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        })
+    kaapanaApiService
+      .federatedClientApiGet("/jobs",{
+        workflow_name: workflow_name,
+        limit: 100,
+      }).then((response) => {
+        if (this.expanded.length > 0) {
+          this.jobsofExpandedWorkflow = response.data;
+        } else {
+          this.jobsofWorkflows = response.data;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   },
   deleteClientWorkflowAPI(workflow_id) {
-      kaapanaApiService
+    kaapanaApiService
       .federatedClientApiDelete("/workflow",{
           workflow_id,
       }).then((response) => {

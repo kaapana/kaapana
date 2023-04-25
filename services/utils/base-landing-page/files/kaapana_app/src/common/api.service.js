26748ddec8d88ac2260de621756f353ef7eaced0
Stefan Denner <stefan.denner@dkfz-heidelberg.de>
2023-04-25 14:55:39 +0200

    Gallery View: - Code cleanup - improved error handling - removed option to delete series from platform via the gallery view

--- services/utils/base-landing-page/files/kaapana_app/src/common/api.service.js
+++ services/utils/base-landing-page/files/kaapana_app/src/common/api.service.js
@@ -1,145 +1,188 @@
 /* eslint-disable */
 
-
 import Vue from "vue";
 import httpClient from "./httpClient";
 
-const WADO_ENDPOINT = process.env.VUE_APP_WADO_ENDPOINT
-const KAAPANA_BACKEND_ENDPOINT = process.env.VUE_APP_KAAPANA_BACKEND_ENDPOINT
-
-const deleteSeriesFromPlatform = async (seriesInstanceUIDs, dag_id = 'delete-series-from-platform') => {
-    return await httpClient.post(
-        KAAPANA_BACKEND_ENDPOINT + 'client/job',
-        {
-            "dag_id": dag_id,
-            "conf_data": {
-                "data_form": {
-                    "identifiers": seriesInstanceUIDs
-                },
-                "form_data": {
-                    "delete_complete_study": false,
-                    "single_execution": false
-                }
-            },
-            "kaapana_instance_id": 1 // TODO: this should rather be set by the backend
-        }
-    )
-}
+const KAAPANA_BACKEND_ENDPOINT = process.env.VUE_APP_KAAPANA_BACKEND_ENDPOINT;
 
 const updateDataset = async (body) => {
-    return await httpClient.put(KAAPANA_BACKEND_ENDPOINT + 'client/dataset', body)
-}
+  return await httpClient.put(
+    KAAPANA_BACKEND_ENDPOINT + "client/dataset",
+    body
+  );
+};
 
 const createDataset = async (body) => {
-    return await httpClient.post(KAAPANA_BACKEND_ENDPOINT + 'client/dataset', body)
-}
+  return await httpClient.post(
+    KAAPANA_BACKEND_ENDPOINT + "client/dataset",
+    body
+  );
+};
 const loadDatasets = async () => {
-    try {
-        // TODO
-        const datasets = (
-            await httpClient.get(KAAPANA_BACKEND_ENDPOINT + 'client/datasets')
-        )
-        return datasets.data
-    } catch (error) {
-        Vue.notify({title: 'Network/Server error', text: error, type: 'error'});
-    }
-}
+  try {
+    // TODO
+    const datasets = await httpClient.get(
+      KAAPANA_BACKEND_ENDPOINT + "client/datasets"
+    );
+    return datasets.data;
+  } catch (error) {
+    Vue.notify({
+      title: "Error",
+      text:
+        error.response && error.response.data && error.response.data.detail
+          ? error.response.data.detail
+          : error,
+      type: "error",
+    });
+  }
+};
 
 const loadDatasetByName = async (datasetName) => {
-    try {
-        const dataset = (
-            await httpClient.get(KAAPANA_BACKEND_ENDPOINT + `client/dataset?name=${datasetName}`)
-        ).data
-        return dataset
-    } catch (error) {
-        Vue.notify({title: 'Network/Server error', text: error, type: 'error'});
-    }
-}
+  try {
+    const dataset = (
+      await httpClient.get(
+        KAAPANA_BACKEND_ENDPOINT + `client/dataset?name=${datasetName}`
+      )
+    ).data;
+    return dataset;
+  } catch (error) {
+    Vue.notify({
+      title: "Error",
+      text:
+        error.response && error.response.data && error.response.data.detail
+          ? error.response.data.detail
+          : error,
+      type: "error",
+    });
+  }
+};
 
 const loadDatasetNames = async () => {
-    try {
-        const datasets = (
-            await httpClient.get(KAAPANA_BACKEND_ENDPOINT + 'client/datasets')
-        )
-        return datasets.data.map(dataset => dataset.name)
-    } catch (error) {
-        Vue.notify({title: 'Network/Server error', text: error, type: 'error'});
-    }
-}
-
+  try {
+    const datasets = await httpClient.get(
+      KAAPANA_BACKEND_ENDPOINT + "client/datasets"
+    );
+    return datasets.data.map((dataset) => dataset.name);
+  } catch (error) {
+    Vue.notify({
+      title: "Error",
+      text:
+        error.response && error.response.data && error.response.data.detail
+          ? error.response.data.detail
+          : error,
+      type: "error",
+    });
+  }
+};
 
 const loadSeriesData = async (seriesInstanceUID) => {
-    try {
-        const response = (await httpClient.get(
-            KAAPANA_BACKEND_ENDPOINT + `dataset/series/${seriesInstanceUID}`
-        ))
-        return response.data
-    } catch (error) {
-        Vue.notify({title: 'Network/Server error', text: error, type: 'error'});
-    }
-}
+  try {
+    const response = await httpClient.get(
+      KAAPANA_BACKEND_ENDPOINT + `dataset/series/${seriesInstanceUID}`
+    );
+    return response.data;
+  } catch (error) {
+    Vue.notify({
+      title: "Error",
+      text:
+        error.response && error.response.data && error.response.data.detail
+          ? error.response.data.detail
+          : error,
+      type: "error",
+    });
+    throw error;
+  }
+};
 
 const loadPatients = async (data) => {
-    try {
-        const res = await httpClient.post(KAAPANA_BACKEND_ENDPOINT + 'dataset/series', data)
-        return res.data
-    } catch (error) {
-        Vue.notify({title: 'Network/Server error', text: error.text, type: 'error'});
-        throw error
-    }
-}
+  try {
+    const res = await httpClient.post(
+      KAAPANA_BACKEND_ENDPOINT + "dataset/series",
+      data
+    );
+    return res.data;
+  } catch (error) {
+    Vue.notify({
+      title: "Error",
+      text:
+        error.response && error.response.data && error.response.data.detail
+          ? error.response.data.detail
+          : error,
+      type: "error",
+    });
+    throw error;
+  }
+};
 
 const loadFieldNames = async () => {
-    try {
-        return (
-            await httpClient.get(KAAPANA_BACKEND_ENDPOINT + 'dataset/field_names')
-        )
-    } catch (error) {
-        Vue.notify({title: 'Network/Server error', text: error, type: 'error'});
-    }
-}
+  try {
+    return await httpClient.get(
+      KAAPANA_BACKEND_ENDPOINT + "dataset/field_names"
+    );
+  } catch (error) {
+    Vue.notify({
+      title: "Error",
+      text:
+        error.response && error.response.data && error.response.data.detail
+          ? error.response.data.detail
+          : error,
+      type: "error",
+    });
+    throw error;
+  }
+};
 
 const loadValues = async (query, key) => {
-    try {
-        return (
-            await httpClient.get(KAAPANA_BACKEND_ENDPOINT + `dataset/query_values/${key}?query=${JSON.stringify(query)}`)
-        )
-    } catch (error) {
-        Vue.notify({title: 'Network/Server error', text: error, type: 'error'});
-    }
-}
-
+  try {
+    return await httpClient.get(
+      KAAPANA_BACKEND_ENDPOINT +
+        `dataset/query_values/${key}?query=${JSON.stringify(query)}`
+    );
+  } catch (error) {
+    Vue.notify({
+      title: "Error",
+      text:
+        error.response && error.response.data && error.response.data.detail
+          ? error.response.data.detail
+          : error,
+      type: "error",
+    });
+  }
+};
 
 const updateTags = async (data) => {
-    const response = await httpClient.post(KAAPANA_BACKEND_ENDPOINT + 'dataset/tag', data)
-    // TODO: ideally this should return the new tags which are then assigned
-}
+  const response = await httpClient.post(
+    KAAPANA_BACKEND_ENDPOINT + "dataset/tag",
+    data
+  );
+  // TODO: ideally this should return the new tags which are then assigned
+};
 
 const loadDashboard = async (seriesInstanceUIDs, fields) => {
-    return (await httpClient.post(KAAPANA_BACKEND_ENDPOINT + 'dataset/dashboard' ,{
-        series_instance_uids: seriesInstanceUIDs,
-        names: fields
-    })).data
-
-}
+  return (
+    await httpClient.post(KAAPANA_BACKEND_ENDPOINT + "dataset/dashboard", {
+      series_instance_uids: seriesInstanceUIDs,
+      names: fields,
+    })
+  ).data;
+};
 
 const loadDicomTagMapping = async () => {
-    return (await httpClient.get(KAAPANA_BACKEND_ENDPOINT + 'dataset/fields')).data
-}
-
+  return (await httpClient.get(KAAPANA_BACKEND_ENDPOINT + "dataset/fields"))
+    .data;
+};
 
 export {
-    updateTags,    
-    loadPatients,
-    loadSeriesData,
-    createDataset,
-    loadDatasets,
-    deleteSeriesFromPlatform,
-    updateDataset,
-    loadDatasetNames,
-    loadDatasetByName,
-    loadDashboard,
-    loadDicomTagMapping,
-    loadFieldNames,
-    loadValues
-}
+  updateTags,
+  loadPatients,
+  loadSeriesData,
+  createDataset,
+  loadDatasets,
+  updateDataset,
+  loadDatasetNames,
+  loadDatasetByName,
+  loadDashboard,
+  loadDicomTagMapping,
+  loadFieldNames,
+  loadValues,
+};

12820f7524b6976e45a038a0bd6de696c3951819
Stefan Denner <stefan.denner@dkfz-heidelberg.de>
2023-04-25 14:55:39 +0200

    Gallery View: - Code cleanup - improved error handling - removed option to delete series from platform via the gallery view

--- services/utils/base-landing-page/files/kaapana_app/src/common/dicomWebClient.js
+++
@@ -1,8 +0,0 @@
-import * as DICOMwebClient from "dicomweb-client";
-
-const dicomWebClient = new DICOMwebClient.api.DICOMwebClient({
-  url: process.env.VUE_APP_RS_ENDPOINT
-})
-
-
-export default dicomWebClient;

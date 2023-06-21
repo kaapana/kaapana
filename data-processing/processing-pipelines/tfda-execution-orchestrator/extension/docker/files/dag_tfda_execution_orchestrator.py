9b1836d6453d9873dbda2b61cd7225212d60b355
Kaushal Parekh <kaushal.parekh@dkfz-heidelberg.de>
2023-06-21 15:28:22 +0200

    Format with Black

--- data-processing/processing-pipelines/tfda-execution-orchestrator/extension/docker/files/dag_tfda_execution_orchestrator.py
+++ data-processing/processing-pipelines/tfda-execution-orchestrator/extension/docker/files/dag_tfda_execution_orchestrator.py
@@ -2,11 +2,16 @@ from airflow.utils.log.logging_mixin import LoggingMixin
 from airflow.utils.dates import days_ago
 from datetime import timedelta
 from airflow.models import DAG
-from tfda_execution_orchestrator.LocalLoadPlatformConfigOperator import LocalLoadPlatformConfigOperator
-from tfda_execution_orchestrator.ManageIsoInstanceOperator import ManageIsoInstanceOperator
+from tfda_execution_orchestrator.LocalLoadPlatformConfigOperator import (
+    LocalLoadPlatformConfigOperator,
+)
+from tfda_execution_orchestrator.ManageIsoInstanceOperator import (
+    ManageIsoInstanceOperator,
+)
 from tfda_execution_orchestrator.TrustedPreETLOperator import TrustedPreETLOperator
 from tfda_execution_orchestrator.CopyDataAndAlgoOperator import CopyDataAndAlgoOperator
 from tfda_execution_orchestrator.RunAlgoOperator import RunAlgoOperator
+
 # from tfda_execution_orchestrator.LocalTFDAPrepareEnvOperator import LocalTFDAPrepareEnvOperator
 from tfda_execution_orchestrator.FetchResultsOperator import FetchResultsOperator
 from tfda_execution_orchestrator.TrustedPostETLOperator import TrustedPostETLOperator
@@ -36,30 +41,69 @@ dag = DAG(
     schedule_interval=None,
 )
 
-load_platform_config = LocalLoadPlatformConfigOperator(dag=dag, platform_config_file="platform_config.json")
-create_iso_env = ManageIsoInstanceOperator(dag=dag, instanceState="present", taskName="create-iso-inst")
+load_platform_config = LocalLoadPlatformConfigOperator(
+    dag=dag, platform_config_file="platform_config.json"
+)
+create_iso_env = ManageIsoInstanceOperator(
+    dag=dag, instanceState="present", taskName="create-iso-inst"
+)
 # prepare_env = LocalTFDAPrepareEnvOperator(dag=dag)
 trusted_pre_etl = TrustedPreETLOperator(dag=dag)
-get_minio_bucket = LocalMinioOperator(action='get', dag=dag, name="get-data-bucket", local_root_dir="{run_dir}/user-selected-data", operator_out_dir="user-selected-data")
+get_minio_bucket = LocalMinioOperator(
+    action="get",
+    dag=dag,
+    name="get-data-bucket",
+    local_root_dir="{run_dir}/user-selected-data",
+    operator_out_dir="user-selected-data",
+)
 copy_data_algo = CopyDataAndAlgoOperator(dag=dag, input_operator=get_minio_bucket)
 run_isolated_workflow = RunAlgoOperator(dag=dag)
 fetch_results = FetchResultsOperator(dag=dag, operator_out_dir="results")
 trusted_post_etl = TrustedPostETLOperator(dag=dag)
-upload_results = LocalMinioOperator(action='put', dag=dag, name="upload-results", bucket_name="results", action_operators=[fetch_results], zip_files=False)
-delete_iso_inst = ManageIsoInstanceOperator(dag=dag, trigger_rule="all_done", instanceState="absent", taskName="delete-iso-inst")
-clean = LocalWorkflowCleanerOperator(dag=dag, clean_workflow_dir=True, trigger_rule="all_done")
+upload_results = LocalMinioOperator(
+    action="put",
+    dag=dag,
+    name="upload-results",
+    bucket_name="results",
+    action_operators=[fetch_results],
+    zip_files=False,
+)
+delete_iso_inst = ManageIsoInstanceOperator(
+    dag=dag, trigger_rule="all_done", instanceState="absent", taskName="delete-iso-inst"
+)
+clean = LocalWorkflowCleanerOperator(
+    dag=dag, clean_workflow_dir=True, trigger_rule="all_done"
+)
+
 
 def final_status(**kwargs):
-    for task_instance in kwargs['dag_run'].get_task_instances():
-        if task_instance.current_state() != "success" and \
-                task_instance.task_id != kwargs['task_instance'].task_id:
-            raise Exception(f"Task {task_instance.task_id} failed! Failing this DAG run...")
+    for task_instance in kwargs["dag_run"].get_task_instances():
+        if (
+            task_instance.current_state() != "success"
+            and task_instance.task_id != kwargs["task_instance"].task_id
+        ):
+            raise Exception(
+                f"Task {task_instance.task_id} failed! Failing this DAG run..."
+            )
+
 
 final_status = PythonOperator(
-    task_id='final_status',
+    task_id="final_status",
     provide_context=True,
     python_callable=final_status,
-    trigger_rule="all_done", # Ensures this task runs even if upstream fails
+    trigger_rule="all_done",  # Ensures this task runs even if upstream fails
     dag=dag,
 )
-load_platform_config >> create_iso_env >> trusted_pre_etl >> get_minio_bucket >> copy_data_algo >> run_isolated_workflow >> fetch_results >> trusted_post_etl >> delete_iso_inst >> clean >> final_status
+(
+    load_platform_config
+    >> create_iso_env
+    >> trusted_pre_etl
+    >> get_minio_bucket
+    >> copy_data_algo
+    >> run_isolated_workflow
+    >> fetch_results
+    >> trusted_post_etl
+    >> delete_iso_inst
+    >> clean
+    >> final_status
+)
--- data-processing/processing-pipelines/tfda-execution-orchestrator/extension/docker/files/tfda_execution_orchestrator/LocalTrustedPreETLOperator.py
+++ data-processing/processing-pipelines/tfda-execution-orchestrator/extension/docker/files/tfda_execution_orchestrator/LocalTrustedPreETLOperator.py
@@ -22,60 +22,92 @@ class LocalTrustedPreETLOperator(KaapanaPythonBaseOperator):
 
     def algo_pre_etl(self, config_params):
         operator_dir = os.path.dirname(os.path.abspath(__file__))
-        user_experiment_path = os.path.join(operator_dir, "algorithm_files", config_params["workflow_type"], config_params["experiment_name"])
+        user_experiment_path = os.path.join(
+            operator_dir,
+            "algorithm_files",
+            config_params["workflow_type"],
+            config_params["experiment_name"],
+        )
         Path(user_experiment_path).mkdir(parents=True, exist_ok=True)
-        if config_params["workflow_type"] == "shell_workflow": 
-            logging.debug("Downloading archive from provided URL for shell-workflow...")           
+        if config_params["workflow_type"] == "shell_workflow":
+            logging.debug("Downloading archive from provided URL for shell-workflow...")
             download_url = config_params["download_url"]
             try:
                 response = requests.get(download_url)
                 response.raise_for_status()
-                with open(os.path.join(user_experiment_path, f"{config_params['experiment_name']}.zip"), "wb") as file:
+                with open(
+                    os.path.join(
+                        user_experiment_path, f"{config_params['experiment_name']}.zip"
+                    ),
+                    "wb",
+                ) as file:
                     file.write(response.content)
             except requests.exceptions.RequestException as e:
                 print(f"An error occurred while downloading the file: {e}")
         elif config_params["workflow_type"] == "container_workflow":
-            logging.debug("Downloading container from registry since container workflow is requested...")
-            container_registry_url = config_params['container_registry_url']
-            container_registry_user = config_params['container_registry_user']
-            container_registry_pwd = config_params['container_registry_pwd']
-            container_name_version = config_params['container_name_version']
+            logging.debug(
+                "Downloading container from registry since container workflow is requested..."
+            )
+            container_registry_url = config_params["container_registry_url"]
+            container_registry_user = config_params["container_registry_user"]
+            container_registry_pwd = config_params["container_registry_pwd"]
+            container_name_version = config_params["container_name_version"]
             logging.info("Logging into container registry...")
-            command = ["skopeo", "login", "--username", f"{container_registry_user}", "--password", f"{container_registry_pwd}", f"{container_registry_url}"]
+            command = [
+                "skopeo",
+                "login",
+                "--username",
+                f"{container_registry_user}",
+                "--password",
+                f"{container_registry_pwd}",
+                f"{container_registry_url}",
+            ]
             return_code = self.run_command(command=command)
             if return_code == 0:
                 logging.info(f"Login to the registry successful!!")
             else:
-                raise AirflowFailException("Login to the registry FAILED! Cannot proceed further...")
+                raise AirflowFailException(
+                    "Login to the registry FAILED! Cannot proceed further..."
+                )
 
             logging.debug(f"Pulling container: {container_name_version}...")
-            tarball_file = os.path.join(user_experiment_path, f"{config_params['experiment_name']}.tar")
+            tarball_file = os.path.join(
+                user_experiment_path, f"{config_params['experiment_name']}.tar"
+            )
             if os.path.exists(tarball_file):
-                logging.debug(f"Submission tarball already exists locally... deleting it now to pull latest!!")
+                logging.debug(
+                    f"Submission tarball already exists locally... deleting it now to pull latest!!"
+                )
                 os.remove(tarball_file)
             """Due to absence of /etc/containers/policy.json in Airflow container, following Skopeo command only works with "--insecure-policy" """
-            command2 = ["skopeo", "--insecure-policy", "copy", f"docker://{container_registry_url}/{container_name_version}", f"docker-archive:{tarball_file}", "--additional-tag", f"{container_name_version}"]
+            command2 = [
+                "skopeo",
+                "--insecure-policy",
+                "copy",
+                f"docker://{container_registry_url}/{container_name_version}",
+                f"docker-archive:{tarball_file}",
+                "--additional-tag",
+                f"{container_name_version}",
+            ]
             return_code2 = self.run_command(command=command2)
             if return_code2 != 0:
-                raise AirflowFailException(f"Error while trying to download container! Exiting...")        
+                raise AirflowFailException(
+                    f"Error while trying to download container! Exiting..."
+                )
         else:
-            raise AirflowFailException(f"Workflow type {config_params['request_type']} not supported yet! Exiting...")
+            raise AirflowFailException(
+                f"Workflow type {config_params['request_type']} not supported yet! Exiting..."
+            )
 
-    
     def start(self, ds, ti, **kwargs):
-        logging.info("Prepare data and algorithm before being loaded into the isolated environment...")
+        logging.info(
+            "Prepare data and algorithm before being loaded into the isolated environment..."
+        )
         conf = kwargs["dag_run"].conf
         ## Prepare algorithm files
         self.algo_pre_etl(conf)
 
-
-    def __init__(self,
-                 dag,
-                 **kwargs):
-
+    def __init__(self, dag, **kwargs):
         super().__init__(
-            dag=dag,
-            name="trusted-pre-etl",
-            python_callable=self.start,
-            **kwargs
+            dag=dag, name="trusted-pre-etl", python_callable=self.start, **kwargs
         )

62630e63b1acae4ff6360e194bac5c9a63b0e01c
Kaushal Parekh <kaushal.parekh@dkfz-heidelberg.de>
2023-07-21 12:13:05 +0200

    Remove config params and operators that aren't necessary

--- data-processing/processing-pipelines/tfda-spe-orchestrator/extension/docker/files/dag_tfda_spe_orchestrator.py
+++ data-processing/processing-pipelines/tfda-spe-orchestrator/extension/docker/files/dag_tfda_spe_orchestrator.py
@@ -1,4 +1,3 @@
-from airflow.utils.log.logging_mixin import LoggingMixin
 from airflow.utils.dates import days_ago
 from datetime import timedelta
 from airflow.models import DAG
@@ -15,13 +14,6 @@ from tfda_spe_orchestrator.FetchResultsOperator import FetchResultsOperator
 from tfda_spe_orchestrator.TrustedPostETLOperator import TrustedPostETLOperator
 from kaapana.operators.LocalWorkflowCleanerOperator import LocalWorkflowCleanerOperator
 from kaapana.operators.LocalMinioOperator import LocalMinioOperator
-from airflow.operators.python_operator import PythonOperator
-
-log = LoggingMixin().log
-
-ae_title = "NONE"
-pacs_host = ""
-pacs_port = 11112
 
 args = {
     "ui_visible": False,
@@ -69,27 +61,7 @@ delete_iso_inst = ManageIsoInstanceOperator(
     dag=dag, trigger_rule="all_done", instanceState="absent", taskName="delete-iso-inst"
 )
 clean = LocalWorkflowCleanerOperator(
-    dag=dag, clean_workflow_dir=True, trigger_rule="all_done"
-)
-
-
-def final_status(**kwargs):
-    for task_instance in kwargs["dag_run"].get_task_instances():
-        if (
-            task_instance.current_state() != "success"
-            and task_instance.task_id != kwargs["task_instance"].task_id
-        ):
-            raise Exception(
-                f"Task {task_instance.task_id} failed! Failing this DAG run..."
-            )
-
-
-final_status = PythonOperator(
-    task_id="final_status",
-    provide_context=True,
-    python_callable=final_status,
-    trigger_rule="all_done",  # Ensures this task runs even if upstream fails
-    dag=dag,
+    dag=dag, clean_workflow_dir=True, trigger_rule="all_success"
 )
 (
     load_platform_config
@@ -103,5 +75,4 @@ final_status = PythonOperator(
     >> upload_results
     >> delete_iso_inst
     >> clean
-    >> final_status
 )

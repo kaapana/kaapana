12f2dd8f126b8bf51431b1cc4c751631b122e12e
Kaushal Parekh <kaushal.parekh@dkfz-heidelberg.de>
2023-07-21 17:34:10 +0200

    Add documentation for SPE related Operators

--- data-processing/processing-pipelines/tfda-spe-orchestrator/extension/docker/files/tfda_spe_orchestrator/RunAlgoOperator.py
+++ data-processing/processing-pipelines/tfda-spe-orchestrator/extension/docker/files/tfda_spe_orchestrator/RunAlgoOperator.py
@@ -7,6 +7,17 @@ from datetime import timedelta
 
 
 class RunAlgoOperator(KaapanaBaseOperator):
+    """
+    Operator for running the containerized workflow on data inside a Secure Processing Environment (SPE).
+
+    This operator extends the KaapanaBaseOperator and triggers a processing container that runs the containerized
+    workflow on user selected data in an SPE. It is intended to be used within the Kaapana platform and
+    makes use of specific Kaapana variables and configurations.
+
+    Notes:
+        1. Ensure that the environment variable called "TASK_TYPE" is passed on with the name of the operator task.
+    """
+
     execution_timeout = timedelta(hours=10)
 
     def __init__(

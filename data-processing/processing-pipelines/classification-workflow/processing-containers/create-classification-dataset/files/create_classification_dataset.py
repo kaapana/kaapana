c0aee63b281ed34f7bc4e5865418645d270179aa
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2023-07-28 17:20:22 +0200

    v1

--- data-processing/processing-pipelines/classification-workflow/processing-containers/create-classification-dataset/files/create_classification_dataset.py
+++ data-processing/processing-pipelines/classification-workflow/processing-containers/create-classification-dataset/files/create_classification_dataset.py
@@ -12,24 +12,7 @@ class Normalizer:
 
 class ZScoreNormalizer(Normalizer):
     def normalize(self, image, mask=None):
-        if mask is not None:
-            mean = np.mean(image[mask])
-            std = np.std(image[mask])
-            return np.where(mask, (image - mean) / std, image)
-        else:
-            return (image - np.mean(image)) / np.std(image)
-
-class NoNormalizer(Normalizer):
-    def normalize(self, image, target_dtype=np.float32):
-        return image.astype(target_dtype)
-
-class RescaleTo01Normalizer(Normalizer):
-    def normalize(self, image):
-        return (image - np.min(image)) / (np.max(image) - np.min(image))
-
-class RGBTo01Normalizer(Normalizer):
-    def normalize(self, image):
-        return image / 255.0
+        return (image - np.mean(image)) / np.std(image)
 
 def compute_statistics(data):
     stats = {}
@@ -43,30 +26,19 @@ def compute_statistics(data):
     stats['percentile_00_5'] = float(np.percentile(data, 0.5))
     return stats
 
-def compute_new_shape(old_shape, old_spacing, new_spacing):
+def resample_image(patient, order=3):
+    assert len(patient['stats']['dimensions']) in [2, 3], "data must be 2D (x, y) or 3D (x, y, z)"
+
+    old_shape = np.array(patient['stats']['dimensions'])
+    old_spacing = np.array(patient['stats']['spacing'])
+    new_spacing = np.array(patient['stats']['spacing_after_resampling'])
+    
     new_shape = np.array([int(round(i / j * k)) for i, j, k in zip(old_spacing, new_spacing, old_shape)])
-    return new_shape
-
-def resample_image(data, current_spacing, new_spacing, order=3):
-    assert len(data.shape) in [2, 3], "data must be 2D (x, y) or 3D (x, y, z)"
-
-    new_shape = compute_new_shape(data.shape, current_spacing, new_spacing)
     
     # Resize using skimage
-    resized = resize(data, new_shape, order, mode='edge', anti_aliasing=False)
+    resized = resize(sitk.GetArrayFromImage(patient['image']), new_shape, order=order, mode='edge', anti_aliasing=False, preserve_range=True)
     return resized
 
-def get_medians(dataset_folder):
-    median_spacings = []
-    for patient_folder in os.listdir(dataset_folder):
-        patient_path = os.path.join(dataset_folder, patient_folder, "dcm-converter")
-        for file_name in os.listdir(patient_path):
-            if file_name.endswith('.nrrd'):
-                file_path = os.path.join(patient_path, file_name)
-                _, header = load_nrrd(file_path)
-                spacings = compute_spacing(header)
-                median_spacings.append(median(spacings))
-    return median_spacings
 
 def get_target_spacing(spacings, target, target_size, anisotropy_threshold=3.0):
     worst_spacing_axis = np.argmax(target)
@@ -87,71 +59,7 @@ def get_target_spacing(spacings, target, target_size, anisotropy_threshold=3.0):
         target[worst_spacing_axis] = target_spacing_of_that_axis
     return target
 
-def select_normalization_method(dataset_stats):
-    selected_normalization_methods = {}
-
-    std_dev = dataset_stats['std']
-    dimensions = dataset_stats['dimensions']
-
-    # If the dataset has high standard deviation, use ZScoreNormalizer
-    if std_dev > 1:
-        return ZScoreNormalizer
-
-    # If the dataset has a normal range of pixel values (0-255), use RescaleTo01Normalizer
-    elif 0 <= min_pixel_intensity and max_pixel_intensity <= 255:
-        return RescaleTo01Normalizer
-
-    # If none of the above conditions are met, don't apply normalization
-    else:
-        return NoNormalizer
-
-
-def process_dataset(batch, median_spacing, spacings):
-    
-    stats = {}
-
-    for patient in batch.keys():
-        target_spacing = get_target_spacing(spacings, list(median_spacing), batch[patient]['image'].GetSize())
-        stats['spacing_after_resampling'] = target_spacing
-
-    lower_percentiles = [stats['percentile_00_5'] for stats in results.values()]
-    upper_percentiles = [stats['percentile_99_5'] for stats in results.values()]
-
-    common_lower_percentile = np.max(lower_percentiles)
-    common_upper_percentile = np.min(upper_percentiles)
-
-    for patient_folder in os.listdir(dataset_folder):
-        patient_path = os.path.join(dataset_folder, patient_folder, "dcm-converter")
-        for file_name in os.listdir(patient_path):
-            if file_name.endswith('.nrrd'):
-                file_path = os.path.join(patient_path, file_name)
-                data, header = load_nrrd(file_path)
-
-                # Normalize and resample together
-                normalizer = select_normalization_method(results[file_name])
-                normalizer = normalizer()
-
-                # Resample
-                data_resampled = resample_image(data, stats['spacings'], stats['spacing_after_resampling'])
-                
-                if isinstance(normalizer, CTNormalizer):
-                    # Normalize
-                    data_normalized = normalizer.normalize(data_resampled, lower=common_lower_percentile, upper=common_upper_percentile)
-                else:
-                    data_normalized = normalizer.normalize(data_resampled)
-
-                target_dir = os.path.join(dataset_folder, patient_folder, "classification-training")
-                os.makedirs(target_dir, exist_ok=True)
-                np.save(os.path.join(target_dir, patient_folder + ".npy"), data_normalized)
-
-
-
-    return results
-
 if __name__ == "__main__":
-
-    training = os.listdir("/kaapana/mounted/data")[0]
-    dataset_folder = os.path.join("/kaapana/mounted/data", training, "batch")
 
     batch = {}
 
@@ -160,29 +68,39 @@ if __name__ == "__main__":
     spacing_y = []
     spacing_z = []
 
-    for patient in os.listdir(dataset_folder):
+    for patient in os.listdir(os.environ['BATCHES_INPUT_DIR']):
         patient_dict = {}
 
-        patient_dict["image"] = sitk.ReadImage(os.path.join(dataset_folder, patient, "dcm-converter", patient + ".nrrd"))
+        patient_dict["image"] = sitk.ReadImage(os.path.join(os.environ['BATCHES_INPUT_DIR'], patient, "dcm-converter", patient + ".nrrd"))
 
-        spacing_x.append(patient_dict["image"].GetSpacing()[0])
-        spacing_y.append(patient_dict["image"].GetSpacing()[1])
-        spacing_z.append(patient_dict["image"].GetSpacing()[2])
+        spacing = patient_dict["image"].GetSpacing()
 
-        spacings.append(patient_dict["image"].GetSpacing())
+        if len(spacing) > 3:
+            raise ValueError("Not covering the scope of >3-dimensional arrays: E.g. 2 images in one")
 
-        if len(patient_dict["image"].GetSpacing()) > 3:
-            raise ValueError("Not covering the scope of n-dimensional arrays: E.g. 2 images in one")
+        spacing_x.append(spacing[0])
+        spacing_y.append(spacing[1])
+        spacing_z.append(spacing[2])
+        spacings.append(spacing)
 
         patient_dict["stats"] = compute_statistics(sitk.GetArrayFromImage(patient_dict["image"]))
+        patient_dict["stats"]["spacing"] = spacing
         
         batch[patient] = patient_dict
 
-    
-    median_spacing = (np.median(np.array(spacing_x)), np.median(np.array(spacing_y)), np.median(np.array(spacing_z)))
+    median_spacing = list((np.median(np.array(spacing_x)), np.median(np.array(spacing_y)), np.median(np.array(spacing_z))))
 
-    dataset_stats = process_dataset(batch, median_spacing, spacings)
+    for patient in batch.keys():
+        target_spacing = get_target_spacing(spacings, median_spacing, batch[patient]['image'].GetSize())
+        batch[patient]['stats']['spacing_after_resampling'] = target_spacing
+        
+        # Resample
+        data_resampled = resample_image(batch[patient])
 
-    # To save results in a file
-    #with open(os.path.join("mounted/data", training, "conf",'dataset_stats.json'), 'w') as outfile:
-        #json.dump(dataset_stats, outfile)
+        # Normalize
+        normalizer = ZScoreNormalizer()
+        data_normalized = normalizer.normalize(data_resampled)
+
+        target_dir = os.path.join(os.environ['BATCHES_INPUT_DIR'], patient, "classification-training")
+        os.makedirs(target_dir, exist_ok=True)
+        np.save(os.path.join(target_dir, patient + ".npy"), data_normalized)

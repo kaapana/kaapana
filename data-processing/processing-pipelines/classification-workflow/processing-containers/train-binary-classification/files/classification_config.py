951f27b3915ceae8dfa054d014a40689673ba363
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2023-07-28 17:38:47 +0200

    classification part

---
+++ data-processing/processing-pipelines/classification-workflow/processing-containers/train-binary-classification/files/classification_config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import torch
+
+# Task details
+TASK = ""  # If empty will be set with Task with the greatest index
+FOLD = 0
+
+# Input details
+PATCH_SIZE = (128, 128, 128)
+NUM_INPUT_CHANNELS = 1
+
+# Training details
+MODEL = "smpl_resnet_18"
+BATCH_SIZE = 1
+NUM_WORKERS = 4
+NUM_EPOCHS = 10 #1600
+SCHEDULER_ENTRY = 200
+SAVE_MODEL_START_STEP = 0
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Important paths
+TRAIN_DIR = "/data/classification_training"
+PATH_TO_DATA = "/data/classification-tasks"
+PATH_TO_RESULTS = "/minio/classification-results"
+CHECKPOINT_PATH = ""
+RESULTS_DIR = ""
+# Important names
+CHECKPOINT = "model.pth.tar"
+CHECKPOINT_BEST = "model_best.pth.tar"

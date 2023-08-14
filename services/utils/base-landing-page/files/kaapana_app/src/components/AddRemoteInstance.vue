88299af63c8dd22db23f51dcd772eaab5f787180
Markus Bujotzek <markus.bujotzek@dkfz-heidelberg.de>
2023-08-14 19:44:51 +0200

    text area to drop instance defnition in AddRemote nicer + error handling

--- services/utils/base-landing-page/files/kaapana_app/src/components/AddRemoteInstance.vue
+++ services/utils/base-landing-page/files/kaapana_app/src/components/AddRemoteInstance.vue
@@ -2,18 +2,17 @@
   <v-dialog v-model="remoteDialog" max-width="600px">
     <template v-slot:activator="{ on, attrs }">
       <v-btn v-bind="attrs" v-on="on" color="primary" small rounded outlined>
-        <!-- v-icon color="primary" >mdi-plus-circle</v-icon -->
         add remote
       </v-btn>
-      <!-- v-btn v-bind="attrs" v-on="on" small icon>
-        <v-icon color="primary" dark >mdi-plus-circle</v-icon>
-      </v-btn -->
     </template>
     <v-card>
       <v-form v-model="remoteValid" ref="remoteForm" lazy-validation="lazy-validation">
         <v-card-title><span class="text-h5">Remote Instance</span></v-card-title>
         <v-card-text>
           <v-container>
+            <v-row>
+              <p>Enter instance attributes in corresponding fields</p>
+            </v-row>
             <v-row>
               <v-col cols="5">
                 <v-text-field v-model="remotePost.instance_name" label="Instance name" required="" :disabled="remoteUpdate"></v-text-field>
@@ -34,8 +33,26 @@
                 <v-checkbox v-model="remotePost.ssl_check" label="Verify SSL" required=""></v-checkbox>
               </v-col>
             </v-row>
+          <v-row>
+            <v-col cols="16">
+              <v-divider></v-divider>
+            </v-col>
+          </v-row>
             <v-row>
-              <v-textarea v-model="pasteRemote" label="Paste remote instance definition" outlined></v-textarea>
+              <v-textarea 
+                v-model="pasteRemote" 
+                label="Or just paste remote instance definition as json string" 
+                placeholder="{
+                  'instance_name': '<instance_name>', 
+                  'host': '<host>', 
+                  'port': '<port>', 
+                  'token': '<token>', 
+                  'fernet_key': '<fernet_key>', 
+                  'ssl_check': '<true/false>'
+                }"
+                rows="8"
+                outlined
+              ></v-textarea>
             </v-row>
           </v-container>
         </v-card-text>
@@ -62,14 +79,28 @@ export default {
   watch: {
     pasteRemote() {
       // read pasted json string and transfer values
-      let jsonData = JSON.parse(this.pasteRemote)
-      this.remotePost.instance_name = jsonData["instance_name"]
-      this.remotePost.host = jsonData["host"]
-      this.remotePost.port = jsonData["port"]
-      this.remotePost.token = jsonData["token"]
-      this.remotePost.fernet_key = jsonData["fernet_key"]
-      this.remotePost.ssl_check = jsonData["ssl_check"]
+      try {
+        let jsonData = JSON.parse(this.pasteRemote)
+        this.remotePost.instance_name = jsonData.hasOwnProperty("instance_name") ? jsonData["instance_name"] : this.remotePost.instance_name;
+        this.remotePost.host = jsonData.hasOwnProperty("host") ? jsonData["host"] : this.remotePost.host;
+        this.remotePost.port = jsonData.hasOwnProperty("port") ? jsonData["port"] : this.remotePost.port;
+        this.remotePost.token = jsonData.hasOwnProperty("token") ? jsonData["token"] : this.remotePost.token;
+        this.remotePost.fernet_key = jsonData.hasOwnProperty("fernet_key") ? jsonData["fernet_key"] : this.remotePost.fernet_key;
+        this.remotePost.ssl_check = jsonData.hasOwnProperty("ssl_check") ? jsonData["ssl_check"] : this.remotePost.ssl_check;
+      } catch (error) {
+        this.$notify({
+          type: "error",
+          title: "Please enter the instance definition in the correct json format with all fields defined!",
+        });
+      }
+      
     }
+  },
+
+  computed: {
+    requiredRule() {
+      return [(v) => !!v || "This field is required"];
+    },
   },
 
   methods: {
@@ -96,17 +127,25 @@ export default {
       Object.assign(this.$data, resetData);
     },
     submitRemoteForm () {
-      kaapanaApiService
-        .federatedClientApiPost("/remote-kaapana-instance", this.remotePost)
-        .then((response) => {
-          this.remoteDialog = false
-          this.$emit('refreshRemoteFromAdding')
-          this.resetForm()
-        })
-        .catch((err) => {
-          console.log(err);
+      if (this.remotePost.token || this.remotePost.host || this.remotePost.instance_name) {
+        kaapanaApiService
+          .federatedClientApiPost("/remote-kaapana-instance", this.remotePost)
+          .then((response) => {
+            this.remoteDialog = false
+            this.$emit('refreshRemoteFromAdding')
+            this.resetForm()
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      else {
+        this.$notify({
+          type: "error",
+          title: "Please enter the instance definition in the correct json format with all fields defined!",
         });
-    }
+      }
+    },
   }
 }
 

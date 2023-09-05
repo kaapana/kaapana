bc9f130cda88a74edbe4b4b66e3617ffe0499bc7
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2023-09-05 12:51:51 +0200

    inference operator

---
+++ data-processing/processing-pipelines/classification-workflow/extension/docker/files/classification_inference_workflow/InferenceOperator.py
@@ -0,0 +1,28 @@
+from datetime import timedelta
+
+from kaapana.blueprints.kaapana_global_variables import (
+    DEFAULT_REGISTRY,
+    KAAPANA_BUILD_VERSION,
+)
+from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator
+
+
+class PreprocessingOperator(KaapanaBaseOperator):
+    def __init__(
+        self,
+        dag,
+        name="classification-preprocessing",
+        execution_timeout=timedelta(minutes=20),
+        *args,
+        **kwargs,
+    ):
+        super().__init__(
+            dag=dag,
+            name=name,
+            image=f"{DEFAULT_REGISTRY}/classification-preprocessing:{KAAPANA_BUILD_VERSION}",
+            image_pull_secrets=["registry-secret"],
+            execution_timeout=execution_timeout,
+            ram_mem_mb=4000,
+            *args,
+            **kwargs,
+        )

ae8b3a8550aa5adb4123ae1805ce3f3f929046a9
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2023-09-06 09:35:23 +0200

    Inference Operator

--- data-processing/processing-pipelines/classification-workflow/extension/docker/files/classification_inference_workflow/InferenceOperator.py
+++ data-processing/processing-pipelines/classification-workflow/extension/docker/files/classification_inference_workflow/InferenceOperator.py
@@ -1,17 +1,15 @@
 from datetime import timedelta
 
-from kaapana.blueprints.kaapana_global_variables import (
-    DEFAULT_REGISTRY,
-    KAAPANA_BUILD_VERSION,
-)
+from kaapana.blueprints.kaapana_global_variables import (DEFAULT_REGISTRY,
+                                                         KAAPANA_BUILD_VERSION)
 from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator
 
 
-class PreprocessingOperator(KaapanaBaseOperator):
+class InferenceOperator(KaapanaBaseOperator):
     def __init__(
         self,
         dag,
-        name="classification-preprocessing",
+        name="classification-inference",
         execution_timeout=timedelta(minutes=20),
         *args,
         **kwargs,
@@ -19,10 +17,10 @@ class PreprocessingOperator(KaapanaBaseOperator):
         super().__init__(
             dag=dag,
             name=name,
-            image=f"{DEFAULT_REGISTRY}/classification-preprocessing:{KAAPANA_BUILD_VERSION}",
+            image=f"{DEFAULT_REGISTRY}/classification-inference:{KAAPANA_BUILD_VERSION}",
             image_pull_secrets=["registry-secret"],
             execution_timeout=execution_timeout,
-            ram_mem_mb=4000,
+            ram_mem_mb = 4000,
             *args,
             **kwargs,
         )

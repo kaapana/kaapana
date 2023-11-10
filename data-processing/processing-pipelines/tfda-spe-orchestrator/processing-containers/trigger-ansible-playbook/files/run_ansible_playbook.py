9b99c42f17fa9bc90eeda516878508d76fc00235
Kaushal Parekh <kaushal.parekh@dkfz-heidelberg.de>
2023-11-10 17:01:42 +0100

    Setup VNC server in SPE via Ansible

--- data-processing/processing-pipelines/tfda-spe-orchestrator/processing-containers/trigger-ansible-playbook/files/run_ansible_playbook.py
+++ data-processing/processing-pipelines/tfda-spe-orchestrator/processing-containers/trigger-ansible-playbook/files/run_ansible_playbook.py
@@ -29,7 +29,7 @@ def run_command(command):
 
 
 def manage_iso_inst():
-    instance_state = getenv("INSTANCE_STATE", "None")
+    instance_state = getenv("INSTANCE_STATE", None)
     if instance_state == "present":
         print("Starting an isolated environment...")
     elif instance_state == "absent":
@@ -92,9 +92,7 @@ def manage_iso_inst():
                 ip_addr_string = re.findall(
                     r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", output
                 )
-                print(
-                    f"IP address of new isolated instance is: {ip_addr_string[-1]}"
-                )
+                print(f"IP address of new isolated instance is: {ip_addr_string[-1]}")
                 with open(iso_env_ip_path, "w+") as f:
                     f.write(ip_addr_string[-1])
     rc = process.poll()
@@ -104,12 +102,31 @@ def manage_iso_inst():
         raise Exception("Failed to manage isolated environment!")
 
 
+def setup_vnc_server():
+    print("Setup a VNC service inside the isolated environment...")
+    playbook_path = os.path.join(playbooks_dir, "setup_vnc_server.yaml")
+    if not os.path.isfile(playbook_path):
+        raise Exception(f"Playbook '{playbook_path}' file not found!")
+
+    with open(iso_env_ip_path, "r") as file:
+        iso_env_ip = file.read().rstrip()
+    playbook_args = f"target_host={iso_env_ip} ssh_key_operator_path={ssh_key_operator_path} ssh_key_name={ssh_key_name} remote_username={remote_username}"
+    command = ["ansible-playbook", playbook_path, "--extra-vars", playbook_args]
+    rc = run_command(command=command)
+    if rc == 0:
+        print(f"VNC server setup successfully!!")
+    else:
+        raise Exception("Playbook FAILED! Cannot proceed further...")
+
+
 def copy_data_algo():
     print("Copy data and algorithm to isolated environment...")
     playbook_path = os.path.join(playbooks_dir, "copy_data_and_algo_to_iso_env.yaml")
     if not os.path.isfile(playbook_path):
         raise Exception(f"Playbook '{playbook_path}' file not found!")
-    container_name_version = getenv("CONTAINER_NAME_VERSION", "None")
+    container_name_version = getenv("CONTAINER_NAME_VERSION", None)
+    pacs_batch_name = getenv("BATCH_NAME", None)
+    pacs_data_path = os.path.join(workflow_dir, pacs_batch_name)
     container_name = container_name_version.split(":")[0]
     user_container_path = os.path.join(workflow_dir, "algorithm_files", container_name)
     src_data_path = os.path.join(workflow_dir, data_dir_name)
@@ -119,7 +136,7 @@ def copy_data_algo():
     playbook_args = f"target_host={iso_env_ip} ssh_key_operator_path={ssh_key_operator_path} ssh_key_name={ssh_key_name} remote_username={remote_username} \
                         workflow_type={workflow_type} src_algorithm_files_path={user_container_path} \
                         user_experiment_name={container_name} src_data_path={src_data_path} \
-                        user_selected_data={data_dir_name}"
+                        user_selected_data={data_dir_name} pacs_data_path={pacs_data_path}"
     command = ["ansible-playbook", playbook_path, "--extra-vars", playbook_args]
     rc = run_command(command=command)
     if rc == 0:
@@ -133,7 +150,7 @@ def run_isolated_workflow():
     playbook_path = os.path.join(playbooks_dir, f"run_{workflow_type}.yaml")
     if not os.path.isfile(playbook_path):
         raise Exception(f"Playbook '{playbook_path}' file not found!")
-    container_name_version = getenv("CONTAINER_NAME_VERSION", "None")
+    container_name_version = getenv("CONTAINER_NAME_VERSION", None)
     container_name = container_name_version.split(":")[0]
     with open(iso_env_ip_path, "r") as file:
         iso_env_ip = file.read().rstrip()
@@ -148,14 +165,14 @@ def run_isolated_workflow():
 
 
 def fetch_results():
-    operator_out_dir = getenv("OPERATOR_OUT_DIR", "None")
+    operator_out_dir = getenv("OPERATOR_OUT_DIR", None)
     operator_out_dir = operator_out_dir if operator_out_dir.lower() != "none" else None
     assert operator_out_dir is not None
     print("Fetching results from isolated execution environment...")
     playbook_path = os.path.join(playbooks_dir, "fetch_results.yaml")
     if not os.path.isfile(playbook_path):
         raise Exception(f"Playbook '{playbook_path}' file not found!")
-    container_name_version = getenv("CONTAINER_NAME_VERSION", "None")
+    container_name_version = getenv("CONTAINER_NAME_VERSION", None)
     container_name = container_name_version.split(":")[0]
     with open(iso_env_ip_path, "r") as file:
         iso_env_ip = file.read().rstrip()
@@ -168,10 +185,10 @@ def fetch_results():
     if rc == 0:
         print(f"Results were fetched successfully!!")
     else:
-        raise Exception("Playbook FAILED! Cannot proceed further...") 
+        raise Exception("Playbook FAILED! Cannot proceed further...")
 
 
-workflow_dir = getenv("WORKFLOW_DIR", "None")
+workflow_dir = getenv("WORKFLOW_DIR", None)
 workflow_dir = workflow_dir if workflow_dir.lower() != "none" else None
 assert workflow_dir is not None
 data_dir_name = "user-selected-data"
@@ -192,15 +209,17 @@ ssh_key_name = platform_config["platforms"][platform_name]["platform_flavors"][
 remote_username = platform_config["platforms"][platform_name]["platform_flavors"][
     flavor_name
 ]["remote_username"]
-container_name_version = getenv("CONTAINER_NAME_VERSION", "None")
+container_name_version = getenv("CONTAINER_NAME_VERSION", None)
 container_name = container_name_version.split(":")[0]
 ssh_key_operator_path = os.path.join(workflow_dir, ".ssh")
 Path(ssh_key_operator_path).mkdir(parents=True, exist_ok=True)
-task_type = getenv("TASK_TYPE", "None")
+task_type = getenv("TASK_TYPE", None)
 task_type = task_type if task_type.lower() != "none" else None
 assert task_type is not None
 if task_type in ["create-iso-inst", "delete-iso-inst"]:
     manage_iso_inst()
+elif task_type == "setup-vnc-server":
+    setup_vnc_server()
 elif task_type == "copy-data-algo":
     copy_data_algo()
 elif task_type == "run-isolated-workflow":

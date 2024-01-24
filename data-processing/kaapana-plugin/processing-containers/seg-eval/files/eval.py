c029218b8cdbcdce49ede07d18f66f23271d4d2f
Ünal Akünal <uenal.akuenal@dkfz-heidelberg.de>
2024-01-24 19:01:51 +0100

    seg-eval: evaluate with merged 'combined_masks.nii.gz' files

--- data-processing/kaapana-plugin/processing-containers/seg-eval/files/eval.py
+++ data-processing/kaapana-plugin/processing-containers/seg-eval/files/eval.py
@@ -40,23 +40,16 @@ dir_in_gt = getenv("GT_IN_DIR", "None")
 dir_in_gt = dir_in_gt if dir_in_gt.lower() != "none" else None
 assert dir_in_gt is not None
 
-seg_filter_gt = getenv("SEG_FILTER_GT", "None")
-seg_filter_gt = seg_filter_gt if seg_filter_gt.lower() != "none" else None
-assert seg_filter_gt is not None
-
-seg_filter_test = getenv("SEG_FILTER_TEST", "None")
-seg_filter_test = seg_filter_test if seg_filter_test.lower() != "none" else None
-assert seg_filter_test is not None
-
 exit_on_error = getenv("EXIT_ON_ERROR", "None")
 exit_on_error = exit_on_error if exit_on_error.lower() != "none" else None
 assert exit_on_error is not None
-exit_on_error = False
 
-eval_metrics = getenv("METRICS", "None")
-eval_metrics = ast.literal_eval(eval_metrics) if eval_metrics.lower() != "none" else None
-assert eval_metrics is not None
-eval_metrics = list(eval_metrics)
+eval_metrics_str = getenv("METRICS", "None")
+eval_metrics_ast = (
+    ast.literal_eval(eval_metrics_str) if eval_metrics_str.lower() != "none" else None
+)
+assert eval_metrics_ast is not None
+eval_metrics: list = list(eval_metrics_ast)
 
 
 def read_nifti_file(filepath):
@@ -68,16 +61,19 @@ def read_nifti_file(filepath):
 def calculate_surface_dice(gt_mask, pred_mask, class_thresholds=[0.5]):
     """Calculate the Surface DICE coefficient."""
     # populate num_of_classes times
-    class_thresholds = np.full(gt_mask.shape[1], class_thresholds[0]).tolist()
+    # class_thresholds = np.full(gt_mask.shape[1], class_thresholds[0]).tolist()
+    
+    # for binary seg.
+    class_thresholds = [0.5]
     res = compute_surface_dice(
-        pred_mask, gt_mask, class_thresholds=class_thresholds, include_background=True
+        pred_mask, gt_mask, class_thresholds=class_thresholds, include_background=False
     )
     res = np.array([v.numpy() for v in res]).tolist()
     return res
 
 
 def calculate_hausdorff(gt_mask, pred_mask):
-    res = compute_hausdorff_distance(pred_mask, gt_mask, include_background=True)
+    res = compute_hausdorff_distance(pred_mask, gt_mask, include_background=False)
     res = np.array([v.numpy() for v in res]).tolist()
     return res
 
@@ -86,7 +82,7 @@ def calculate_asd(gt_mask, pred_mask):
     res = compute_average_surface_distance(
         pred_mask,
         gt_mask,
-        include_background=True,
+        include_background=False,
     )
     res = np.array([v.numpy() for v in res]).tolist()
     return res
@@ -158,43 +154,21 @@ def get_dataset_map(write_file=True) -> dict:
 
 
 def convert_to_tensor(nifti_mask) -> torch.Tensor:
-    # TODO: find a way to get label_int label_info pairs similar to seg_info.json in seg-check
-    max_label_encoding = 1
-
-    # divide by label int
-    non_zero = nifti_mask[np.nonzero(nifti_mask)]
-    unique = np.unique(non_zero)
-    if len(unique) == 1:
-        print(f"# single label {unique[0]} found")
-        nifti_mask = nifti_mask / unique[0]
-    else:
-        # TODO: remove this exit when max_label_encoding no longer assumes binary segmentation
-        print(f"# multiple labels {unique} found, aborting")
-        exit(1)
-
-    # one hot encoding
-    one_hot_encoded = (
-        (np.arange(max_label_encoding + 1) == nifti_mask[..., None])
-        .astype(int)
-        .transpose()
-    )
+    # convert all non-zero to one
+    binary_mask = (nifti_mask > 0).astype(int)
+    one_hot_encoded = np.stack([(binary_mask == 0), (binary_mask == 1)], axis=0)
+    # add new dim for batch-size for monai funcs
     one_hot_encoded = np.expand_dims(one_hot_encoded, axis=0)
     encoded_tensor = torch.from_numpy(one_hot_encoded)
 
     return encoded_tensor
 
 
-def evaluate_segmentation(
-    dataset_map, filter_keyword_gt=None, filter_keyword_test=None
-):
+def evaluate_segmentation(dataset_map):
     """
     Calculate and return multiple segmentation evaluation metrics.
     """
-    # TODO: (optional) add metrics selection
-
-    print(
-        f"# Evaluate segmentations with {dataset_map=} , {filter_keyword_gt=} , {filter_keyword_test=}"
-    )
+    print(f"# Evaluate segmentations with {dataset_map=}")
 
     metrics = {}
 
@@ -204,26 +178,11 @@ def evaluate_segmentation(
             gt_path = Path(data["gt_path"])
             test_path = Path(data["test_path"])
 
-            # Filter based on keywords
-
-            # TODO: add here label name based filters
             filtered_gt = [
-                i
-                for i in gt_path.glob("*")
-                if (filter_keyword_gt is not None)
-                and (
-                    filter_keyword_gt in str(i)
-                    or str.lower(filter_keyword_gt) in str(i)
-                )
+                i for i in gt_path.glob("*") if "combined_masks.nii.gz" in str(i)
             ]
             filtered_test = [
-                i
-                for i in test_path.glob("*")
-                if (filter_keyword_test is not None)
-                and (
-                    filter_keyword_test in str(i)
-                    or str.lower(filter_keyword_test) in str(i)
-                )
+                i for i in test_path.glob("*") if "combined_masks.nii.gz" in str(i)
             ]
 
             print(f"# {filtered_gt=}, {filtered_test=}")
@@ -231,18 +190,18 @@ def evaluate_segmentation(
             # should not be empty
             assert (
                 len(filtered_gt) > 0
-            ), f"Failed to find a mask with keyword '{filter_keyword_gt}' for {data['gt_id']} under path {gt_path}"
+            ), f"Failed to find combined_masks for {data['gt_id']} under path {gt_path}"
             assert (
                 len(filtered_test) > 0
-            ), f"Failed to find a mask with keyword '{filter_keyword_test}' for {data['test_id']} under path {test_path}"
+            ), f"Failed to find combined_masks for {data['test_id']} under path {test_path}"
 
             if len(filtered_gt) > 1:
                 print(
-                    f"More than one file found for keyword {filter_keyword_gt}, using the first one"
+                    f"More than one combine_mask.nii.gz files found under path {gt_path}, using the first one"
                 )
             if len(filtered_test) > 1:
                 print(
-                    f"More than one file found for keyword {filter_keyword_test}, using the first one"
+                    f"More than one combine_mask.nii.gz files found under path {test_path}, using the first one"
                 )
 
             # Read ground truth and test masks
@@ -253,7 +212,7 @@ def evaluate_segmentation(
 
             # Calculate metrics for each mask pair
             metric = {}
-            if "surface_dice" in eval_metrics:
+            if "surface_dice" in eval_metrics or "dice_score" in eval_metrics:
                 print(f"# Calculating surface dice for test mask {data['test_id']} ...")
                 metric["surface_dice"] = calculate_surface_dice(gt_mask, pred_mask)
             if "hausdorff_distance" in eval_metrics:
@@ -284,8 +243,6 @@ def evaluate_segmentation(
 
 
 dataset_map = get_dataset_map(write_file=True)
-evaluate_segmentation(
-    dataset_map, filter_keyword_gt=seg_filter_gt, filter_keyword_test=seg_filter_test
-)
+evaluate_segmentation(dataset_map)
 
 print("# Segmentation evaluation completed.")

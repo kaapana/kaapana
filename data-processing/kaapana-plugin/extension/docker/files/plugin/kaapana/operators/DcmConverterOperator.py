f94f650d34dbca137e6142d16dbd7a6fd33e0d41
Stefan Denner <stefan.denner@dkfz-heidelberg.de>
2024-01-30 16:56:34 +0100

    Making default parameters for super class configurable in dag defintion

--- data-processing/kaapana-plugin/extension/docker/files/plugin/kaapana/operators/DcmConverterOperator.py
+++ data-processing/kaapana-plugin/extension/docker/files/plugin/kaapana/operators/DcmConverterOperator.py
@@ -18,10 +18,13 @@ class DcmConverterOperator(KaapanaBaseOperator):
     def __init__(
         self,
         dag,
+        name="dcm-converter",
         output_format="nrrd",
         parallel_processes=3,
         env_vars=None,
         execution_timeout=timedelta(hours=10),
+        ram_mem_mb=2000,
+        ram_mem_mb_lmt=12000,
         **kwargs,
     ):
         """
@@ -49,11 +52,11 @@ class DcmConverterOperator(KaapanaBaseOperator):
         super().__init__(
             dag=dag,
             image=f"{DEFAULT_REGISTRY}/mitk-fileconverter:{KAAPANA_BUILD_VERSION}",
-            name="dcm-converter",
+            name=name,
             env_vars=env_vars,
             image_pull_secrets=["registry-secret"],
             execution_timeout=execution_timeout,
-            ram_mem_mb=2000,
-            ram_mem_mb_lmt=12000,
+            ram_mem_mb=ram_mem_mb,
+            ram_mem_mb_lmt=ram_mem_mb_lmt,
             **kwargs,
         )

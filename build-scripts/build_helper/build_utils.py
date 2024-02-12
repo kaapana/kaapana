c8a74fb035141f920c59437c524b3063666b98f6
Philipp Schader <philipp.schader@dkfz-heidelberg.de>
2024-02-12 13:05:59 +0100

    Fix build script if registry includes port number

--- build-scripts/build_helper/build_utils.py
+++ build-scripts/build_helper/build_utils.py
@@ -121,9 +121,7 @@ class BuildUtils:
             if "root" in entry.lower():
                 continue
 
-            name = entry.split(":")[1]
-            version = entry.split(":")[2]
-            entry_id = f"{name}:{version}"
+            entry_id = entry.split(":",1)[1]
 
             if "chart:" in entry:
                 unused_chart = [
@@ -166,7 +164,7 @@ class BuildUtils:
                         f"{entry_id} not found in container_images_unused!"
                     )
 
-            if "local-only" in name or BuildUtils.default_registry in name:
+            if entry_id.startswith("local-only") or entry_id.startswith(BuildUtils.default_registry):
                 build_order.append(entry_id)
 
         return build_order

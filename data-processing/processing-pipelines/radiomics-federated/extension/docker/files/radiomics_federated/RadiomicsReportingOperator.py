c5a4179ae57cf7c9a57b7ed2c7f4261270164bcf
Lorenz Feineis <lorenz.feineis@dkfz-heidelberg.de>
2024-02-15 10:00:24 +0100

    Adjust radiomics-federated to not realy on minio bucket mounted into filesystem.

--- data-processing/processing-pipelines/radiomics-federated/extension/docker/files/radiomics_federated/RadiomicsReportingOperator.py
+++ data-processing/processing-pipelines/radiomics-federated/extension/docker/files/radiomics_federated/RadiomicsReportingOperator.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from kaapana.blueprints.kaapana_global_variables import (
     DEFAULT_REGISTRY,
     KAAPANA_BUILD_VERSION,
+    SERVICES_NAMESPACE,
 )
 from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator
 
@@ -21,16 +22,21 @@ class RadiomicsReportingOperator(KaapanaBaseOperator):
         super().__init__(
             dag=dag,
             name=name,
-            image=f"{DEFAULT_REGISTRY}/jupyterlab:{KAAPANA_BUILD_VERSION}",
+            image=f"{DEFAULT_REGISTRY}/radiomics-federated-analysis:{KAAPANA_BUILD_VERSION}",
             image_pull_secrets=["registry-secret"],
             cmds=["/bin/bash"],
             arguments=[
-                "-c",
-                f"jupyter nbconvert --to html --execute --no-input /minio/{minio_path}  --output-dir /$WORKFLOW_DIR/$OPERATOR_OUT_DIR --output $RUN_ID-report.html",
+                f"/files/run_notebook.sh",
             ],
             execution_timeout=execution_timeout,
             ram_mem_mb=1000,
             ram_mem_mb_lmt=3000,
-            *args,
+            env_vars={
+                "MINIO_PATH": minio_path,
+                "MINIO_SERVICE": f"minio-service.{SERVICES_NAMESPACE}.svc:9000",
+                "MINIO_USER": "kaapanaminio",
+                "MINIO_PASSWORD": "Kaapana2020",
+            }
+            * args,
             **kwargs,
         )

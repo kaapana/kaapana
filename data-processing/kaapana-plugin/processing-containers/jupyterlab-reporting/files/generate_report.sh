5880af4c45c4fd191cb70ab6e635c578d6e68b30
Lorenz Feineis <lorenz.feineis@dkfz-heidelberg.de>
2024-02-16 14:26:53 +0100

    Enable to store jupyterlab notebooks in analysis-script bucket.

--- data-processing/kaapana-plugin/processing-containers/jupyterlab-reporting/files/generate_report.sh
+++ data-processing/kaapana-plugin/processing-containers/jupyterlab-reporting/files/generate_report.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -eu
 
-#!/bin/bash
 IFS=',' read -ra FORMATS_ARRAY <<< "${OUTPUT_FORMAT}"
-
 for format in "${FORMATS_ARRAY[@]}"; do
     jupyter nbconvert --to ${format} --execute --no-input /${WORKFLOW_DIR}/${NOTEBOOK_FILENAME}  --output-dir /${WORKFLOW_DIR}/${OPERATOR_OUT_DIR} --output ${RUN_ID}-report.${format}
 done

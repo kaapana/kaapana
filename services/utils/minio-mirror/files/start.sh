1f222c70fcec4680376596f3d53c9b4f6165dfa5
Lorenz Feineis <lorenz.feineis@dkfz-heidelberg.de>
2024-03-08 11:01:28 +0100

    Add flag --skip-errors to minio mirror.

--- services/utils/minio-mirror/files/start.sh
+++ services/utils/minio-mirror/files/start.sh
@@ -8,10 +8,10 @@ mkdir -p $LOCAL_PATH
 
 if [[ $ACTION == "FETCH" ]]; then
     echo "INFO: Start to mirror minio objects from ${MINIO_PATH} into local directory ${LOCAL_PATH}"
-    mc mirror minio/$MINIO_PATH $LOCAL_PATH --watch
+    mc mirror minio/$MINIO_PATH $LOCAL_PATH --watch --skip-errors
 elif [[ $ACTION == "PUSH" ]]; then
     echo "INFO: Start to mirror data from local directory ${LOCAL_PATH} into  minio objects at ${MINIO_PATH}"
-    mc mirror $LOCAL_PATH minio/$MINIO_PATH --watch
+    mc mirror $LOCAL_PATH minio/$MINIO_PATH --watch --skip-errors
 else
     echo "ERROR: ACTION ${ACTION} not supported!!"
     echo "ERROR: ACTION must be one of FETCH or PUSH"

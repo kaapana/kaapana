a29219f044586358f54ee30fd1a660efe6d1e7f1
Rajesh Baidya <rajesh.baidya@dkfz-heidelberg.de>
2024-03-25 10:35:30 +0100

    Kube helm api user input sanitization and issue resolved specific to feature/PoC branch

--- services/kaapana-admin/kube-helm/docker/files/backend/app/middlewares.py
+++ services/kaapana-admin/kube-helm/docker/files/backend/app/middlewares.py
@@ -1,26 +1,3 @@
-from fastapi import Request
-from starlette.middleware.base import BaseHTTPMiddleware
-
-
-class SecurityMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        response = await call_next(request)
-
-        # X-Frame-Options
-        response.headers["X-Frame-Options"] = "DENY"
-
-        # Referrer-Policy
-        response.headers["Referrer-Policy"] = "no-referrer"
-
-        # Strict-Transport-Security
-        response.headers["Strict-Transport-Security"] = (
-            "max-age=31536000; includeSubDomains"
-        )
-        # X-Content-Type-Options
-        response.headers["X-Content-Type-Options"] = "nosniff"
-        return response
-
-
 import html
 import json
 from fastapi import FastAPI, Request, Response

58081f7395d87e3951764c284804e619aa983886
hanno <h.gao@dkfz-heidelberg.de>
2024-03-27 14:39:38 +0100

    remove old Idlevue from landingpage

--- services/utils/base-landing-page/files/kaapana_app/src/main.ts
+++ services/utils/base-landing-page/files/kaapana_app/src/main.ts
@@ -4,17 +4,6 @@ import App from './App.vue'
 import router from './routes'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import IdleVue from 'idle-vue'
-
-const eventsHub = new Vue()
-
-Vue.use(IdleVue, {
-  eventEmitter: eventsHub,
-  // idleTime: 240000, //every 6 minute
-  idleTime: 1900000, //after ~30 minute
-  //idleTime: 190000, //every ~30 minute
-  events: ['keydown', 'mousedown', 'touchstart']
-})
 
 Vue.config.productionTip = true;
 

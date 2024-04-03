31b584f431e22fdbc89c504911bb491d7e18fdef
hanno <h.gao@dkfz-heidelberg.de>
2024-04-03 14:28:01 +0200

    remove readme from init

--- services/store/store-init/minio-init/docker/files/minio.sh
+++ services/store/store-init/minio-init/docker/files/minio.sh
@@ -4,6 +4,5 @@ sleep 30s
 mc config host add kaapana http://minio-service.$SERVICES_NAMESPACE.svc:9000/ $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
 mc mb --ignore-existing kaapana/uploads
 mc mb --ignore-existing kaapana/staticwebsiteresults
-mc cp /kaapana/app/readme.txt kaapana/uploads/readme.txt
 
 mc admin policy create kaapana kaapanaUser /kaapana/app/user-policy.json

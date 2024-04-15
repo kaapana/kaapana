32fe46322687d197afdddc8d3b17aee47cef5ec2
Philipp Schader <philipp.schader@dkfz-heidelberg.de>
2024-04-15 16:42:07 +0200

    Version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,9 @@ copyright = "2024, MIC @DKFZ"
 author = "MIC @DKFZ"
 
 # The short X.Y version
-version = "0.2.6"
+version = "0.3.0"
 # The full version, including alpha/beta/rc tags
-release = "0.2.6"
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------

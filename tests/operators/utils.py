bb591e47ef7a71fe0ce1e1bacb19ffa5a0a96c00
Mikulas Bankovic <mikulas.bankovic27@gmail.com>
2024-05-10 11:26:16 +0200

    Solve patient_age_inconsistency

---
+++ tests/operators/utils.py
@@ -0,0 +1,32 @@
+import sys
+from pathlib import Path
+from unittest.mock import MagicMock
+
+
+def mock_modules():
+    # Kubernetes + Kubetools
+    sys.modules["kaapana.kubetools"] = MagicMock()
+    sys.modules["kaapana.kubetools.volume_mount"] = MagicMock()
+    sys.modules["kaapana.kubetools.volume"] = MagicMock()
+    sys.modules["kaapana.kubetools.pod"] = MagicMock()
+    sys.modules["kaapana.kubetools.pod_stopper"] = MagicMock()
+    sys.modules["kaapana.kubetools.resources"] = MagicMock()
+
+    # Flask
+    sys.modules["requests"] = MagicMock()
+    sys.modules["kaapana.blueprints"] = MagicMock()
+    sys.modules["kaapana.blueprints.kaapana_utils"] = MagicMock()
+    sys.modules["kaapana.blueprints.kaapana_global_variables"] = MagicMock()
+
+    # Caching and intercommunication
+    sys.modules["kaapana.operators.HelperMinio"] = MagicMock()
+    sys.modules["kaapana.operators.HelperFederated"] = MagicMock()
+
+
+KAAPANA_DIR = Path(__file__).resolve().parents[2]
+PLUGIN_DIR = (
+    KAAPANA_DIR / "data-processing/kaapana-plugin/extension/docker/files/plugin/"
+)
+DICOM_TAG_DICT = (
+    KAAPANA_DIR / "services/flow/airflow/docker/files/scripts/dicom_tag_dict.json"
+)

a9aa6ff2f884a1e9963c5368d20e47b0b09669fb
rajesh.baidya <rajesh.baidya@dkfz-heidelberg.de>
2024-05-16 11:11:00 +0200

    dicom-validator operator added

---
+++ data-processing/kaapana-plugin/processing-containers/dicom-validator/files/dciodvfy.py
@@ -0,0 +1,109 @@
+import re
+import subprocess
+
+from base import ValidationItem, DicomValidator
+
+
+class DCIodValidator(DicomValidator):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @staticmethod
+    def process_dciodvfy_output(output: str):
+        lines = output.split("\n")
+        outputs = []
+
+        for l in lines:
+            if l.strip() == "":
+                continue
+
+            splitted = tuple(l.split(" - "))
+
+            outputs.append(splitted)
+
+        return outputs
+
+    @staticmethod
+    def get_validataion_item_from_err_tuple(source: tuple, ref: int = -1):
+        valid_tag_regex = re.compile(r"<\/|[A-Za-z0-9]*\([0-9]{4},[0-9]{4}\)\[\d\]>")
+
+        def extract_tag(target: str):
+            extracted = re.search(r"\([0-9a-fA-F]{4},[0-9a-fA-F]{4}\)", target)
+            if extracted:
+                return extracted.group(0)
+            extracted_wout_brakets = re.search(r"[0-9a-fA-F]{4},[0-9a-fA-F]{4}", target)
+            if extracted_wout_brakets:
+                return f"({extracted_wout_brakets.group(0)})"
+
+            # print(f"Could not able to extract tag from {target}")
+            return ""
+
+        def extract_name(target: str):
+            isvalid = re.match(valid_tag_regex, target)
+            if not isvalid:
+                # print(f'{target} is not a valid tag')
+                return ""
+
+            name_extracted = re.search(r"<\/[A-Za-z0-9]+\(", target)
+            if name_extracted:
+                extract = name_extracted.group(0)
+                return extract[2:-1]
+
+            return ""
+
+        def extract_index(target: str):
+            isvalid = re.match(valid_tag_regex, target)
+            if not isvalid:
+                return None
+
+            idx_extracted = re.search(r"\[\d\]", target)
+            if idx_extracted:
+                extract = idx_extracted.group(0)
+                return int(extract[1:-1])
+
+            return None
+
+        if len(source) > 1:
+            type = source[0]
+            tag = extract_tag(source[1])
+            if tag == "" and ref != -1:
+                tag = f"general-{ref}"
+            elif tag == "":
+                tag = "general"
+            name = extract_name(source[1])
+            index = extract_index(source[1])
+            message = source[2]
+            module = ""
+            if len(source) > 3:
+                module = source[3]
+            return ValidationItem(
+                tag,
+                type,
+                message,
+                name,
+                module,
+                index=index,
+                # raw=' - '.join(map(str, source)),
+            )
+
+        return None
+
+    def validate_dicom(self, dicom_path: str) -> tuple:
+        cmd = ["dciodvfy", "-new", dicom_path]
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        _, err = process.communicate()
+        errs = self.process_dciodvfy_output(err.decode("utf-8"))
+        vitems = [
+            self.get_validataion_item_from_err_tuple(item, idx)
+            for idx, item in enumerate(errs)
+            if self.get_validataion_item_from_err_tuple(item)
+        ]
+        errors, warnings = [], []
+
+        for item in vitems:
+            if item.type == "Error":
+                errors.append(item)
+            else:
+                warnings.append(item)
+
+        return errors, warnings

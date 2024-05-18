64a3475985ce571e8ecf379e018f59693796ee11
Ünal Akünal <uenal.akuenal@dkfz-heidelberg.de>
2024-05-18 17:58:04 +0200

    edk: retry cloning repo with feature/$branch

--- services/applications/edk/docker/edk/files/init.sh
+++ services/applications/edk/docker/edk/files/init.sh
@@ -4,7 +4,20 @@ set -euf -o pipefail
 echo "KAAPANA_BUILD_BRANCH"
 echo "$KAAPANA_BUILD_BRANCH"
 
-# clone Kaapana
-git clone -b $KAAPANA_BUILD_BRANCH --single-branch https://github.com/kaapana/kaapana
+clone_repo() {
+  local branch=$1
+  echo "cloning repo from branch $branch"
+  if git clone -b "$branch" --single-branch https://github.com/kaapana/kaapana; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+if ! clone_repo "$KAAPANA_BUILD_BRANCH"; then
+  echo "git clone failed, retrying with 'feature/' prefix"
+  clone_repo "feature/$KAAPANA_BUILD_BRANCH"
+fi
+
 KAAPANA_COMMIT_HASH=$(echo $KAAPANA_BUILD_VERSION | sed 's/.*-g//')
 git checkout $KAAPANA_COMMIT_HASH

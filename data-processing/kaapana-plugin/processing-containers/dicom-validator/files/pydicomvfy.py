9b417cd4c5803a651ee0f46ba08518bfd5888523
rajesh.baidya <rajesh.baidya@dkfz-heidelberg.de>
2024-05-21 18:51:33 +0200

    comments added

--- data-processing/kaapana-plugin/processing-containers/dicom-validator/files/pydicomvfy.py
+++ data-processing/kaapana-plugin/processing-containers/dicom-validator/files/pydicomvfy.py
@@ -14,12 +14,29 @@ class PyDicomValidator(DicomValidator):
         revision: str = "2024a",
         log_level: int = logging.ERROR,
     ) -> None:
+        """
+        Initialize the PyDicomValidator instance by calling the parent class initializer and setting up the validator.
+
+        Args:
+            dicom_definition_root (str): Root directory for DICOM definitions.
+            revision (str): Specific revision of DICOM definitions to use.
+            log_level (int): Logging level for the validator.
+        """
         super().__init__()
         self.dicom_definition_root = dicom_definition_root
         self.revision = revision
         self.validator = self._load_validator(log_level)
 
     def _load_validator(self, log_level: int = logging.DEBUG):
+        """
+        Load the DICOM validator with the specified log level.
+
+        Args:
+            log_level (int): Logging level for the validator.
+
+        Returns:
+            DicomFileValidator: An instance of the DICOM file validator.
+        """
         edition_reader = EditionReader(self.dicom_definition_root)
         # destination = edition_reader.get_revision(self.revision, recreate_json=False, create_json=False)
         destination = Path(self.dicom_definition_root, self.revision)
@@ -32,6 +49,17 @@ class PyDicomValidator(DicomValidator):
 
     @staticmethod
     def get_validatation_item_form_err_mssg(tag: str, raw_mssg: str, module: str = ""):
+        """
+        Create a ValidationItem from an error message string.
+
+        Args:
+            tag (str): The DICOM tag.
+            raw_mssg (str): The raw error message.
+            module (str, optional): The module name. Defaults to "".
+
+        Returns:
+            ValidationItem: An instance of the ValidationItem.
+        """
         tag_pos = raw_mssg.find(tag.upper())
         if tag_pos != -1:
             cropped_mssg = raw_mssg[tag_pos + len(tag) :]
@@ -62,6 +90,15 @@ class PyDicomValidator(DicomValidator):
         )
 
     def validate_dicom(self, dicom_path: str):
+        """
+        Validate a DICOM file using the dicom-validator package.
+
+        Args:
+            dicom_path (str): The file path of the DICOM file to be validated.
+
+        Returns:
+            Tuple[List[ValidationItem], List[ValidationItem]]: Two lists containing error and warning ValidationItems respectively.
+        """
         out = self.validator.validate(dicom_path)
         modules = out[dicom_path]
         errors = []

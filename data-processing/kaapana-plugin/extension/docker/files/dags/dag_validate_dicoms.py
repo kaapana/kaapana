d71f0f7edf2c4995f625d72cbce3e3a148160ff3
rajesh.baidya <rajesh.baidya@dkfz-heidelberg.de>
2024-05-22 17:59:11 +0200

    validate dag and StoreResults operator added

---
+++ data-processing/kaapana-plugin/extension/docker/files/dags/dag_validate_dicoms.py
@@ -0,0 +1,80 @@
+from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+from airflow.models import DAG
+
+from kaapana.operators.LocalGetInputDataOperator import LocalGetInputDataOperator
+from kaapana.operators.LocalMinioOperator import LocalMinioOperator
+from kaapana.operators.LocalWorkflowCleanerOperator import LocalWorkflowCleanerOperator
+from kaapana.operators.DcmValidatorOperator import DcmValidatorOperator
+from kaapana.operators.LocalStoreValidationResultsOperator import (
+    LocalStoreValidationResultsOperator,
+)
+
+
+ui_forms = {
+    "workflow_form": {
+        "type": "object",
+        "properties": {
+            "validator_algorithm": {
+                "title": "Action",
+                "description": "Choose the algorithm to validate your dicoms",
+                "enum": ["dicom-validator", "dciodvfy"],
+                "type": "string",
+                "default": "dicom-validator",
+                "required": True,
+            },
+        },
+    }
+}
+
+log = LoggingMixin().log
+
+args = {
+    "ui_forms": ui_forms,
+    "ui_visible": True,
+    "owner": "kaapana",
+    "start_date": days_ago(0),
+    "retries": 0,
+    "retry_delay": timedelta(seconds=30),
+}
+
+dag = DAG(dag_id="validate-dicoms", default_args=args, schedule_interval=None)
+
+
+get_input = LocalGetInputDataOperator(dag=dag)
+validate = DcmValidatorOperator(
+    dag=dag,
+    input_operator=get_input,
+    exit_on_error=False,
+)
+
+get_input_json = LocalGetInputDataOperator(
+    dag=dag, name="get-json-input-data", data_type="json"
+)
+save_to_opensearch = LocalStoreValidationResultsOperator(
+    dag=dag,
+    input_operator=get_input_json,
+    validator_output_dir=validate.operator_out_dir,
+    validation_tag="00111001",
+)
+
+put_to_minio_html = LocalMinioOperator(
+    dag=dag,
+    action_operator_dirs=[validate.operator_out_dir],
+    name="put-errors-html-to-minio",
+    action="put",
+    bucket_name="staticwebsiteresults",
+    file_white_tuples=(".html"),
+)
+
+clean = LocalWorkflowCleanerOperator(dag=dag, clean_workflow_dir=False)
+
+(
+    get_input
+    >> validate
+    >> get_input_json
+    >> save_to_opensearch
+    >> put_to_minio_html
+    >> clean
+)
---
+++ data-processing/kaapana-plugin/extension/docker/files/plugin/kaapana/operators/LocalStoreValidationResultsOperator.py
@@ -0,0 +1,220 @@
+import os
+import time
+from datetime import datetime
+from pytz import timezone
+import json
+import glob
+from html.parser import HTMLParser
+from opensearchpy import OpenSearch
+from enum import Enum
+from typing import List
+from kaapana.operators.KaapanaPythonBaseOperator import KaapanaPythonBaseOperator
+from kaapana.blueprints.kaapana_global_variables import SERVICES_NAMESPACE
+
+
+class ClassHTMLParser(HTMLParser):
+    def __init__(self, class_name):
+        super().__init__()
+        self.class_name = class_name
+        self.capture = False
+        self.data = []
+
+    def handle_starttag(self, tag, attrs):
+        if any((attr == ("class", self.class_name) for attr in attrs)):
+            self.capture = True
+
+    def handle_endtag(self, tag):
+        if self.capture:
+            self.capture = False
+
+    def handle_data(self, data):
+        if self.capture:
+            self.data.append(data.strip())
+
+
+def get_file_creation_time(file_path):
+    # Get file stats
+    stats = os.stat(file_path)
+    # Get creation time (on Unix, this is the change time)
+    creation_time = datetime.fromtimestamp(stats.st_ctime, tz=timezone("Europe/Berlin"))
+    # Convert to a human-readable format
+    readable_time = creation_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+    return readable_time
+
+
+class LocalStoreValidationResultsOperator(KaapanaPythonBaseOperator):
+    class Action(Enum):
+        ADD = "add"
+        DELETE = "delete"
+        ADD_FROM_FILE = "add_from_file"
+
+    @staticmethod
+    def _get_next_hex_tag(current_tag: str):
+        first_part = current_tag[0:4]
+        second_part = current_tag[-4:]
+
+        item_num = int(second_part, 16)
+
+        item_num += 1
+        item_hex = hex(item_num)
+        item_hex = item_hex[2:]
+
+        next_tag = f"{first_part}{item_hex}"
+        return next_tag
+
+    def tagging(
+        self,
+        series_instance_uid: str,
+        validation_tags: List[tuple],
+        clear_results: bool = False,
+    ):
+        print(series_instance_uid)
+        print(f"Tags 2 add: {validation_tags}")
+
+        # Read Tags
+        auth = None
+        os_client = OpenSearch(
+            hosts=[{"host": self.opensearch_host, "port": self.opensearch_port}],
+            http_compress=True,  # enables gzip compression for request bodies
+            http_auth=auth,
+            # client_cert = client_cert_path,
+            # client_key = client_key_path,
+            use_ssl=False,
+            verify_certs=False,
+            ssl_assert_hostname=False,
+            ssl_show_warn=False,
+            timeout=2,
+            # ca_certs = ca_certs_path
+        )
+
+        doc = os_client.get(index=self.opensearch_index, id=series_instance_uid)
+        print(doc)
+
+        if clear_results:
+            # Write Tags back
+            body = {"doc": {self.tag_field: None}}
+            os_client.update(
+                index=self.opensearch_index, id=series_instance_uid, body=body
+            )
+
+        final_tags = {}
+
+        current_tag = str(self.validation_tag)
+        for idx, item in enumerate(validation_tags):
+            item_tag = self._get_next_hex_tag(current_tag)
+            item_key = f"{item_tag} Validation{item[0]}_{item[1]}"
+            final_tags[item_key] = item[2]
+            current_tag = str(item_tag)
+
+        print(f"Final tags: {final_tags}")
+
+        # Write Tags back
+        body = {"doc": {self.tag_field: final_tags}}
+        os_client.update(index=self.opensearch_index, id=series_instance_uid, body=body)
+
+    def _extract_validation_results_from_html(self, html_output_path: str):
+        error_parser = ClassHTMLParser("item-count-label error")
+        with open(html_output_path, "r") as file:
+            error_parser.feed(file.read())
+
+        warning_parser = ClassHTMLParser("item-count-label warning")
+        with open(html_output_path, "r") as file:
+            warning_parser.feed(file.read())
+
+        n_errors = error_parser.data[0] if len(error_parser.data) > 0 else ""
+        n_warnings = warning_parser.data[0] if len(warning_parser.data) > 0 else ""
+        validation_time = get_file_creation_time(html_output_path)
+
+        return n_errors, n_warnings, validation_time
+
+    def start(self, ds, **kwargs):
+        print("Start tagging")
+
+        run_dir = os.path.join(self.airflow_workflow_dir, kwargs["dag_run"].run_id)
+        batch_folder = [
+            f for f in glob.glob(os.path.join(run_dir, self.batch_name, "*"))
+        ]
+
+        print(self.batch_name, batch_folder)
+
+        for batch_element_dir in batch_folder:
+
+            html_outputs = []
+            if self.validator_output_dir != "":
+                html_outputs = sorted(
+                    glob.glob(
+                        os.path.join(
+                            batch_element_dir, self.validator_output_dir, "*.html*"
+                        ),
+                        recursive=True,
+                    )
+                )
+
+            if len(html_outputs) == 0:
+                print(
+                    f"No validation output file found to update validation results in batch directory {batch_element_dir}. Skipping validation meta tagging"
+                )
+                continue
+
+            n_errors, n_warnings, validation_time = (
+                self._extract_validation_results_from_html(html_outputs[0])
+            )
+
+            json_files = sorted(
+                glob.glob(
+                    os.path.join(batch_element_dir, self.operator_in_dir, "*.json*"),
+                    recursive=True,
+                )
+            )
+
+            # tags = n_errors
+            tags_tuple = [
+                ("Errors", "integer", n_errors),  # (key, opensearch datatype, value)
+                ("Warnings", "integer", n_warnings),
+                ("Date", "datetime", validation_time),
+            ]
+
+            for meta_files in json_files:
+                print(f"Do tagging for file {meta_files}")
+                with open(meta_files) as fs:
+                    metadata = json.load(fs)
+                    series_uid = metadata["0020000E SeriesInstanceUID_keyword"]
+                    existing_tags = metadata.get(self.tag_field, None)
+
+                    clear_old_results = False
+                    if existing_tags:
+                        print(
+                            f"Warning!! Data found on tag {self.tag_field}. Will be replaced by newer results"
+                        )
+                        clear_old_results = True
+
+                    self.tagging(
+                        series_uid,
+                        validation_tags=tags_tuple,
+                        clear_results=clear_old_results,
+                    )
+
+    def __init__(
+        self,
+        dag,
+        validator_output_dir: str,
+        validation_tag: str = "00111001",
+        name: str = "results-to-open-search",
+        opensearch_host=f"opensearch-service.{SERVICES_NAMESPACE}.svc",
+        opensearch_port=9200,
+        opensearch_index="meta-index",
+        *args,
+        **kwargs,
+    ):
+        """
+        :param tag_field: the field of the opensearch object where the tags are stored
+        """
+
+        self.tag_field = f"{validation_tag} ValidationResults_object"
+        self.validator_output_dir = validator_output_dir
+        self.validation_tag = validation_tag
+        self.opensearch_host = opensearch_host
+        self.opensearch_port = opensearch_port
+        self.opensearch_index = opensearch_index
+
+        super().__init__(dag=dag, name=name, python_callable=self.start, **kwargs)

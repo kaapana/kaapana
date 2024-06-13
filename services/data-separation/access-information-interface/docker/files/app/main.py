f9c840bbcce49270beaac321266978d40ed5d1fb
lorenz.feineis <lorenz.feineis@dkfz-heidelberg.de>
2024-06-13 16:44:48 +0200

    Restructured endpoints for backend

--- services/data-separation/access-information-interface/docker/files/app/main.py
+++ services/data-separation/access-information-interface/docker/files/app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
-from .routes import router
+from .aii.routes import router as aii_router
+from .projects.routes import router as projects_router
+
 from .database import engine
 from .models import Base
 
 Base.metadata.create_all(engine)
 
-app = FastAPI(root_path="",title="access-information-interface", docs_url="/docs")
+app = FastAPI(root_path="", title="access-information-interface", docs_url="/docs")
 app.openapi_version = "3.0.0"
 
 
-app.include_router(router)
+app.include_router(aii_router, prefix="/aii")
+app.include_router(projects_router, prefix="/projects")

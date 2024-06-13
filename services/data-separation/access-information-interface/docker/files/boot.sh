7e9bbda6ad401fcc820b17f62966d07c462ed06c
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-06-13 18:17:20 +0200

    swagger docs

--- services/data-separation/access-information-interface/docker/files/boot.sh
+++ services/data-separation/access-information-interface/docker/files/boot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export PYTHONPATH="$PWD" 
-APPLICATION_ROOT="/"
+APPLICATION_ROOT="/aii"
 # forwarded-allow-ips  inc
 uvicorn app.main:app --reload --host 0.0.0.0 --port $PORT --workers $WORKERS --root-path $APPLICATION_ROOT --forwarded-allow-ips '*'

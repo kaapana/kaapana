08f2231b76705e53afe51a93cb1e18bc934d1d4d
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-06-13 18:17:20 +0200

    swagger docs

--- services/data-separation/access-information-interface/docker/files/app/main.py
+++ services/data-separation/access-information-interface/docker/files/app/main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
 from .aii.routes import router as aii_router
 from .projects.routes import router as projects_router
+from fastapi.responses import FileResponse
 
 from .database import engine
 from .models import Base
 
 Base.metadata.create_all(engine)
 
-app = FastAPI(root_path="", title="access-information-interface", docs_url="/docs")
-app.openapi_version = "3.0.0"
-
+app = FastAPI(
+    root_path="/aii",
+    title="access-information-interface",
+    docs_url="/docs",
+    openapi_url="/openapi.json",
+    version="0.1.0",
+)
 
 app.include_router(aii_router, prefix="/aii")
 app.include_router(projects_router, prefix="/projects")

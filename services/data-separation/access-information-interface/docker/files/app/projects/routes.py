cad8caa9b675e61317e706badb743b2765251f6c
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-06-13 18:17:41 +0200

    project routes

--- services/data-separation/access-information-interface/docker/files/app/projects/routes.py
+++ services/data-separation/access-information-interface/docker/files/app/projects/routes.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
-from ..schemas import Projects, Data, Rights
+from .schemas import Project, CreateProject
+from .crud import create_project
 
 from ..database import get_session
 
 router = APIRouter()
+
+
+@router.post("", response_model=Project)  # POST /projects
+async def projects(
+    project: CreateProject, session: AsyncSession = Depends(get_session)
+):
+    return await create_project(session, project)

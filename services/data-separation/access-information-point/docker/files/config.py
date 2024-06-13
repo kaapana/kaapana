ad89f1afd71d4ef45f055f31a40ae0523a7d8a89
lorenz.feineis <lorenz.feineis@dkfz-heidelberg.de>
2024-06-13 15:33:38 +0200

    Fix database deployment; remove init-db job.

--- services/data-separation/access-information-point/docker/files/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-POSTGRES_USERNAME = os.getenv("POSTGRES_USERNAME", "kaapanauser")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
-POSTGRES_HOST = f"aip-postgres-service.services.svc"
-POSTGRES_PORT = f"5432"
-DATABASE_NAME = "access_information_point"

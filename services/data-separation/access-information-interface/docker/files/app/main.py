bb93a29bbc7b8307285f2085990458a2cb1ddd45
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-06-14 18:55:31 +0200

    Initial database population

--- services/data-separation/access-information-interface/docker/files/app/main.py
+++ services/data-separation/access-information-interface/docker/files/app/main.py
@@ -1,22 +1,10 @@
 from fastapi import FastAPI
 from .aii.routes import router as aii_router
 from .projects.routes import router as projects_router
-from .projects.crud import (
-    create_rights,
-    create_roles,
-    create_roles_rights_mapping,
-    get_roles,
-    get_rights,
-    create_project,
-    create_users_projects_roles_mapping,
-    get_projects,
-)
-from .projects.schemas import CreateRight, CreateRole, CreateProject
-from .database import async_session, async_engine
+from .database import async_engine
 from .models import Base
 from contextlib import asynccontextmanager
-from sqlalchemy.exc import IntegrityError
-
+from . import init_scripts
 import logging
 
 logger = logging.getLogger(__name__)
@@ -26,87 +14,7 @@ logger = logging.getLogger(__name__)
 async def lifespan(app: FastAPI):
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    async with async_session() as session:
-        # init rights
-        init_rights = [
-            CreateRight(
-                name="read",
-                description="Read rights",
-                claim_key="aii_test1",
-                claim_value="lol",
-            ),
-            CreateRight(
-                name="write",
-                description="Write rights",
-                claim_key="aii_test1",
-                claim_value="rofl",
-            ),
-            CreateRight(
-                name="delete",
-                description="Delete rights",
-                claim_key="aii_test2",
-                claim_value="hdgdl",
-            ),
-        ]
-        for right in init_rights:
-            try:
-                await create_rights(session, right)
-            except IntegrityError:
-                logger.warning(f"Right {right} already exists")
-                await session.rollback()
-        # init roles
-        init_roles = [
-            CreateRole(name="admin", description="Admin role"),
-            CreateRole(name="read-only", description="Read only role"),
-        ]
-        for role in init_roles:
-            try:
-                await create_roles(session, role)
-            except IntegrityError:
-                logger.warning(f"Role {role} already exists")
-                await session.rollback()
-        # init role mappings
-        role_admin = await get_roles(session, name="admin")
-        for right_name in ["read", "write", "delete"]:
-            right = await get_rights(session, name=right_name)
-            try:
-                await create_roles_rights_mapping(
-                    session, role_admin[0].id, right[0].id
-                )
-            except IntegrityError:
-                logger.warning(
-                    f"RolesRights mapping for {role_admin[0]} and {right[0]} already exists"
-                )
-                await session.rollback()
-                role_admin = await get_roles(session, name="admin")
-
-        # init project
-        try:
-            admin_project = await create_project(
-                session,
-                project=CreateProject(
-                    name="admin", description="Initial admin project"
-                ),
-            )
-        except IntegrityError:
-            logger.warning(f"Initial admin project already exists")
-            await session.rollback()
-            role_admin = await get_roles(session, name="admin")
-            admin_project = await get_projects(session, name="admin")
-            admin_project = admin_project[0]
-
-        try:
-            await create_users_projects_roles_mapping(
-                session,
-                project_id=admin_project.id,
-                role_id=role_admin[0].id,
-                keycloak_id="bd1d8eb5-49f1-48b9-828b-9f3771691eb5",
-            )
-        except IntegrityError:
-            logger.warning(f"Initial UsersProjectsRoles mapping already exists")
-            await session.rollback()
-
-        # TODO cread from configmap
+    # await init_scripts.initial_database_population()
     yield  # This yield separates startup from shutdown code
     # Code here would run after the application stops
 

e85c1a92b836ed48a8043a0392955c499a5021b6
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-06-14 08:31:36 +0200

    Add tags for endpoints

--- services/data-separation/access-information-interface/docker/files/app/aii/routes.py
+++ services/data-separation/access-information-interface/docker/files/app/aii/routes.py
@@ -9,16 +9,16 @@ from ..database import get_session
 router = APIRouter()
 
 
-@router.get("/projects", response_model=List[Project])
+@router.get("/projects", response_model=List[Project], tags=["Aii"])
 async def projects(keycloak_id: str, session: AsyncSession = Depends(get_session)):
     return await get_user_projects(session, keycloak_id)
 
 
-@router.get("/projects/{project_id}/data", response_model=List[Data])
+@router.get("/projects/{project_id}/data", response_model=List[Data], tags=["Aii"])
 async def project_data(project_id: int, session: AsyncSession = Depends(get_session)):
     return await get_project_data(session, project_id)
 
 
-@router.get("/users/{user_id}/rights", response_model=List[Right])
+@router.get("/users/{user_id}/rights", response_model=List[Right], tags=["Aii"])
 async def user_rights(user_id: int, session: AsyncSession = Depends(get_session)):
     return await get_user_rights(session, user_id)

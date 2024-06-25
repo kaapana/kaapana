e0f716aa8decfa5d270d65577bdc1ecc35400144
Rajesh Baidya <rajesh.baidya@dkfz-heidelberg.de>
2024-06-25 11:22:02 +0200

    Feature - Create Dicom Validation DAG

--- data-processing/kaapana-plugin/extension/docker/files/dags/dag_validate_dicoms.py
+++ data-processing/kaapana-plugin/extension/docker/files/dags/dag_validate_dicoms.py
@@ -32,13 +32,6 @@ ui_forms = {
                 "type": "boolean",
                 "default": False,
             },
-            "tags_whitelist": {
-                "type": "array",
-                "title": "Tags Whitelist",
-                "description": "List of DICOM tags, that will be ignored while validating",
-                "items": {"type": "string", "title": "DICOM tag"},
-                "default": [],
-            },
         },
     }
 }

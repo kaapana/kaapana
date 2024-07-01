4506a05204072aa6df85b10a490271e4a4b39439
Bujotzek, Markus <markus.bujotzek@dkfz-heidelberg.de>
2024-07-01 09:22:55 +0200

    upgrade nnunet shell commands to nnunetv2

--- data-processing/processing-pipelines/nnunet/processing-containers/nnunet/files/start_nnunet.sh
+++ data-processing/processing-pipelines/nnunet/processing-containers/nnunet/files/start_nnunet.sh
@@ -61,7 +61,29 @@ if [ "$MODE" = "preprocess" ]; then
 
     echo "# COMMAND: nnUNet_plan_and_preprocess -t $TASK_NUM -tl $PREP_TL -tf $PREP_TF $preprocess $preprocess_verify --increment_step $PREP_INCREMENT_STEP"
     echo "#"
-    nnUNet_plan_and_preprocess -t $TASK_NUM -tl $PREP_TL -tf $PREP_TF $preprocess $preprocess_verify --increment_step $PREP_INCREMENT_STEP
+    echo "# PREPROCESS:      $PREP_PREPROCESS";
+    echo "# PREP_INCREMENT_STEP: $PREP_INCREMENT_STEP";
+    echo "# CHECK_INTEGRITY: $PREP_CHECK_INTEGRITY";
+    echo "#"
+    echo "# OMP_THREAD_LIMIT" $OMP_THREAD_LIMIT
+    echo "# OMP_NUM_THREADS" $OMP_NUM_THREADS
+    echo "# PREP_TL" $PREP_TL
+    echo "# PREP_TF" $PREP_TF
+    echo "#"
+    echo "# NIFTI_DIRS: $INPUT_MODALITY_DIRS";
+    echo "# LABEL_DIR: $PREP_LABEL_DIRS";
+    echo "# MODALITIES: $PREP_MODALITIES";
+    echo "#"
+    echo "# nnUNet_raw_data_base: $nnUNet_raw_data_base";
+    echo "# nnUNet_preprocessed:  $nnUNet_preprocessed";
+    echo "# RESULTS_FOLDER:       $RESULTS_FOLDER";
+    echo "#"
+    echo "# pre-trained weights: $PRETRAINED_WEIGHTS";
+    echo "#"
+    echo "#"
+    echo "# COMMAND:     nnUNetv2_plan_and_preprocess -t $TASK_NUM -tl $PREP_TL -tf $PREP_TF $preprocess $preprocess_verify --increment_step $PREP_INCREMENT_STEP $experiment_planner_pretrain $overwrite_plans $overwrite_plans_identifier"
+    echo "#"
+    nnUNetv2_plan_and_preprocess -t $TASK_NUM -tl $PREP_TL -tf $PREP_TF $preprocess $preprocess_verify --increment_step $PREP_INCREMENT_STEP $experiment_planner_pretrain $overwrite_plans $overwrite_plans_identifier
     echo "#"
     echo "# Dataset itegrity OK!"
     echo "#"
@@ -96,38 +118,8 @@ elif [ "$MODE" = "training" ]; then
     echo "# TRAIN_CONTINUE:       $TRAIN_CONTINUE"
     echo "# TRAIN_NPZ:            $TRAIN_NPZ"
     echo "#"
-    
-    # if ! [ -z "${TENSORBOARD_DIR}" ]; then
-    #     echo "# Starting monitoring:";
-    #     python3 -u /src/monitoring.py $RESULTS_FOLDER $TRAIN_FOLD $TENSORBOARD_DIR $nnUNet_raw_data_base &
-    #     echo "#"
-    # fi
-    
-    if [ "$TRAIN_CONTINUE" = "True" ] || [ "$TRAIN_CONTINUE" = "true" ]; then
-        continue="--continue_training"
-    else
-        continue=""
-    fi
-    
-    if [ "$FP32" = "True" ] || [ "$FP32" = "true" ]; then
-        fp32="--fp32"
-    else
-        fp32=""
-    fi
-    if [ "$TRAIN_NPZ" = "True" ] || [ "$TRAIN_NPZ" = "true" ]; then
-        npz="--npz"
-    else
-        npz=""
-    fi
-    if [ "$TRAIN_DISABLE_POSTPROCESSING" = "True" ] || [ "$TRAIN_DISABLE_POSTPROCESSING" = "true" ]; then
-        disable_postprocessing="--disable_postprocessing_on_folds"
-    else
-        disable_postprocessing=""
-    fi
-    
-    echo "#"
-    echo "# COMMAND: nnUNet_train $MODEL $TRAIN_NETWORK_TRAINER $TASK $TRAIN_FOLD $fp32 $npz $continue "
-    nnUNet_train $MODEL $TRAIN_NETWORK_TRAINER $TASK $TRAIN_FOLD $fp32 $npz $continue
+    echo "# COMMAND: nnUNetv2_train $MODEL $TRAIN_NETWORK_TRAINER $TASK $TRAIN_FOLD $pretrained_plan $pretrain $fp32 $npz $continue"
+    nnUNetv2_train $MODEL $TRAIN_NETWORK_TRAINER $TASK $TRAIN_FOLD $pretrained_plan $pretrain $fp32 $npz $continue
 
     # CREATE_REPORT="True"
 
@@ -187,8 +179,8 @@ elif [ "$MODE" = "identify-best" ]; then
         strict=""
     fi
 
-    echo "# COMMAND: nnUNet_find_best_configuration -m $MODEL -t $TASK_NUM $strict"
-    nnUNet_find_best_configuration -m $MODEL -t $TASK_NUM $strict
+    echo "# COMMAND: nnUNetv2_find_best_configuration -m $MODEL -t $TASK_NUM $strict"
+    nnUNetv2_find_best_configuration -m $MODEL -t $TASK_NUM $strict
 
     echo "#"
     echo "# DONE"
@@ -245,10 +237,10 @@ elif [ "$MODE" = "export-model" ]; then
     echo "# model_output_path:     $model_output_path"
     echo "#"
     echo "#"
-    echo "# COMMAND: nnUNet_export_model_to_zip -t $TASK -m $MODEL -tr $TRAIN_NETWORK_TRAINER -o $model_output_path "
+    echo "# COMMAND: nnUNetv2_export_model_to_zip -t $TASK -m $MODEL -tr $TRAIN_NETWORK_TRAINER -o $model_output_path "
     echo "#"
     echo "# DONE"
-    nnUNet_export_model_to_zip -t $TASK -m $MODEL -tr $TRAIN_NETWORK_TRAINER -o $model_output_path -f 0 1 2 3 4
+    nnUNetv2_export_model_to_zip -t $TASK -m $MODEL -tr $TRAIN_NETWORK_TRAINER -o $model_output_path -f 0 1 2 3 4
     
 elif [ "$MODE" = "install-model" ]; then
     export nnUNet_raw_data_base="/$WORKFLOW_DIR/$OPERATOR_IN_DIR"
@@ -279,8 +271,8 @@ elif [ "$MODE" = "install-model" ]; then
     shopt -s nullglob
     for MODEL_PATH in *.zip; do
         echo "Found zip-file: $MODEL_PATH"
-        echo "Installing: nnUNet_install_pretrained_model_from_zip $MODEL_PATH"
-        nnUNet_install_pretrained_model_from_zip $MODEL_PATH
+        echo "Installing: nnUNetv2_install_pretrained_model_from_zip $MODEL_PATH"
+        nnUNetv2_install_pretrained_model_from_zip $MODEL_PATH
         
     done
     echo "#"
@@ -294,79 +286,3 @@ echo "##########################        DONE       ##########################"
 echo "#"
 echo "#######################################################################"
 exit 0
-
-# usage: nnUNet_plan_and_preprocess [-h] [-t TASK_NAMES [TASK_NAMES ...]]
-#                                   [-pl3d PLANNER3D] [-pl2d PLANNER2D] [-no_pp]
-#                                   [-tl TL] [-tf TF]
-#                                   [--verify_dataset_integrity]
-
-# optional arguments:
-#   -h, --help            show this help message and exit
-#   -t TASK_NAMES [TASK_NAMES ...], --TASK_NAMEs TASK_NAMES [TASK_NAMES ...]
-#                         List of integers belonging to the task ids you wish to
-#                         run experiment planning and preprocessing for. Each of
-#                         these ids must, have a matching folder 'TaskXXX_' in
-#                         the raw data folder
-#   -pl3d PLANNER3D, --planner3d PLANNER3D
-#                         Name of the ExperimentPlanner class for the full
-#                         resolution 3D U-Net and U-Net cascade. Default is
-#                         ExperimentPlanner3D_v21. Can be 'None', in which case
-#                         these U-Nets will not be configured
-#   -pl2d PLANNER2D, --planner2d PLANNER2D
-#                         Name of the ExperimentPlanner class for the 2D U-Net.
-#                         Default is ExperimentPlanner2D_v21. Can be 'None', in
-#                         which case this U-Net will not be configured
-#   -no_pp                Set this flag if you dont want to run the
-#                         preprocessing. If this is set then this script will
-#                         only run the experiment planning and create the plans
-#                         file
-#   -tl TL                Number of processes used for preprocessing the low
-#                         resolution data for the 3D low resolution U-Net. This
-#                         can be larger than -tf. Don't overdo it or you will
-#                         run out of RAM
-#   -tf TF                Number of processes used for preprocessing the full
-#                         resolution data of the 2D U-Net and 3D U-Net. Don't
-#                         overdo it or you will run out of RAM
-#   --verify_dataset_integrity
-#                         set this flag to check the dataset integrity. This is
-#                         useful and should be done once for each dataset!
-
-# usage: nnUNet_train [-h] [-val] [-c] [-p P] [--use_compressed_data]
-#                     [--deterministic] [--npz] [--find_lr] [--valbest] [--fp32]
-#                     [--val_folder VAL_FOLDER]
-#                     network network_trainer task fold
-
-# positional arguments:
-#   network
-#   network_trainer
-#   task                  can be task name or task id
-#   fold                  0, 1, ..., 5 or 'all'
-
-# optional arguments:
-#   -h, --help            show this help message and exit
-#   -val, --validation_only
-#                         use this if you want to only run the validation
-#   -c, --continue_training
-#                         use this if you want to continue a training
-#   -p P                  plans identifier. Only change this if you created a
-#                         custom experiment planner
-#   --use_compressed_data
-#                         If you set use_compressed_data, the training cases
-#                         will not be decompressed. Reading compressed data is
-#                         much more CPU and RAM intensive and should only be
-#                         used if you know what you are doing
-#   --deterministic       Makes training deterministic, but reduces training
-#                         speed substantially. I (Fabian) think this is not
-#                         necessary. Deterministic training will make you
-#                         overfit to some random seed. Don't use that.
-#   --npz                 if set then nnUNet will export npz files of predicted
-#                         segmentations in the validation as well. This is
-#                         needed to run the ensembling step so unless you are
-#                         developing nnUNet you should enable this
-#   --find_lr             not used here, just for fun
-#   --valbest             hands off. This is not intended to be used
-#   --fp32                disable mixed precision training and run old school
-#                         fp32
-#   --val_folder VAL_FOLDER
-#                         name of the validation folder. No need to use this for
-#                         most people

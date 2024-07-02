d071cfcfe27d132adbc890c774ce0d84b8e772d1
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-07-02 18:39:09 +0200

    Refactored LocalDicomSendOperator

--- data-processing/kaapana-plugin/extension/docker/files/plugin/kaapana/operators/LocalDicomSendOperator.py
+++ data-processing/kaapana-plugin/extension/docker/files/plugin/kaapana/operators/LocalDicomSendOperator.py
@@ -1,6 +1,7 @@
 import glob
 import os
 from datetime import timedelta
+from pathlib import Path
 
 from kaapana.operators.KaapanaPythonBaseOperator import KaapanaPythonBaseOperator
 from kaapana.operators.HelperDcmWeb import HelperDcmWeb
@@ -13,41 +14,10 @@ class LocalDicomSendOperator(KaapanaPythonBaseOperator):
     This operator is used for sending data to the platform locally.
     """
 
-    def start(self, **kwargs):
-        self.conf = kwargs["dag_run"].conf
-        self.dcmweb_helper = HelperDcmWeb(
-            dag_run=kwargs["dag_run"]
-        )
-        run_dir = os.path.join(self.airflow_workflow_dir, kwargs["dag_run"].run_id)
-        batch_folders = [
-            f for f in glob.glob(os.path.join(run_dir, self.batch_name, "*"))
-        ]
-        for batch_element_dir in batch_folders:
-            print("input operator ", self.operator_in_dir)
-            element_input_dir = os.path.join(batch_element_dir, self.operator_in_dir)
-            print(element_input_dir)
-
-            dcm_files = sorted(
-                glob.glob(os.path.join(element_input_dir, "**/*.dcm*"), recursive=True)
-            )
-            number_of_instances = len(dcm_files)
-            if number_of_instances == 0:
-                continue
-            print("Number of instances to send: ", number_of_instances)
-            self.dcmweb_helper.upload_dcm_files(element_input_dir)
-
-    def __init__(
-        self,
-        dag,
-        ae_title: str = "KAAPANA",
-        **kwargs,
-    ):
+    def __init__(self, dag, **kwargs):
         """
         Constructor for the LocalDicomSendOperator.
         """
-
-        self.aetitle = ae_title
-
         super().__init__(
             dag=dag,
             task_id="local-dcm-send",
@@ -56,3 +26,19 @@ class LocalDicomSendOperator(KaapanaPythonBaseOperator):
             execution_timeout=timedelta(minutes=60),
             **kwargs,
         )
+
+    def start(self, **kwargs):
+        self.dcmweb_helper = HelperDcmWeb(dag_run=kwargs["dag_run"])
+
+        run_dir = (
+            Path(self.airflow_workflow_dir) / kwargs["dag_run"].run_id / self.batch_name
+        )
+        # Creates a generator object of all the folders in the run directory
+        batch_folders = run_dir.glob("*")
+
+        for batch_element_dir in batch_folders:
+            element_input_dir = os.path.join(batch_element_dir, self.operator_in_dir)
+            print("Processing directory:", element_input_dir)
+
+            # upload_dcm_files does a walk through the directory and uploads all the dcm files
+            self.dcmweb_helper.upload_dcm_files(element_input_dir)

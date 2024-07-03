3f280b2be7f58961c0f411fe0d06521ca0255479
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-07-03 11:50:01 +0200

    Database init

--- services/data-separation/dicom-web-filter/docker/files/app/main.py
+++ services/data-separation/dicom-web-filter/docker/files/app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Request
+from contextlib import asynccontextmanager
+from .models import Base
+from .database import async_engine
 from .QIDO_RS.routes import router as qido_router
 from .STOW_RS.routes import router as stow_router
-
-# from .UPS_RS.routes import router as ups_router
 from .WADO_RS.routes import router as wado_router
 from .CUSTOM_RS.routes import router as custom_router
 from .WADO_URI.routes import router as wado_uri_router
@@ -10,6 +11,16 @@ from .proxy_request import proxy_request
 import logging
 
 logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Create tables
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield  # This yield separates startup from shutdown code
+    # Code here would run after the application stops
+
 
 tags_metadata = [
     {
@@ -53,7 +64,6 @@ app = FastAPI(
 
 app.include_router(qido_router)
 app.include_router(stow_router)
-# app.include_router(ups_router) TODO: Test properly, currently no usecase
 app.include_router(wado_router)
 app.include_router(custom_router)
 app.include_router(wado_uri_router, prefix="/wado-uri")

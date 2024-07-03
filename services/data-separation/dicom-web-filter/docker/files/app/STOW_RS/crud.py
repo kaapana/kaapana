81a838b7b4c52a66901de7684b9fe0d24230ec1b
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-07-03 17:48:34 +0200

    Basic database connectivity for STOW

---
+++ services/data-separation/dicom-web-filter/docker/files/app/STOW_RS/crud.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from ..models import (
+    DicomData,
+    DataProjects,
+)
+
+
+async def add_dicom_data(
+    session: AsyncSession,
+    series_instance_uid: str,
+    study_instance_uid: str,
+    description: str,
+) -> DicomData:
+    new_data = DicomData(
+        series_instance_uid=series_instance_uid,
+        study_instance_uid=study_instance_uid,
+        description=description,
+    )
+    session.add(new_data)
+    await session.commit()
+    return new_data
+
+
+async def add_data_project_mapping(
+    session: AsyncSession, series_instance_uid: str, project_id: int
+) -> DataProjects:
+    new_mapping = DataProjects(
+        series_instance_uid=series_instance_uid, project_id=project_id
+    )
+    session.add(new_mapping)
+    await session.commit()
+    return new_mapping

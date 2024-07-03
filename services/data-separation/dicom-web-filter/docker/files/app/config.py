d410f00493e75f2ed37a4f058defa9b7c2cca8ca
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-07-03 11:50:01 +0200

    Database init

--- services/data-separation/dicom-web-filter/docker/files/app/config.py
+++ services/data-separation/dicom-web-filter/docker/files/app/config.py
@@ -2,3 +2,9 @@ import os
 
 DICOMWEB_BASE_URL = os.environ["DICOMWEB_BASE_URL"]
 DICOMWEB_BASE_URL_WADO_URI = os.environ["DICOMWEB_BASE_URL_WADO_URI"]
+
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+POSTGRES_HOST = f"aip-postgres-service.services.svc"
+POSTGRES_PORT = f"5432"
+DATABASE_NAME = "access_information_point"

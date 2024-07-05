aebba37f89fd933b979bb38bb85332931091ad5a
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-07-05 11:33:23 +0200

    Filtering todos

--- services/data-separation/dicom-web-filter/docker/files/app/QIDO_RS/routes.py
+++ services/data-separation/dicom-web-filter/docker/files/app/QIDO_RS/routes.py
@@ -6,16 +6,25 @@ router = APIRouter()
 
 @router.get("/studies", tags=["QIDO-RS"])
 async def query_studies(request: Request):
+
+    #TODO: Only return studies that are mapped to the project
+
     return await proxy_request(request, "/studies", "GET")
 
 
 @router.get("/studies/{study}/series", tags=["QIDO-RS"])
 async def query_series(study: str, request: Request):
+
+    #TODO: Only return series of the study that are mapped to the project
+
     return await proxy_request(request, f"/studies/{study}/series", "GET")
 
 
 @router.get("/studies/{study}/series/{series}/instances", tags=["QIDO-RS"])
 async def query_instances(study: str, series: str, request: Request):
+
+    # TODO: Only return instances of the series that are mapped to the project (Filtering is on series level)
+
     return await proxy_request(
         request, f"/studies/{study}/series/{series}/instances", "GET"
     )

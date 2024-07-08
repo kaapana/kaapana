7ae77458efa49ec3f4f4ba78cf6200aef77c0b2e
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-07-08 11:11:06 +0200

    Route /studies/{study}/series filtering

--- services/data-separation/dicom-web-filter/docker/files/app/QIDO_RS/crud.py
+++ services/data-separation/dicom-web-filter/docker/files/app/QIDO_RS/crud.py
@@ -4,9 +4,10 @@ from ..models import (
     DataProjects,
 )
 from sqlalchemy import select
+from typing import List
 
 
-async def get_all_studies_mapped_to_project(session: AsyncSession, project_id: int):
+async def get_all_studies_mapped_to_project(session: AsyncSession, project_id: int) -> List[str]:
     stmt = (
         select(DicomData.study_instance_uid)
         .join(
@@ -20,7 +21,7 @@ async def get_all_studies_mapped_to_project(session: AsyncSession, project_id: i
     return studies
 
 
-async def get_all_series_mapped_to_project(session: AsyncSession, project_id: int):
+async def get_all_series_mapped_to_project(session: AsyncSession, project_id: int) -> List[str]:
     stmt = (
         select(DicomData.series_instance_uid)
         .join(
@@ -28,6 +29,20 @@ async def get_all_series_mapped_to_project(session: AsyncSession, project_id: in
             DataProjects.series_instance_uid == DicomData.series_instance_uid,
         )
         .where(DataProjects.project_id == project_id)
+    )
+    result = await session.execute(stmt)
+    series = result.scalars().all()
+    return series
+
+async def get_series_instance_uids_of_study_which_are_mapped_to_project(session: AsyncSession, project_id: int, study_instance_uid: str) -> List[str]:
+    stmt = (
+        select(DicomData.series_instance_uid)
+        .join(
+            DataProjects,
+            DataProjects.series_instance_uid == DicomData.series_instance_uid,
+        )
+        .where(DataProjects.project_id == project_id)
+        .where(DicomData.study_instance_uid == study_instance_uid)
     )
     result = await session.execute(stmt)
     series = result.scalars().all()

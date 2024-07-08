39a12bf90c1bb4cc9edb3bb6f5c95bf0f7c55c0d
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-07-08 11:11:06 +0200

    Route /studies/{study}/series filtering

--- services/data-separation/dicom-web-filter/docker/files/app/proxy_request.py
+++ services/data-separation/dicom-web-filter/docker/files/app/proxy_request.py
@@ -5,7 +5,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-logging.basicConfig(level=logging.WARNING)  # TODO: Change before deployment
+logging.basicConfig(level=logging.INFO)  # TODO: Change before deployment
 
 
 async def proxy_request(

f1cec150637f507d3e338fd9b0ba90682fcb82a0
Ünal Akünal <uenal.akuenal@dkfz-heidelberg.de>
2024-07-09 16:21:43 +0200

    templates_and_examples: convert ndarray to list in pyradiomics features for json dump

--- templates_and_examples/examples/processing-pipelines/pyradiomics-feature-extractor/processing-containers/pyradiomics-feature-extractor/files/extract_features.py
+++ templates_and_examples/examples/processing-pipelines/pyradiomics-feature-extractor/processing-containers/pyradiomics-feature-extractor/files/extract_features.py
@@ -1,6 +1,7 @@
 import os
 import ast
 import json
+import numpy as np
 import SimpleITK as sitk
 from radiomics import featureextractor
 
@@ -19,7 +20,7 @@ def get_env_str(env_var, can_be_empty=False) -> str:
     Raises:
         AssertionError: If the environment variable is empty and can_be_empty is False.
     """
-    val = os.getenv(env_var, "")
+    val = os.getenv(env_var.upper(), "")
 
     if can_be_empty is not True:
         assert val != "", f"ERROR: {env_var} is passed as empty, aborting"
@@ -40,35 +41,38 @@ def get_env_list(env_var):
 
     value_str = get_env_str(
         env_var
-    )  # get the actual values as a str, i.e. "['firstorder', 'shape2D']"
+    )  # get the actual values as a str, e.g. "['firstorder', 'shape2D']"
     value_literal = (
         ast.literal_eval(value_str) if value_str.lower() != "none" else None
-    )  # convert string to python expression, i.e. ['firstorder', 'shape2D']
+    )  # convert string to python expression, e.g. ['firstorder', 'shape2D']
     assert value_literal is not None
 
     value_list: list = list(
         value_literal
-    )  # make it an actual list, i.e. ['firstorder', 'shape2D']
+    )  # make it an actual list, e.g. ['firstorder', 'shape2D']
     return value_list
 
 
-def get_paths():
+def get_batch_path():
     """
-    Set up input and output directories based on environment variables.
-    These directory values are passed by default via Kaapana Airflow plugin.
+    Set up input batch directory based on environment variables.
+    Batch dir contains multiple series uids folders, and each series contains operators as subfolders.
+    Example:
+    * batch_path = /kaapana/mounted/data/pyradiomics-extract-features-<time>/batch/
+        * batch_path/<series_ins_uid_1>/get_input_data/*.dcm
+        * batch_path/<series_ins_uid_1>/dcm_converter/<series_ins_uid_1>.nii.gz
+        * batch_path/<series_ins_uid_2>/dcm_converter/<series_ins_uid_2>.nii.gz
 
     Returns:
-        tuple: Input and output paths.
+        str: Batch path to iterate over for series uid folders.
     """
 
     workflow_dir = get_env_str("WORKFLOW_DIR")
-    in_dir = get_env_str("OPERATOR_IN_DIR")
-    out_dir = get_env_str("OPERATOR_OUT_DIR")
+    batch_dir = get_env_str("BATCH_NAME")
 
-    in_path = os.path.join("/", workflow_dir, in_dir)
-    out_path = os.path.join("/", workflow_dir, out_dir)
+    batch_path = os.path.join("/", workflow_dir, batch_dir)
 
-    return in_path, out_path
+    return batch_path
 
 
 def get_feature_classes():
@@ -79,19 +83,39 @@ def get_feature_classes():
     Returns:
         list: List of selected feature classes.
     """
-    # this env var specifies the property key inside the UI form, i.e. FEATURE_CLASS_PROP_KEY="feature_classes"
+    # this env var specifies the property key inside the UI form, e.g. FEATURE_CLASS_PROP_KEY="feature_classes"
     feature_classes_key = get_env_str("FEATURE_CLASS_PROP_KEY")
 
-    # get the actual list stored in key, i.e. FEATURE_CLASSES=""['firstorder', 'shape2D']"
+    # get the actual list stored in key, e.g. FEATURE_CLASSES=""['firstorder', 'shape2D']"
     feature_classes = get_env_list(feature_classes_key)
 
     return feature_classes
 
+def convert_ndarray_to_list(d):
+    """
+    Convert any numpy ndarrays in a dictionary to lists.
+    Useful for converting some of the output of pyradiomics feature extractor.
+    """
+    for key, value in d.items():
+        if isinstance(value, np.ndarray):
+            d[key] = value.tolist()
+        elif isinstance(value, dict):
+            d[key] = convert_ndarray_to_list(value)
+    return d
+
 
 # Main script execution starts here
 
-### PATHS ###
-in_path, out_path = get_paths()
+### PATHS & DIRS ###
+batch_path = (
+    get_batch_path()
+)  # i.e. /kaapana/mounted/data/pyradiomics-extract-features-<time>/batch/
+in_operator = get_env_str(
+    "OPERATOR_IN_DIR"
+)  # Name of the input operator, e.g. dcm-converter
+out_operator = get_env_str(
+    "OPERATOR_OUT_DIR"
+)  # Name of this operator as the output operator, e.g. pyradiomics-feature-extractor
 
 
 ### FEATURE CLASSES ###
@@ -102,25 +126,37 @@ feature_classes = get_feature_classes()
 feature_classes_dict = {feature_class: [] for feature_class in feature_classes}
 extractor = featureextractor.RadiomicsFeatureExtractor(**feature_classes_dict)
 
-# Process each nifti in the input folder
-for file_name in os.listdir(in_path):
-    if file_name.endswith(".nii") or file_name.endswith(".nii.gz"):
-        print(f"extracting radiomics features of {file_name}")
-        file_path = os.path.join(in_path, file_name)
+# Go through each data sample based on series instance uid
+for series_ins_uid in os.listdir(batch_path):
+    print(f"processing {series_ins_uid}")
+    # generate input & output paths, e.g. /kaapana/mounted/data/pyradiomics-extract-features-<time>/batch/<series_ins_uid>/dcm-converter
+    in_path = os.path.join(batch_path, series_ins_uid, in_operator)
+    out_path = os.path.join(batch_path, series_ins_uid, out_operator)
+    os.makedirs(out_path, exist_ok=True)  # Create output directory if it does not exist
 
-        # load image
-        image = sitk.ReadImage(file_path)
+    # Process every nifti file under input path
+    for file_name in os.listdir(in_path):
+        if file_name.endswith(".nii") or file_name.endswith(".nii.gz"):
+            print(f"extracting radiomics features of {file_name}")
+            file_path = os.path.join(in_path, file_name)
 
-        # extract features (using the entire image)
-        features = extractor.execute(image, image)
+            # load image
+            image = sitk.ReadImage(file_path)
 
-        # save features as json
-        output_file = os.path.join(
-            out_path, f"{os.path.splitext(file_name)[0]}_features.json"
-        )
-        with open(output_file, "w") as json_file:
-            json.dump(features, json_file, indent=4)
+            # extract features (using the entire image)
+            features = extractor.execute(image, image)
 
-        print(
-            f"PyRadiomics feature extraction completed for {file_name}, results saved in {output_file}"
-        )
+            # convert all ndarrays in features to lists for json serialization
+            features = convert_ndarray_to_list(features)
+
+            # save features as json
+            output_file = os.path.join(
+                out_path, f"{series_ins_uid}_features.json"
+            )
+            print(f"output_file {output_file}")
+            with open(output_file, "w") as json_file:
+                json.dump(features, json_file, indent=4)
+
+            print(
+                f"PyRadiomics feature extraction completed for {file_name}, results saved in {output_file}"
+            )

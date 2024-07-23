75985ba5431940fdfbb337a5357e068b1c8f5f5f
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-07-23 10:05:35 +0200

    init wado uri

--- services/data-separation/dicom-web-filter/docker/files/app/WADO_URI/routes.py
+++ services/data-separation/dicom-web-filter/docker/files/app/WADO_URI/routes.py
@@ -1,6 +1,13 @@
-from fastapi import APIRouter, Request
 from ..proxy_request import proxy_request
-from ..config import DICOMWEB_BASE_URL_WADO_URI
+from ..database import get_session
+from . import crud
+from ..config import DEFAULT_PROJECT_ID, DICOMWEB_BASE_URL, DICOMWEB_BASE_URL_WADO_URI
+from sqlalchemy.ext.asyncio import AsyncSession
+import re
+import httpx
+import debugpy
+from fastapi import APIRouter, Request, Depends, Response
+from fastapi.responses import StreamingResponse
 
 router = APIRouter()
 
@@ -8,8 +15,16 @@ router = APIRouter()
 
 
 @router.get("/wado", tags=["WADO-URI"])
-async def retrieve_instance(request: Request):
+async def retrieve_instance(
+    request: Request, session: AsyncSession = Depends(get_session)
+):
 
-    # TODO: Ensure that the query is only executed within the series that are mapped to the project (or studies if whole studies are mapped to the project)
+    # check if StudyInstanceUID is in the query parameters
+    if "StudyInstanceUID" in request.query_params:
+        pass
+
+    # check if SeriesInstanceUID is in the query parameters
+    if "SeriesInstanceUID" in request.query_params:
+        pass
 
     return await proxy_request(request, "", "GET", DICOMWEB_BASE_URL_WADO_URI)

de1a55c918791321c7d6eee6f3630cb5b6a46bd0
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-07-27 14:07:11 +0200

    WADO URI init

--- services/data-separation/dicom-web-filter/docker/files/app/WADO_URI/crud.py
+++ services/data-separation/dicom-web-filter/docker/files/app/WADO_URI/crud.py
@@ -7,6 +7,22 @@ from sqlalchemy import select
 from typing import List
 
 
+async def get_all_studies_mapped_to_project(
+    session: AsyncSession, project_id: int
+) -> List[str]:
+    stmt = (
+        select(DicomData.study_instance_uid)
+        .join(
+            DataProjects,
+            DataProjects.series_instance_uid == DicomData.series_instance_uid,
+        )
+        .where(DataProjects.project_id == project_id)
+    )
+    result = await session.execute(stmt)
+    studies = result.scalars().all()
+    return studies
+
+
 async def get_series_instance_uids_of_study_which_are_mapped_to_project(
     session: AsyncSession, project_id: int, study_instance_uid: str
 ) -> List[str]:
@@ -22,35 +38,3 @@ async def get_series_instance_uids_of_study_which_are_mapped_to_project(
     result = await session.execute(stmt)
     series = result.scalars().all()
     return series
-
-
-async def get_all_series_of_study(
-    session: AsyncSession, study_instance_uid: str
-) -> List[str]:
-    stmt = select(DicomData.series_instance_uid).where(
-        DicomData.study_instance_uid == study_instance_uid
-    )
-    result = await session.execute(stmt)
-    series = result.scalars().all()
-    return series
-
-
-async def check_if_series_in_given_study_is_mapped_to_project(
-    session: AsyncSession,
-    project_id: int,
-    study_instance_uid: str,
-    series_instance_uid: str,
-) -> bool:
-    stmt = (
-        select(DicomData.series_instance_uid)
-        .join(
-            DataProjects,
-            DataProjects.series_instance_uid == DicomData.series_instance_uid,
-        )
-        .where(DataProjects.project_id == project_id)
-        .where(DicomData.study_instance_uid == study_instance_uid)
-        .where(DicomData.series_instance_uid == series_instance_uid)
-    )
-    result = await session.execute(stmt)
-    series = result.scalars().all()
-    return bool(series)  # If the series is found, return True, else False

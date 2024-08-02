72c24f01d64e6e54b10bccff1996edcd1ecea95a
rajesh.baidya <rajesh.baidya@dkfz-heidelberg.de>
2024-08-02 16:04:51 +0200

    workflow properties converted to snakecase optional param

--- services/base/kaapana-backend/docker/files/app/workflows/routers/client.py
+++ services/base/kaapana-backend/docker/files/app/workflows/routers/client.py
@@ -817,7 +817,9 @@ def get_settings(db: Session = Depends(get_db)):
 
 # get workflow settings of a given dag for instance
 @router.get("/settings/workflows/{dag_id}", response_model=schemas.SettingsBase)
-def get_workflow_settings(dag_id: str, db: Session = Depends(get_db)):
+def get_workflow_settings(
+    dag_id: str, snakecase: bool = False, db: Session = Depends(get_db)
+):
     db_response = crud.get_settings_item(db, "workflows")
 
     settings_item: schemas.SettingsBase = schemas.SettingsBase(key=dag_id, value={})
@@ -825,9 +827,10 @@ def get_workflow_settings(dag_id: str, db: Session = Depends(get_db)):
         workflows = db_response.value
         dag_id_converted = camel_case(dag_id)
         if dag_id_converted in workflows:
-            settings_item = schemas.SettingsBase(
-                key=dag_id, value=workflows[dag_id_converted]
-            )
+            workflow_settings = workflows[dag_id_converted]
+            if snakecase:
+                workflow_settings = convert_dict_keys_to_snake_case(workflow_settings)
+            settings_item = schemas.SettingsBase(key=dag_id, value=workflow_settings)
 
     return settings_item
 
@@ -842,6 +845,17 @@ def create_or_update_settings(
     return True
 
 
+def convert_dict_keys_to_snake_case(target_dict):
+    new_dict = {}
+    for key in target_dict.keys():
+        new_key = snake_case(key)
+        if isinstance(target_dict[key], dict):
+            new_dict[new_key] = convert_dict_keys_to_snake_case(target_dict[key])
+        else:
+            new_dict[new_key] = target_dict[key]
+    return new_dict
+
+
 # Define a function to convert a string to camel case
 def camel_case(s):
     # Use regular expression substitution to replace underscores and hyphens with spaces,
@@ -850,3 +864,8 @@ def camel_case(s):
 
     # Join the string, ensuring the first letter is lowercase
     return "".join([s[0].lower(), s[1:]])
+
+
+def snake_case(name):
+    name = sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
+    return sub("([a-z0-9])([A-Z])", r"\1_\2", name).lower()

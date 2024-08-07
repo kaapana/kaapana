4bbe20dee4199dfee484c28e38be3138f3f76f2b
Lorenz Feineis <lorenz.feineis@dkfz-heidelberg.de>
2024-08-07 13:29:53 +0200

    Add project header to all requests made from the frontend; Remove redundant axios instance structure.

--- services/utils/base-landing-page/files/kaapana_app/src/common/auth.service.ts
+++ services/utils/base-landing-page/files/kaapana_app/src/common/auth.service.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import request from '@/request'
+import httpClient from './httpClient'
 
 const AuthService = {
   getToken() {
@@ -10,7 +10,7 @@ const AuthService = {
       } else {
         oauthUrl = '/jsons/testingAuthenticationToken.json'
       }
-      request.get(oauthUrl).then((response: any) =>  {
+      httpClient.get(oauthUrl).then((response: any) =>  {
         resolve(response.data)
       }).catch((error: any) => {
         console.log('not token there', error)
@@ -24,7 +24,7 @@ const AuthService = {
   },
   getFederatedHeaders() {
     return new Promise((resolve, reject) => {
-      request.get('/kaapana-backend/client/kaapana-instance').then((response: any) =>  {
+      httpClient.get('/kaapana-backend/client/kaapana-instance').then((response: any) =>  {
         resolve({
           FederatedAuthorization: response.data['token']
         })

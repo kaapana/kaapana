2761a318a37017f83d5cac1f67e98ffab00f62cb
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-09-03 10:45:39 +0200

    Docstring auth middleware

--- services/data-separation/dicom-web-filter/docker/files/app/auth_middleware.py
+++ services/data-separation/dicom-web-filter/docker/files/app/auth_middleware.py
@@ -9,6 +9,12 @@ logger = logging.getLogger(__name__)
 
 
 class AuthMiddleware(BaseHTTPMiddleware):
+    """Middleware for authenticating requests using OpenID Connect.
+
+    Args:
+        BaseHTTPMiddleware (class): The base class for HTTP middleware.
+    """
+
     def __init__(self, app, config_url: str, client_id: str):
         super().__init__(app)
         self.config_url = config_url
@@ -18,6 +24,7 @@ class AuthMiddleware(BaseHTTPMiddleware):
         self.jwks_client = None
 
     async def load_openid_config(self):
+        """Load OpenID configuration from the provided URL."""
         async with httpx.AsyncClient() as client:
             response = await client.get(self.config_url)
             response.raise_for_status()
@@ -27,6 +34,17 @@ class AuthMiddleware(BaseHTTPMiddleware):
             self.jwks_client = PyJWKClient(self.jwks_url)
 
     async def authenticate(self, token: str):
+        """Authenticate the user using the provided token.
+
+        Args:
+            token (str): The token to authenticate the user with.
+
+        Raises:
+            HTTPException: If the token is invalid or the claims are missing.
+
+        Returns:
+            dict: The payload of the token.
+        """
         if not self.jwks_url or not self.issuer or not self.jwks_client:
             await self.load_openid_config()
 
@@ -45,6 +63,18 @@ class AuthMiddleware(BaseHTTPMiddleware):
             raise HTTPException(status_code=403, detail="Invalid token or claims")
 
     async def dispatch(self, request: Request, call_next):
+        """Dispatch the request to the next middleware in the stack.
+
+        Args:
+            request (Request): The incoming request.
+            call_next (function): The next middleware in the stack or the endpoint.
+
+        Raises:
+            HTTPException: If the token is missing or invalid.
+
+        Returns:
+            Response: The response from the next middleware or the endpoint.
+        """
         auth_header = request.headers.get("Authorization")
         if auth_header and auth_header.startswith("Bearer "):
             token = auth_header.split(" ")[1]

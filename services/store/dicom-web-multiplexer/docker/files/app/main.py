1c2d4be4e6b178557cd094795671c84aa0fca77d
Mikulas Bankovic <mikulas.bankovic27@gmail.com>
2024-09-09 11:01:31 +0200

    [WIP] Build pass Deployment pass Extension Installed Functionality missing

--- services/store/dicom-web-multiplexer/docker/files/app/main.py
+++ services/store/dicom-web-multiplexer/docker/files/app/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Request, Response
-from proxy_request import proxy_request
+from fastapi import FastAPI
 
 app = FastAPI(
     root_path="/dicom-web-multiplexer",
@@ -10,29 +9,29 @@ app = FastAPI(
 )
 
 
-async def query_opensearch(series_uid: str):
-    """
-    Mock OpenSearch call. Replace this with actual OpenSearch querying logic.
-    Returns:
-    - 'local': for local processing
-    - 'remote': for remote processing
-    """
-    # Placeholder logic, return 'local' or 'remote' based on some condition.
-    if series_uid.startswith("1.2"):
-        return "local"
-    return "remote"
+# async def query_opensearch(series_uid: str):
+#     """
+#     Mock OpenSearch call. Replace this with actual OpenSearch querying logic.
+#     Returns:
+#     - 'local': for local processing
+#     - 'remote': for remote processing
+#     """
+#     # Placeholder logic, return 'local' or 'remote' based on some condition.
+#     if series_uid.startswith("1.2"):
+#         return "local"
+#     return "remote"
 
 
-@app.middleware("http")
-async def process_request_middleware(request: Request):
-    series_uid = request.query_params.get("SeriesUID", "default")
-    pacs = await query_opensearch(series_uid)
+# @app.middleware("http")
+# async def process_request_middleware(request: Request):
+#     series_uid = request.query_params.get("SeriesUID", "default")
+#     pacs = await query_opensearch(series_uid)
 
-    if pacs == "local":
-        return proxy_request(request, str(request.url), request.method, timeout=10)
-    elif pacs == "google":
-        print("Google Dcm Web")
-    else:
-        raise NotImplementedError(
-            "This will be implemented in the future. Currently supported PACS are: google, dcm4chee"
-        )
+#     if pacs == "local":
+#         return proxy_request(request, str(request.url), request.method, timeout=10)
+#     elif pacs == "google":
+#         print("Google Dcm Web")
+#     else:
+#         raise NotImplementedError(
+#             "This will be implemented in the future. Currently supported PACS are: google, dcm4chee"
+#         )

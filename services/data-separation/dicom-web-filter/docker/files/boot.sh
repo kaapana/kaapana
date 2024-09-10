8927a5116c958d4d17360aa774afac7dd0a8ee23
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-09-10 15:40:17 +0200

    switch dicom-web-filter to gunicorn for production

--- services/data-separation/dicom-web-filter/docker/files/boot.sh
+++ services/data-separation/dicom-web-filter/docker/files/boot.sh
@@ -2,6 +2,10 @@
 
 export PYTHONPATH="$PWD" 
 APPLICATION_ROOT="/dicom-web-filter"
-# forwarded-allow-ips  inc
-uvicorn app.main:app --reload --host 0.0.0.0 --port $PORT --workers $WORKERS --root-path $APPLICATION_ROOT --forwarded-allow-ips '*'
-# SCRIPT_NAME=$APPLICATION_ROOT gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT --access-logfile - --error-logfile -
+
+# Production
+echo "Running at $APPLICATION_ROOT"
+SCRIPT_NAME=$APPLICATION_ROOT gunicorn app.main:app --workers $WORKERS --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT --access-logfile - --error-logfile - 
+
+# Development
+# uvicorn app.main:app --reload --host 0.0.0.0 --port $PORT --workers $WORKERS --root-path $APPLICATION_ROOT --forwarded-allow-ips '*'

9ef5c2e160d52da440c140573cefb718d3d24c47
Mikulas Bankovic <mikulas.bankovic27@gmail.com>
2024-09-11 13:11:15 +0200

    Extension installed Ingress redirected [WIP] Multiplexer Dev Mode

--- services/store/dicom-web-multiplexer/docker/files/app/main.py
+++ services/store/dicom-web-multiplexer/docker/files/app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from .proxy_request import proxy_request
 
 app = FastAPI(
     root_path="/dicom-web-multiplexer",
@@ -9,29 +11,28 @@ app = FastAPI(
 )
 
 
-# async def query_opensearch(series_uid: str):
-#     """
-#     Mock OpenSearch call. Replace this with actual OpenSearch querying logic.
-#     Returns:
-#     - 'local': for local processing
-#     - 'remote': for remote processing
-#     """
-#     # Placeholder logic, return 'local' or 'remote' based on some condition.
-#     if series_uid.startswith("1.2"):
-#         return "local"
-#     return "remote"
+async def get_endpoint_from_opensearch(series_uid: str):
+    """
+    Mock OpenSearch call. Replace this with actual OpenSearch querying logic.
+    Returns:
+    - 'local': for local processing
+    - 'remote': for remote processing
+    """
+    # Placeholder logic, return 'local' or 'remote' based on some condition.
+    return "Local"
 
 
-# @app.middleware("http")
-# async def process_request_middleware(request: Request):
-#     series_uid = request.query_params.get("SeriesUID", "default")
-#     pacs = await query_opensearch(series_uid)
+@app.middleware("http")
+async def process_request_middleware(request: Request):
 
-#     if pacs == "local":
-#         return proxy_request(request, str(request.url), request.method, timeout=10)
-#     elif pacs == "google":
-#         print("Google Dcm Web")
-#     else:
-#         raise NotImplementedError(
-#             "This will be implemented in the future. Currently supported PACS are: google, dcm4chee"
-#         )
+    series_uid = request.query_params.get("SeriesUID", "default")
+    endpoint = await get_endpoint_from_opensearch(series_uid)
+
+    if endpoint == "local":
+        return proxy_request(request, str(request.url), request.method, timeout=10)
+    elif endpoint == "google":
+        print("Google Dcm Web")
+    else:
+        raise NotImplementedError(
+            "This will be implemented in the future. Currently supported PACS are: google, dcm4chee"
+        )

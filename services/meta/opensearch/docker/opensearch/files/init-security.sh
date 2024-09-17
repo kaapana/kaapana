b0097d87b4ae6b1e3321e2b7fea7214a6ad6ccfc
Lorenz Feineis <lorenz.feineis@dkfz-heidelberg.de>
2024-09-17 15:20:23 +0200

    Persist security configuration of opensearch such as roles and rolemappings in persistentVolume; Initialize opensearch security on this data if data exists. Otherwise init from congimap.

--- services/meta/opensearch/docker/opensearch/files/init-security.sh
+++ services/meta/opensearch/docker/opensearch/files/init-security.sh
@@ -4,4 +4,15 @@ echo "OpenSearch not available yet, retrying..."
 sleep 5
 done
 echo "OpenSearch is now available!"
-/usr/share/opensearch/plugins/opensearch-security/tools/securityadmin.sh -icl -nhnv -cacert /usr/share/opensearch/config/root-ca.pem -cert /usr/share/opensearch/config/admin.pem -key /usr/share/opensearch/config/admin-key.pem -cd /usr/share/opensearch/config/opensearch-security --hostname opensearch-service.${SERVICES_NAMESPACE}.svc
+CONFIG_DIR="/usr/share/opensearch/config/"
+OPENSEARCH_BACK_UP_DIR="/kaapana/opensearch-security-backup"
+
+if [ -d "${OPENSEARCH_BACK_UP_DIR}" ] && [ "$(ls -A ${OPENSEARCH_BACK_UP_DIR})" ]; then
+  echo "Init security extension with back up files."
+  SECURITY_CONFIG_DIR="${OPENSEARCH_BACK_UP_DIR}"
+else
+  echo "Init security extension from scratch."
+  SECURITY_CONFIG_DIR="${CONFIG_DIR}/opensearch-security"
+fi
+
+/usr/share/opensearch/plugins/opensearch-security/tools/securityadmin.sh -icl -nhnv -cacert ${CONFIG_DIR}/root-ca.pem -cert ${CONFIG_DIR}/admin.pem -key ${CONFIG_DIR}/admin-key.pem -cd ${SECURITY_CONFIG_DIR} --hostname opensearch-service.${SERVICES_NAMESPACE}.svc

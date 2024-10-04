c98c80d94f67124a0ce18ad41e2359468dca6a0b
xbankov <mikulas.bankovic27@gmail.com>
2024-10-04 18:23:14 +0200

    [WIP] Proxy through multiplexer fixed

---
+++ services/store/dicom-web-multiplexer/docker/files/app/utils.py
@@ -0,0 +1,10 @@
+# from fastapi import Request
+# from kaapanapy.helper import HelperOpensearch
+
+# def get_endpoint(request: Request):
+#     str(request.url).split(["study", "series"])
+#     # study_uid = 
+#     # series_uid = 
+#     os_helper = HelperOpensearch()
+#     os_helper.get_dcm_uid_objects([series_instance_uids])
+    

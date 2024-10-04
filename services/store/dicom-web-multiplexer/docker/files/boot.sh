746f68515a3b62f83ebbad4256351728d1928757
xbankov <mikulas.bankovic27@gmail.com>
2024-10-04 14:44:54 +0200

    Remove IngressRoute and use just Ingress

--- services/store/dicom-web-multiplexer/docker/files/boot.sh
+++ services/store/dicom-web-multiplexer/docker/files/boot.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 export PYTHONPATH="$PWD"
-APPLICATION_ROOT="/dicom-web-multiplexer"
 
-uvicorn app.main:app --reload --host 0.0.0.0 --port $PORT --workers $WORKERS --root-path $APPLICATION_ROOT --forwarded-allow-ips '*'
+uvicorn app.main:app --reload --host 0.0.0.0 --port $PORT --workers $WORKERS --forwarded-allow-ips '*'

1838625d40f3d5b659c57e424c2bd73104861ea2
Philipp Schader <philipp.schader@dkfz-heidelberg.de>
2024-10-08 18:51:41 +0200

    Add API instrumentation

--- services/kaapana-admin/kube-helm/docker/files/backend/app/main.py
+++ services/kaapana-admin/kube-helm/docker/files/backend/app/main.py
@@ -1,13 +1,12 @@
 from os.path import dirname, join
-import uvicorn
 
+import uvicorn
+from config import settings
 from fastapi import APIRouter, FastAPI
 from fastapi.staticfiles import StaticFiles
-# from fastapi.middleware.cors import CORSMiddleware
-
-from routes import router
-from config import settings
 from helm_helper import get_extensions_list
+from prometheus_fastapi_instrumentator import Instrumentator
+from routes import router
 
 app = FastAPI(title="Kube-Helm API", root_path=settings.application_root)
 
@@ -17,7 +16,7 @@ app.mount(
     StaticFiles(directory=join(dirname(str(__file__)), "static")),
     name="static",
 )
-
+Instrumentator().instrument(app).expose(app)
 
 
 if __name__ == "__main__":

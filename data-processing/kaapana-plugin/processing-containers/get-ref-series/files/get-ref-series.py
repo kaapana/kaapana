f8577b727cc97933412a2af310702a5f4dc81a2e
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-10-09 18:10:31 +0200

    Implemented missing query options study_uid and OpenSearch search_query

--- data-processing/kaapana-plugin/processing-containers/get-ref-series/files/get-ref-series.py
+++ data-processing/kaapana-plugin/processing-containers/get-ref-series/files/get-ref-series.py
@@ -1,3 +1,4 @@
+import ast
 import glob
 import json
 import logging
@@ -24,6 +25,43 @@ class DownloadSeries:
     target_dir: str
 
 
+def build_opensearch_query(modalities: list = [], custom_tags: list = []) -> dict:
+    """Build an OpenSearch query based on the given modalities and custom tags.
+
+    Args:
+        modalities (list, optional): Given modalities. Defaults to []. Must be list of strings.
+        custom_tags (list, optional): Given custom tags. Defaults to []. Must be list of strings.
+
+    Returns:
+        dict: The OpenSearch query.
+    """
+    # Base of the OpenSearch query
+    query = {"bool": {"must": []}}
+
+    # If 'modalities' is filled, we add a condition to check "00080060 Modality_keyword"
+    if modalities:
+        query["bool"]["should"] = []
+        for modality in modalities:
+            query["bool"]["should"].append(
+                {"match": {"00080060 Modality_keyword": modality}}
+            )
+
+    # If 'custom_tags' is filled, we add a 'terms_set' query to check "00000000 Tags_keyword" contains all tags
+    if custom_tags:
+        tags_query = {
+            "terms_set": {
+                "00000000 Tags_keyword": {
+                    "terms": custom_tags,
+                    "minimum_should_match_script": {"source": "params.num_terms"},
+                }
+            }
+        }
+        query["bool"]["must"].append(tags_query)
+
+    # Return the final query structure
+    return query
+
+
 class GetRefSeriesOperator:
     """
     Operator to get DICOM series.
@@ -34,32 +72,75 @@ class GetRefSeriesOperator:
 
     def __init__(
         self,
-        search_policy: str = "reference_uid",  # reference_uid, study_uid, patient_uid
+        search_policy: str = "reference_uid",
         data_type: str = "dicom",
-        dicom_tags: list = [],  # list of dicom tags to filter the series
+        search_query: dict = {},
         parallel_downloads: int = 3,
+        modalities: list = [],
+        custom_tags: list = [],
     ):
-        if data_type not in ["dicom", "json"]:
-            raise ValueError("Unknown data-type!")
-        if search_policy not in ["reference_uid", "study_uid", "patient_uid"]:
-            raise ValueError("Unknown search policy!")
+        """Initialize the GetRefSeriesOperator.
 
-        if search_policy in ["study_uid", "patient_uid"]:
-            raise NotImplementedError(
-                "search_policy 'study_uid' and 'patient_uid' are not implemented yet! Please use search_policy 'reference_uid' instead."
-            )
-        if not dicom_tags == []:
-            raise NotImplementedError("dicom_tags is not implemented yet!")
+        Args:
+            search_policy (str, optional): The search policy to filter the series. Can be "reference_uid", "study_uid" or "search_query". Defaults to "reference_uid". "reference_uid" downloads the reference series of a SEG or RTSTRUCT, "study_uid" downloads all series of a study, "search_query" downloads all series based on a search query.
+            data_type (str, optional): The type of data to download. Can be "dicom" or "json". Defaults to "dicom".
+            search_query (list, optional): The search query for opensearch. Syntax must be the same as for opensearch. Defaults to {}. Must be a bool query (https://opensearch.org/docs/latest/query-dsl/compound/bool).
+            parallel_downloads (int, optional): Number of parallel downloads. Defaults to 3.
+            modalities (list, optional): List of modalities to filter the series. Defaults to []. Cant be used together with search_query. Only makes sense in conjunction with search_policy="study_uid".
+            custom_tags (list, optional): List of custom tags to filter the series. Defaults to []. Cant be used together with search_query. Only makes sense in conjunction with search_policy="study_uid".
+
+        """
+        assert data_type in ["dicom", "json"], "Unknown data-type!"
+
+        assert search_policy in [
+            "reference_uid",
+            "study_uid",
+            "search_query",
+        ], "Unknown search policy!"
+
+        assert (
+            search_policy != "search_query" or search_query != {}
+        ), "Search query must be set!"
+
+        # To avoid ambiguity:
+        # If search query is set, modality and custom tags cannot be set
+        assert search_query == {} or (
+            modalities == [] and custom_tags == []
+        ), "Modality and custom tags cannot be set if search query is set!"
+
+        # If modality is set, search query can't be set
+        assert (
+            modalities == [] or search_query == {}
+        ), "Modality cannot be set if search query is set!"
+
+        # If custom tags are set, search query can't be set
+        assert (
+            custom_tags == [] or search_query == {}
+        ), "Custom tags cannot be set if search query is set!"
 
         if data_type == "json":
             self.download_function = self.download_metadata_from_opensearch
         elif data_type == "dicom":
             self.download_function = self.download_series_from_pacs
 
-        self.search_policy = search_policy
-        self.data_type = data_type
+        if modalities != [] or custom_tags != []:
+            # Create search query
+            self.search_query = build_opensearch_query(
+                modalities=modalities, custom_tags=custom_tags
+            )
+        else:
+            self.search_query = search_query
 
+        if self.search_query != {}:
+            assert (
+                "bool" in self.search_query
+            ), "Search query must be a bool query! (https://opensearch.org/docs/latest/query-dsl/compound/bool)"
+
+            logger.info(f"Initial search query: {self.search_query}")
+
+        self.data_type = data_type
         self.parallel_downloads = parallel_downloads
+        self.search_policy = search_policy
 
     def download_metadata_from_opensearch(self, series: DownloadSeries):
         """Download metadata of a series from opensearch and store it as a json file into target_dir.
@@ -117,7 +198,91 @@ class GetRefSeriesOperator:
             f"Downloaded series {series.reference_series_uid} to {series.target_dir}"
         )
 
-    def prepare_download(
+    def prepare_download_of_study_series(
+        self,
+        path_to_dicom_slice: str,
+        workflow_dir: str,
+        batch_name: str,
+        operator_out_dir: str,
+    ) -> DownloadSeries:
+        """Prepare the download of all series of a study. Means:
+        - Load the dicom file and get the study instance uid.
+        - Find all series of the given study in OpenSearch.
+        - Create the target directories for the download.
+        - Return a list of DownloadSeries objects, which contains the information needed for the download.
+
+        Args:
+            path_to_dicom_slice (str): The path to the dicom slice.
+            workflow_dir (str): The workflow directory.
+            batch_name (str): The batch name.
+            operator_out_dir (str): The operator out directory.
+
+        Returns:
+            DownloadSeries: Dataclass containing the information needed for the download.
+        """
+
+        # Load the dicom file
+        ds = pydicom.dcmread(join(path_to_dicom_slice))
+
+        # get study instance uid
+        study_instance_uid = ds.StudyInstanceUID
+
+        logger.info(f"Study instance uid: {study_instance_uid}")
+
+        # Check if search query is set
+        if self.search_query != {}:
+            # Check if "must" is set in search query
+            if not "must" in self.search_query["bool"]:
+                self.search_query["bool"]["must"] = []
+
+            # Add study_instance_uid to search query
+            self.search_query["bool"]["must"].append(
+                {"term": {"0020000D StudyInstanceUID_keyword": study_instance_uid}}
+            )
+
+            query = self.search_query
+        else:
+            # Take all series of the given study
+            query = {
+                "term": {
+                    "0020000D StudyInstanceUID_keyword": study_instance_uid,
+                }
+            }
+
+        logger.info(f"Search query: {query}")
+
+        series_of_study = HelperOpensearch.execute_opensearch_query(query=query)
+
+        # Extract SeriesInstanceUID from each study
+        series_instance_uids = [
+            study["_source"]["0020000E SeriesInstanceUID_keyword"]
+            for study in series_of_study
+        ]
+
+        list_of_series = []
+
+        for series_instance_uid in series_instance_uids:
+            target_dir = join(
+                workflow_dir,
+                batch_name,
+                series_instance_uid,
+                operator_out_dir,
+            )
+
+            os.makedirs(target_dir, exist_ok=True)
+
+            list_of_series.append(
+                DownloadSeries(
+                    series_instance_uid=series_instance_uid,
+                    reference_series_uid=series_instance_uid,
+                    study_instance_uid=study_instance_uid,
+                    target_dir=target_dir,
+                )
+            )
+
+        return list_of_series
+
+    def prepare_download_of_ref_series(
         self,
         path_to_dicom_slice: str,
         workflow_dir: str,
@@ -183,6 +348,63 @@ class GetRefSeriesOperator:
             target_dir=target_dir,
         )
 
+    def prepare_download_of_search_query_series(
+        self,
+        search_query: dict,
+        workflow_dir: str,
+        batch_name: str,
+        operator_out_dir: str,
+    ) -> DownloadSeries:
+        """Prepare the download of series based on a search_query. Means:
+        - Get all series of the given search_query in OpenSearch.
+        - Create the target directories for the download.
+        - Return a list of DownloadSeries objects, which contains the information needed for the download.
+
+
+        Args:
+            search_query (dict): The search query.
+            workflow_dir (str): The workflow directory.
+            batch_name (str): The batch name.
+            operator_out_dir (str): The operator out directory.
+
+        Returns:
+            DownloadSeries: Dataclass containing the information needed for the download.
+        """
+
+        logger.info(f"Search query: {search_query}")
+
+        series_of_search_query = HelperOpensearch.execute_opensearch_query(
+            query=search_query
+        )
+
+        list_of_series = []
+
+        for series in series_of_search_query:
+            series_instance_uid = series["_source"][
+                "0020000E SeriesInstanceUID_keyword"
+            ]
+            study_instance_uid = series["_source"]["0020000D StudyInstanceUID_keyword"]
+
+            target_dir = join(
+                workflow_dir,
+                batch_name,
+                series_instance_uid,
+                operator_out_dir,
+            )
+
+            os.makedirs(target_dir, exist_ok=True)
+
+            list_of_series.append(
+                DownloadSeries(
+                    series_instance_uid=series_instance_uid,
+                    reference_series_uid=series_instance_uid,
+                    study_instance_uid=study_instance_uid,
+                    target_dir=target_dir,
+                )
+            )
+
+        return list_of_series
+
     def get_files(self, workflow_dir, batch_name, operator_in_dir, operator_out_dir):
         """Download series from PACS or Opensearch.
 
@@ -197,6 +419,12 @@ class GetRefSeriesOperator:
 
         series_dirs = glob.glob(join("/", workflow_dir, batch_name, "*"))
 
+        # Check if series_dirs is empty
+        if self.search_policy != "search_query":
+            assert (
+                series_dirs != [] and self.search_policy != "search_query"
+            ), "No series in the workflow directory!"
+
         download_series_list = []
 
         if self.search_policy == "reference_uid":
@@ -204,7 +432,7 @@ class GetRefSeriesOperator:
             with ThreadPoolExecutor(max_workers=self.parallel_downloads) as executor:
                 futures = [
                     executor.submit(
-                        self.prepare_download,
+                        self.prepare_download_of_ref_series,
                         join(
                             series_dir,
                             operator_in_dir,
@@ -218,6 +446,21 @@ class GetRefSeriesOperator:
                 ]
                 for future in as_completed(futures):
                     download_series_list.append(future.result())
+        elif self.search_policy == "study_uid":
+            download_series_list = self.prepare_download_of_study_series(
+                join(
+                    series_dirs[0],
+                    operator_in_dir,
+                    os.listdir(join(series_dirs[0], operator_in_dir))[0],
+                ),
+                workflow_dir,
+                batch_name,
+                operator_out_dir,
+            )
+        elif self.search_policy == "search_query":
+            download_series_list = self.prepare_download_of_search_query_series(
+                self.search_query, workflow_dir, batch_name, operator_out_dir
+            )
 
         # Download the requested data
         logging.info(f"Downloading {len(download_series_list)} series.")
@@ -248,15 +491,20 @@ if __name__ == "__main__":
     batch_name = getenv("BATCH_NAME", None)
     assert batch_name is not None, "Batch name is not set!"
 
-    operator_in_dir = getenv("OPERATOR_IN_DIR", None)
-    assert operator_in_dir is not None, "Operator in directory is not set!"
-
     operator_out_dir = getenv("OPERATOR_OUT_DIR", None)
     assert operator_out_dir is not None, "Operator out directory is not set!"
 
+    custom_tags = ast.literal_eval(getenv("CUSTOM_TAGS", "[]"))
+    modalities = ast.literal_eval(getenv("MODALITIES", "[]"))
+
     search_policy = getenv("SEARCH_POLICY", "reference_uid")
+
+    operator_in_dir = getenv("OPERATOR_IN_DIR", None)
+    if not search_policy == "search_query":
+        assert operator_in_dir is not None, "Operator in directory is not set!"
+
     data_type = getenv("DATA_TYPE", "dicom")
-    dicom_tags = json.loads(getenv("DICOM_TAGS", "[]"))
+    search_query = json.loads(getenv("SEARCH_QUERY", "{}"))
     parallel_downloads = int(getenv("PARALLEL_DOWNLOADS", 3))
 
     logger.info(f"Worflow dir: {workflow_dir}")
@@ -265,15 +513,19 @@ if __name__ == "__main__":
     logger.info(f"Operator out dir: {operator_out_dir}")
     logger.info(f"Search policy: {search_policy}")
     logger.info(f"Data type: {data_type}")
-    logger.info(f"DICOM tags: {dicom_tags}")
+    logger.info(f"Search query: {search_query}")
     logger.info(f"Parallel downloads: {parallel_downloads}")
+    logger.info(f"Modalities: {modalities}")
+    logger.info(f"Custom tags: {custom_tags}")
 
     # Start the operator
     get_ref_series_operator = GetRefSeriesOperator(
         search_policy=search_policy,
         data_type=data_type,
-        dicom_tags=dicom_tags,
+        search_query=search_query,
         parallel_downloads=parallel_downloads,
+        modalities=modalities,
+        custom_tags=custom_tags,
     )
 
     get_ref_series_operator.get_files(

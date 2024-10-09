705b2a42f73e841c807ac8cd09206ab7a7cf3eef
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-10-09 18:10:31 +0200

    Implemented missing query options study_uid and OpenSearch search_query

--- data-processing/kaapana-plugin/extension/docker/files/plugin/kaapana/operators/GetRefSeriesOperator.py
+++ data-processing/kaapana-plugin/extension/docker/files/plugin/kaapana/operators/GetRefSeriesOperator.py
@@ -10,21 +10,21 @@ import json
 class GetRefSeriesOperator(KaapanaBaseOperator):
     """Operator to get DICOM series in dependency of certain search filters.
     This can be used to:
-    - download a reference series from a PACS system. (reference_uid).
-    - download all series of a study from a PACS system. (study_uid)
-    - NOT IMPLEMENTED YET: download all series of a patient from a PACS system. (patient_uid)
-
-    This can be additinally filtered by OpenSearch metadata.
+    - download a reference series (SEG or RTSTRUCT). (reference_uid).
+    - download all series of a study. This can be additinally filtered by an OpenSearch query.(study_uid)
+    - download series based on a OpenSearch search query. (search_query)
     """
 
     def __init__(
         self,
         dag,
         name: str = "get-ref-series",
-        search_policy: str = "reference_uid",  # reference_uid, study_uid, patient_uid
-        data_type: str = "dicom",  # dicom, json (NOT IMPLEMENTED YET)
-        dicom_tags: list = [],  # list of dicom tags to filter the series
+        search_policy: str = "reference_uid",
+        data_type: str = "dicom",
+        search_query: dict = {},
         parallel_downloads: int = 3,
+        modalities: list = [],
+        custom_tags: list = [],
         **kwargs,
     ):
         """Operator to get DICOM series in dependency of certain search filters.
@@ -32,19 +32,23 @@ class GetRefSeriesOperator(KaapanaBaseOperator):
         Args:
             dag (DAG): The DAG object.
             name (str, optional): The name of the operator. Defaults to "get-ref-series".
-            search_policy (str, optional): The search policy to filter the series. Can be "reference_uid" or "study_uid". Defaults to "reference_uid".
-            data_type (str, optional): The type of data to download. Defaults to "dicom".
-            dicom_tags (list, optional): List of DICOM tags to filter the series. Defaults to [].
+            search_policy (str, optional): The search policy to filter the series. Can be "reference_uid", "study_uid" or "search_query". Defaults to "reference_uid". "reference_uid" downloads the reference series of a SEG or RTSTRUCT, "study_uid" downloads all series of a study, "search_query" downloads all series based on a search query.
+            data_type (str, optional): The type of data to download. Can be "dicom" or "json". Defaults to "dicom".
+            search_query (list, optional): The search query for opensearch. Syntax must be the same as for opensearch. Defaults to {}. Must be a bool query (https://opensearch.org/docs/latest/query-dsl/compound/bool).
             parallel_downloads (int, optional): Number of parallel downloads. Defaults to 3.
+            modalities (list, optional): List of modalities to filter the series. Defaults to []. Cant be used together with search_query. Only makes sense in conjunction with search_policy="study_uid".
+            custom_tags (list, optional): List of custom tags to filter the series. Defaults to []. Cant be used together with search_query. Only makes sense in conjunction with search_policy="study_uid".
         """
 
         env_vars = {}
 
         envs = {
-            "DATA_TYPE": data_type,
             "SEARCH_POLICY": search_policy,
+            "DATA_TYPE": data_type,
+            "SEARCH_QUERY": json.dumps(search_query),
             "PARALLEL_DOWNLOADS": str(parallel_downloads),
-            "DICOM_TAGS": json.dumps(dicom_tags),
+            "MODALITIES": json.dumps(modalities),
+            "CUSTOM_TAGS": json.dumps(custom_tags),
         }
 
         env_vars.update(envs)

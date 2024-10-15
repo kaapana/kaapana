8c4a96b725c3428312e5849ffe0f41cbd7656fff
rajesh.baidya <rajesh.baidya@dkfz-heidelberg.de>
2024-10-15 22:04:17 +0200

    project single page ui created

--- services/data-separation/project-management-ui/docker/files/typed-router.d.ts
+++ services/data-separation/project-management-ui/docker/files/typed-router.d.ts
@@ -19,5 +19,6 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/project': RouteRecordInfo<'/project', '/project', Record<never, never>, Record<never, never>>,
   }
 }

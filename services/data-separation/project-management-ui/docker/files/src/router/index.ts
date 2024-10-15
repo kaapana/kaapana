037f02da0dbacdf69e693eed1ca718df9894983f
rajesh.baidya <rajesh.baidya@dkfz-heidelberg.de>
2024-10-15 22:04:17 +0200

    project single page ui created

--- services/data-separation/project-management-ui/docker/files/src/router/index.ts
+++ services/data-separation/project-management-ui/docker/files/src/router/index.ts
@@ -7,11 +7,14 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { routes } from 'vue-router/auto-routes'
+import Project from '@/pages/project.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
+
+router.addRoute({ path: '/project/:id', component: Project })
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
 router.onError((err, to) => {

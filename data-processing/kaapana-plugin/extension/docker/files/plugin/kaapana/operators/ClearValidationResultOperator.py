7d7a2dae43543b150a0926f354da21f26e5d4fda
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-10-21 17:31:07 +0200

    Create ClearValidationResultOperator

---
+++ data-processing/kaapana-plugin/extension/docker/files/plugin/kaapana/operators/ClearValidationResultOperator.py
@@ -0,0 +1,68 @@
+from datetime import timedelta
+
+from kaapana.blueprints.kaapana_global_variables import (
+    DEFAULT_REGISTRY,
+    KAAPANA_BUILD_VERSION,
+)
+from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator
+from kaapanapy.logger import get_logger
+from kaapanapy.settings import KaapanaSettings
+
+logger = get_logger(__name__)
+
+SERVICES_NAMESPACE = KaapanaSettings().services_namespace
+
+
+class ClearValidationResultOperator(KaapanaBaseOperator):
+    """
+    Initializes the LocalClearValidationResultOperator.
+
+    Args:
+        dag (DAG): The DAG to which the operator belongs.
+        name (str): The name of the operator. Defaults to "clear-validation-results".
+        results_bucket (str): minio bucket which stores the validation results html files. Defaults to "staticwebsiteresults".
+        validation_tag (str): Base tag used to store validation results on OpenSearch (default: "00111001").
+        opensearch_index (str): Index in OpenSearch where metadata will be stored. Defaults to OpensearchSettings().default_index.
+        *args: Additional arguments for the parent class.
+        **kwargs: Additional keyword arguments for the parent class.
+
+    Returns:
+        None
+    """
+
+    def __init__(
+        self,
+        dag,
+        name="clear-validation-results",
+        result_bucket: str = "staticwebsiteresults",
+        validation_tag: str = "00111001",
+        opensearch_index=None,
+        **kwargs,
+    ):
+        """Clears validation results from OpenSearch and Minio.
+
+        Args:
+            dag (DAG): The DAG to which the operator belongs.
+            name (str, optional): The name of the operator. Defaults to "clear-validation-results".
+            result_bucket (str, optional): minio bucket which stores the validation results html files. Defaults to "staticwebsiteresults".
+            validation_tag (str, optional): Base tag used to store validation results on OpenSearch (default: "00111001").
+            opensearch_index (_type_, optional): Index in OpenSearch where metadata will be stored. Defaults to OpensearchSettings().default_index.
+        """
+
+        env_vars = {}
+
+        env_vars["RESULT_BUCKET"] = result_bucket
+        env_vars["VALIDATION_TAG"] = validation_tag
+        env_vars["OPENSEARCH_INDEX"] = opensearch_index
+
+        super().__init__(
+            dag=dag,
+            name=name,
+            max_active_tis_per_dag=10,
+            execution_timeout=timedelta(minutes=60),
+            image=f"{DEFAULT_REGISTRY}/clear-validation-results:{KAAPANA_BUILD_VERSION}",
+            image_pull_secrets=["registry-secret"],
+            env_vars=env_vars,
+            ram_mem_mb=1000,
+            **kwargs,
+        )
---
+++ data-processing/kaapana-plugin/processing-containers/clear-validation-results/files/start.py
@@ -0,0 +1,211 @@
+import glob
+import json
+import os
+import re
+from os import getenv
+from os.path import exists
+
+from kaapanapy.helper import get_minio_client
+from kaapanapy.helper.HelperOpensearch import HelperOpensearch
+from kaapanapy.logger import get_logger
+from kaapanapy.settings import OpensearchSettings
+from minio import Minio
+
+logger = get_logger(__name__, level="INFO")
+
+
+class ClearValidationResultOperator:
+    """
+    This Operator delete validation results from minio.
+
+    Attributes:
+        minio_client (HelperMinio): MinIO client for interacting with the MinIO service.
+        os_client (OpenSearch): OpenSearch client for interacting with the OpenSearch service.
+        validation_field (str): Field in the OpenSearch index used for validation results.
+        result_bucket (str): minio bucket which stores the validation results html files.
+        opensearch_index (str): Index in OpenSearch where metadata is stored.
+    """
+
+    def __init__(
+        self,
+        result_bucket: str = "staticwebsiteresults",
+        validation_tag: str = "00111001",
+        opensearch_index: str = OpensearchSettings().default_index,
+        operator_in_dir: str = None,
+        workflow_dir: str = None,
+        batch_name: str = None,
+        run_id: str = None,
+    ):
+        # Operator attributes
+        self.result_bucket = result_bucket
+        self.validation_tag = validation_tag
+        self.opensearch_index = opensearch_index
+
+        # Airflow variables
+        self.operator_in_dir = operator_in_dir
+        self.workflow_dir = workflow_dir
+        self.batch_name = batch_name
+        self.run_id = run_id
+
+        self.minio_client: Minio = get_minio_client()
+        self.os_client = HelperOpensearch.os_client
+
+    def get_all_files_from_result_bucket(self, prefix=""):
+        """
+        Recursively retrieves all files from the specified MinIO bucket `staticwebsiteresults`.
+
+        Args:
+            prefix (str): The prefix to filter the objects in the MinIO bucket. Defaults to an empty string.
+
+        Returns:
+            list: A list of file paths in the specified MinIO bucket.
+        """
+        allresults = self.minio_client.list_objects(self.result_bucket, prefix)
+        files = []
+        for item in allresults:
+            if item.is_dir:
+                files.extend(
+                    self.get_all_files_from_result_bucket(prefix=item.object_name)
+                )
+            else:
+                files.append(item.object_name)
+        return files
+
+    def remove_field_in_opensearch(self, seriesuid: str, tagfield: str = ""):
+        """
+        Removes a specified field from a document in OpenSearch.
+
+        Args:
+            seriesuid (str): The unique identifier for the series in OpenSearch.
+            tagfield (str): The field to be removed from the document. Defaults to the class attribute `validation_field`.
+
+        Returns:
+            None
+        """
+        if tagfield == "":
+            tagfield = self.validation_field
+
+        # Update the document to remove the field
+        response = self.os_client.update(
+            index=self.opensearch_index,
+            id=seriesuid,
+            body={
+                "script": {
+                    "source": "ctx._source.remove(params.field)",
+                    "lang": "painless",
+                    "params": {"field": tagfield},
+                }
+            },
+        )
+
+        if response["result"] == "updated":
+            logger.info(f"{tagfield} is deleted from the {seriesuid} in OpenSearch")
+        else:
+            logger.info(
+                f"Warning!! {tagfield} could not be deleted from the {seriesuid} document in OpenSearch"
+            )
+
+        return
+
+    def remove_from_minio(self, seriesuid: str):
+        """
+        Deletes all validation results for a given series from MinIO bucket `staticwebsiteresults`.
+
+        Args:
+            seriesuid (str): The unique identifier for the series to be deleted from MinIO.
+
+        Returns:
+            None
+        """
+        allfiles = self.get_all_files_from_result_bucket()
+
+        # match only the files placed under the subdirectory of serisuid
+        sereismatcher = re.compile(f"\/?{re.escape(seriesuid)}\/")
+        seriesresults = [s for s in allfiles if sereismatcher.search(s)]
+
+        if len(seriesresults) == 0:
+            logger.info(f"No validation results found in minio for series {seriesuid}")
+            return
+
+        for result in seriesresults:
+            self.minio_client.remove_file(self.result_bucket, result)
+            logger.info(f"{result} is removed from minio")
+
+        return
+
+    def start(self):
+        """
+        Main execution method called by Airflow to run the operator.
+
+        Returns:
+            None
+        """
+
+        logger.info("Start Deleting Validation results")
+
+        batch_folder = [
+            f for f in glob.glob(os.path.join(self.workflow_dir, self.batch_name, "*"))
+        ]
+
+        for batch_element_dir in batch_folder:
+            jsonfiles = sorted(
+                glob.glob(
+                    os.path.join(batch_element_dir, self.operator_in_dir, "*.json*"),
+                    recursive=True,
+                )
+            )
+
+            for metafile in jsonfiles:
+                logger.info(f"Deleting validation results for file {metafile}")
+
+                with open(metafile) as fs:
+                    metadata = json.load(fs)
+
+                seriesuid = metadata[
+                    HelperOpensearch.series_uid_tag
+                ]  # "0020000E SeriesInstanceUID_keyword"
+
+                self.remove_from_minio(seriesuid)
+                self.remove_field_in_opensearch(
+                    seriesuid, tagfield=self.validation_field
+                )
+
+
+if __name__ == "__main__":
+
+    result_bucket = getenv("RESULT_BUCKET", None)
+    validation_tag = getenv("VALIDATION_TAG", None)
+    opensearch_index = getenv("OPENSEARCH_INDEX", None)
+
+    operator_in_dir = getenv("OPERATOR_IN_DIR", None)
+    assert operator_in_dir, "Operator in directory is not set!"
+
+    workflow_dir = getenv("WORKFLOW_DIR", None)
+    if not exists(workflow_dir):
+        # Workaround if this is being run in dev-server
+        workflow_dir_dev = workflow_dir.split("/")
+        workflow_dir_dev.insert(3, "workflows")
+        workflow_dir_dev = "/".join(workflow_dir_dev)
+
+        if not exists(workflow_dir_dev):
+            raise Exception(f"Workflow directory {workflow_dir} does not exist!")
+
+        workflow_dir = workflow_dir_dev
+
+    batch_name = getenv("BATCH_NAME", None)
+    assert batch_name, "Batch name is not set!"
+
+    run_id = getenv("RUN_ID", None)
+    assert run_id, "Run ID is not set!"
+
+    operator = ClearValidationResultOperator(
+        result_bucket=result_bucket,
+        validation_tag=validation_tag,
+        opensearch_index=opensearch_index,
+        operator_in_dir=operator_in_dir,
+        workflow_dir=workflow_dir,
+        batch_name=batch_name,
+        run_id=run_id,
+    )
+
+    operator.start()

06d6ffae484c267f1a8e48a5d17852bed4f88410
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-10-22 13:52:08 +0200

    Use OperatorSettings instead of manual env reading

--- data-processing/kaapana-plugin/processing-containers/validation-result-to-meta/files/start.py
+++ data-processing/kaapana-plugin/processing-containers/validation-result-to-meta/files/start.py
@@ -6,12 +6,11 @@ from datetime import datetime
 from enum import Enum
 from html.parser import HTMLParser
 from os import getenv
-from os.path import exists
 from typing import List
 
 from kaapanapy.helper.HelperOpensearch import HelperOpensearch
 from kaapanapy.logger import get_logger
-from kaapanapy.settings import OpensearchSettings
+from kaapanapy.settings import OpensearchSettings, OperatorSettings
 from pytz import timezone
 
 logger = get_logger(__name__, level="INFO")
@@ -84,15 +83,12 @@ class ValidationResult2MetaOperator:
         _init_client(): Initializes the OpenSearch client.
         start(ds, **kwargs): Main execution method called by Airflow to run the operator.
     """
+
     def __init__(
         self,
         validator_output_dir: str = None,
         opensearch_index: str = None,
         validation_tag: str = "00111001",
-        operator_in_dir: str = None,
-        workflow_dir: str = None,
-        batch_name: str = None,
-        run_id: str = None,
     ):
         # OS settings
         self.opensearch_settings = OpensearchSettings()
@@ -106,11 +102,13 @@ class ValidationResult2MetaOperator:
         self.validation_tag = validation_tag
         self.tag_field = f"{validation_tag} ValidationResults_object"
 
-        # Airflow settings
-        self.operator_in_dir = operator_in_dir
-        self.workflow_dir = workflow_dir
-        self.batch_name = batch_name
-        self.run_id = run_id
+        # Airflow variables
+        operator_settings = OperatorSettings()
+
+        self.operator_in_dir = operator_settings.operator_in_dir
+        self.workflow_dir = operator_settings.workflow_dir
+        self.batch_name = operator_settings.batch_name
+        self.run_id = operator_settings.run_id
 
     class Action(Enum):
         ADD = "add"
@@ -310,35 +308,10 @@ if __name__ == "__main__":
     opensearch_index = getenv("OPENSEARCH_INDEX", None)
     validation_tag = getenv("VALIDATION_TAG", None)
 
-    operator_in_dir = getenv("OPERATOR_IN_DIR", None)
-    assert operator_in_dir, "Operator in dir is not set!"
-
-    workflow_dir = getenv("WORKFLOW_DIR", None)
-    if not exists(workflow_dir):
-        # Workaround if this is being run in dev-server
-        workflow_dir_dev = workflow_dir.split("/")
-        workflow_dir_dev.insert(3, "workflows")
-        workflow_dir_dev = "/".join(workflow_dir_dev)
-
-        if not exists(workflow_dir_dev):
-            raise Exception(f"Workflow directory {workflow_dir} does not exist!")
-
-        workflow_dir = workflow_dir_dev
-
-    batch_name = getenv("BATCH_NAME", None)
-    assert batch_name, "Batch name is not set!"
-
-    run_id = getenv("RUN_ID", None)
-    assert run_id, "Run ID is not set!"
-
     operator = ValidationResult2MetaOperator(
         validator_output_dir=validator_output_dir,
         opensearch_index=opensearch_index,
         validation_tag=validation_tag,
-        operator_in_dir=operator_in_dir,
-        workflow_dir=workflow_dir,
-        batch_name=batch_name,
-        run_id=run_id,
     )
 
     operator.start()

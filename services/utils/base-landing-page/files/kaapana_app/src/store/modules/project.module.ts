e7813c24b344e2c44c879e3ff80a1095c3ccf94b
Lorenz Feineis <lorenz.feineis@dkfz-heidelberg.de>
2024-10-24 07:34:02 +0200

    Remove logs to console.

--- services/utils/base-landing-page/files/kaapana_app/src/store/modules/project.module.ts
+++ services/utils/base-landing-page/files/kaapana_app/src/store/modules/project.module.ts
@@ -6,16 +6,12 @@ import {
 import { SET_SELECTED_PROJECT } from '@/store/mutations.type'
 
 const defaults = {
-  defaultProject: {},
   selectedProject: {}
 }
 
 const state = Object.assign({}, defaults)
 
 const getters = {
-  defaultProject(state: any) {
-    return state.defaultProject
-  },
   selectedProject(state: any) {
     return state.selectedProject
   }
@@ -23,16 +19,13 @@ const getters = {
 
 const actions = {
   [GET_SELECTED_PROJECT](context: any) {
-    console.log("This is called:")
     const storedProject = localStorage.getItem("selectedProject")
-    console.log("storedProject: "+ storedProject)
     if (storedProject) {
       context.commit(SET_SELECTED_PROJECT, JSON.parse(storedProject))
     } else {
       return new Promise((resolve: any) => {
         httpClient.get("/aii/projects").then((response: any) => {
           const defaultProject = response.data[0]
-          console.log("Set default project: " + JSON.stringify(defaultProject))
           context.commit(SET_SELECTED_PROJECT, defaultProject)
         }).catch((error: any) => {
         console.error("Error fetching projects:", error);
@@ -42,7 +35,6 @@ const actions = {
     }
   },
   [UPDATE_SELECTED_PROJECT](context: any, selectedProject: any) {
-    console.log("UPDATE_SELECTED_PROJECT: "+ JSON.stringify(selectedProject))
     localStorage.setItem("selectedProject", JSON.stringify(selectedProject))
     context.commit(SET_SELECTED_PROJECT, selectedProject)
   }
@@ -50,7 +42,6 @@ const actions = {
 
 const mutations = {
   [SET_SELECTED_PROJECT](state: any, selectedProject: any) {
-    console.log("SET_SELECTED_PROJECT: "+ JSON.stringify(selectedProject))
     state.selectedProject = selectedProject
   },
 }

f212075d075767f4d81189dfb224c5453e0aa2b1
Bujotzek, Markus <markus.bujotzek@dkfz-heidelberg.de>
2024-10-24 18:23:43 +0200

    nnunetv2-ensemble working for evaluation with one model and ensemble with multiple models

--- data-processing/processing-pipelines/nnunet/processing-containers/nnunet/files/ensemble.py
+++ data-processing/processing-pipelines/nnunet/processing-containers/nnunet/files/ensemble.py
@@ -6,7 +6,8 @@ from pathlib import Path
 from os import getenv
 from os.path import basename, join, exists
 from glob import glob
-from nnunet.inference.ensemble_predictions import merge
+from nnunetv2.ensembling.ensemble import ensemble_folders
+
 
 batch_name = getenv("BATCH_NAME", "None")
 batch_name = batch_name if batch_name.lower() != "none" else None
@@ -81,165 +82,170 @@ def check_seg_info(inference_dir):
     print("#")
 
 
-print("##################################################")
-print("#")
-print("# Starting nnUNet simple predict....")
-print("#")
-print(f"# override:            {override}")
-print(f"# threads_nifiti:      {threads_nifiti}")
-print(f"# operator_in_dir:     {operator_in_dir}")
-print(f"# operator_out_dir:    {operator_out_dir}")
-print(f"# postprocessing_file: {postprocessing_file}")
-print("#")
-print(f"# pred_min_combination: {pred_min_combination}")
-print("#")
-print("##################################################")
-print("#")
+def main():
+    global batch_name, workflow_dir, operator_in_dir, operator_out_dir, threads_nifiti, pred_min_combination, override, store_npz, postprocessing_file, global_seg_info
 
-processed_count = 0
-batch_folders = sorted([f for f in glob(join("/", workflow_dir, batch_name, "*"))])
-for batch_element_dir in batch_folders:
-    element_input_dir = os.path.join(batch_element_dir, operator_in_dir)
-    if exists(element_input_dir):
-        if "skip" not in element_input_dir:
-            check_seg_info(element_input_dir)
-            print(f"#")
-        else:
-            print(f"# Skipping {element_input_dir} to the ensemble...")
-            print(f"#")
-    else:
-        print(f"# Input-Dir {element_input_dir} not found! -> unexpected -> ABORT")
-        print(f"#")
-        exit(1)
-
-assert len(global_seg_info) > 0
-
-if len(global_seg_info) > 1:
-    global_seg_info = sorted(
-        global_seg_info, key=lambda k: len(k["inference_dirs"]), reverse=True
-    )
-
-    for skipped_models in global_seg_info[1:]:
-        print("#")
-        print("##################################################")
-        print("#")
-        print("#################  INFO  #######################")
-        print("#")
-        print("# ----> Skipped models because of different label configuration:")
-        print("#")
-        print(json.dumps(skipped_models, indent=4, sort_keys=True, default=str))
-        print("#")
-        print("##################################################")
-        print("#")
-
-print("#")
-
-ensemble_dirs = global_seg_info[0]["inference_dirs"]
-print(f"# ensemble_dirs: {ensemble_dirs}")
-seg_file_path = global_seg_info[0]["seg_file_path"]
-print(f"# seg_file_path: {seg_file_path}")
-
-if len(ensemble_dirs) < 2:
+    print("##################################################")
+    print("#")
+    print("# Starting nnUNet simple predict....")
+    print("#")
+    print(f"# override:            {override}")
+    print(f"# threads_nifiti:      {threads_nifiti}")
+    print(f"# operator_in_dir:     {operator_in_dir}")
+    print(f"# operator_out_dir:    {operator_out_dir}")
+    print(f"# postprocessing_file: {postprocessing_file}")
+    print("#")
+    print(f"# pred_min_combination: {pred_min_combination}")
     print("#")
     print("##################################################")
     print("#")
-    print("#################  ERROR  #######################")
-    print("#")
-    print("# ----> Not enough valid model predictions!")
-    print("#")
-    print(f"# -> ensemble_dirs: {ensemble_dirs}")
-    print("#")
-    print("##################################################")
-    print("#")
-    exit(0)
 
-pred_min_combination = (
-    pred_min_combination if pred_min_combination is not None else len(ensemble_dirs)
-)
-pred_min_combination = 2 if pred_min_combination < 2 else pred_min_combination
-
-model_combinations = []
-for L in range(0, len(ensemble_dirs) + 1):
-    for subset in itertools.combinations(ensemble_dirs, L):
-        subset = list(subset)
-        if len(subset) >= pred_min_combination:
-            model_combinations.append(subset)
-
-for combination_index in range(0, len(model_combinations)):
-    model_combination = model_combinations[combination_index]
-    combination_output_dir = os.path.join(
-        "/", workflow_dir, operator_out_dir, f"combination_{combination_index}"
-    )
-    Path(combination_output_dir).mkdir(parents=True, exist_ok=True)
-    print(f"#")
-    print(f"# Evaluating combination: {combination_index}: {model_combination}")
-    print(f"#")
-    print(f"# combination_output_dir:    {combination_output_dir}")
-    print(f"#")
-
-    merge(
-        folders=ensemble_dirs,
-        output_folder=combination_output_dir,
-        threads=threads_nifiti,
-        postprocessing_file=postprocessing_file,
-        store_npz=store_npz,
-        override=override,
-    )
-    print(f"# COMBINATION {model_combination} DONE #")
-
-print("#")
-print("##################################################")
-print("#")
-print("# ALL COMBINATIONS DONE #")
-print("#")
-print("##################################################")
-print("#")
-print("# -> collecting ensemble files ...")
-print("#")
-
-final_target = join("/", workflow_dir, operator_out_dir)
-combination_output_dirs = sorted(
-    [f for f in glob(join("/", workflow_dir, operator_out_dir, "*"))]
-)
-for combination_output_dir in combination_output_dirs:
-    print(
-        f"# -> moving files from {basename(combination_output_dir)} to operator_out_dir"
-    )
-
-    assert "combination_" in combination_output_dir
-    combination_id = int(combination_output_dir.split("_")[-1])
-    combination_files = glob(join(combination_output_dir, "*"), recursive=False)
-    for combination_file in combination_files:
-        if ".nii.gz" in combination_file:
-            extension = "nii.gz"
+    processed_count = 0
+    batch_folders = sorted([f for f in glob(join("/", workflow_dir, batch_name, "*"))])
+    for batch_element_dir in batch_folders:
+        element_input_dir = os.path.join(batch_element_dir, operator_in_dir)
+        if exists(element_input_dir):
+            if "skip" not in element_input_dir:
+                check_seg_info(element_input_dir)
+                print(f"#")
+            else:
+                print(f"# Skipping {element_input_dir} to the ensemble...")
+                print(f"#")
         else:
-            extension = basename(combination_file).split(".")[-1]
-        target_file_path = join(
-            final_target,
-            basename(combination_file).replace(
-                f".{extension}", f"_combination_{combination_id}.{extension}"
-            ),
+            print(f"# Input-Dir {element_input_dir} not found! -> unexpected -> ABORT")
+            print(f"#")
+            exit(1)
+
+    assert len(global_seg_info) > 0
+
+    if len(global_seg_info) > 1:
+        global_seg_info = sorted(
+            global_seg_info, key=lambda k: len(k["inference_dirs"]), reverse=True
         )
-        print(f"# {basename(combination_file)} -> {basename(target_file_path)}")
-        shutil.move(src=combination_file, dst=target_file_path)
 
-    print(f"# -> deleting combination dir: {basename(combination_output_dir)}")
-    shutil.rmtree(path=combination_output_dir)
-    print(f"# done")
+        for skipped_models in global_seg_info[1:]:
+            print("#")
+            print("##################################################")
+            print("#")
+            print("#################  INFO  #######################")
+            print("#")
+            print("# ----> Skipped models because of different label configuration:")
+            print("#")
+            print(json.dumps(skipped_models, indent=4, sort_keys=True, default=str))
+            print("#")
+            print("##################################################")
+            print("#")
 
-print("# Writing model_combinations.json")
-print("# ")
-print(json.dumps(model_combinations, indent=4, sort_keys=True, default=str))
-print("# ")
-with open(join(final_target, "model_combinations.json"), "w") as jsonData:
-    json.dump(model_combinations, jsonData, indent=4, sort_keys=False, default=str)
+    print("#")
 
-shutil.copy(src=seg_file_path, dst=join(final_target, "ensemble_seg_info.json"))
-print("#")
-print("#")
-print("##################################################")
-print("#")
-print("# ENSEMBLE DONE")
-print("#")
-print("##################################################")
-print("#")
+    ensemble_dirs = global_seg_info[0]["inference_dirs"]
+    print(f"# ensemble_dirs: {ensemble_dirs}")
+    seg_file_path = global_seg_info[0]["seg_file_path"]
+    print(f"# seg_file_path: {seg_file_path}")
+
+    if len(ensemble_dirs) < 2:
+        print("#")
+        print("##################################################")
+        print("#")
+        print("#################  ERROR  #######################")
+        print("#")
+        print("# ----> Not enough valid model predictions!")
+        print("#")
+        print(f"# -> ensemble_dirs: {ensemble_dirs}")
+        print("#")
+        print("##################################################")
+        print("#")
+        exit(0)
+
+    pred_min_combination = (
+        pred_min_combination if pred_min_combination is not None else len(ensemble_dirs)
+    )
+    pred_min_combination = 2 if pred_min_combination < 2 else pred_min_combination
+
+    model_combinations = []
+    for L in range(0, len(ensemble_dirs) + 1):
+        for subset in itertools.combinations(ensemble_dirs, L):
+            subset = list(subset)
+            if len(subset) >= pred_min_combination:
+                model_combinations.append(subset)
+
+    for combination_index in range(0, len(model_combinations)):
+        model_combination = model_combinations[combination_index]
+        combination_output_dir = os.path.join(
+            "/", workflow_dir, operator_out_dir, f"combination_{combination_index}"
+        )
+        Path(combination_output_dir).mkdir(parents=True, exist_ok=True)
+        print(f"#")
+        print(f"# Evaluating combination: {combination_index}: {model_combination}")
+        print(f"#")
+        print(f"# combination_output_dir:    {combination_output_dir}")
+        print(f"#")
+
+        ensemble_folders(
+            list_of_input_folders=ensemble_dirs,
+            output_folder=combination_output_dir,
+            save_merged_probabilities=store_npz,
+            num_processes=threads_nifiti,
+        )
+        print(f"# COMBINATION {model_combination} DONE #")
+
+    print("#")
+    print("##################################################")
+    print("#")
+    print("# ALL COMBINATIONS DONE #")
+    print("#")
+    print("##################################################")
+    print("#")
+    print("# -> collecting ensemble files ...")
+    print("#")
+
+    final_target = join("/", workflow_dir, operator_out_dir)
+    combination_output_dirs = sorted(
+        [f for f in glob(join("/", workflow_dir, operator_out_dir, "*"))]
+    )
+    for combination_output_dir in combination_output_dirs:
+        print(
+            f"# -> moving files from {basename(combination_output_dir)} to operator_out_dir"
+        )
+
+        assert "combination_" in combination_output_dir
+        combination_id = int(combination_output_dir.split("_")[-1])
+        combination_files = glob(join(combination_output_dir, "*"), recursive=False)
+        for combination_file in combination_files:
+            if ".nii.gz" in combination_file:
+                extension = "nii.gz"
+            else:
+                extension = basename(combination_file).split(".")[-1]
+            target_file_path = join(
+                final_target,
+                basename(combination_file).replace(
+                    f".{extension}", f"_combination_{combination_id}.{extension}"
+                ),
+            )
+            print(f"# {basename(combination_file)} -> {basename(target_file_path)}")
+            shutil.move(src=combination_file, dst=target_file_path)
+
+        print(f"# -> deleting combination dir: {basename(combination_output_dir)}")
+        shutil.rmtree(path=combination_output_dir)
+        print(f"# done")
+
+    print("# Writing model_combinations.json")
+    print("# ")
+    print(json.dumps(model_combinations, indent=4, sort_keys=True, default=str))
+    print("# ")
+    with open(join(final_target, "model_combinations.json"), "w") as jsonData:
+        json.dump(model_combinations, jsonData, indent=4, sort_keys=False, default=str)
+
+    shutil.copy(src=seg_file_path, dst=join(final_target, "ensemble_seg_info.json"))
+    print("#")
+    print("#")
+    print("##################################################")
+    print("#")
+    print("# ENSEMBLE DONE")
+    print("#")
+    print("##################################################")
+    print("#")
+
+
+if __name__ == "__main__":
+    main()

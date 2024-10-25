18e4c3edc9b69a0c1e433b70de96a6ab2e9b01b8
xbankov <mikulas.bankovic27@gmail.com>
2024-10-25 16:05:31 +0200

    Remove redeploy.sh

--- utils/redeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-../build-scripts/start_build.py
-../build/kaapana-admin-chart/deploy_platform.sh --quiet --undeploy --no-hooks
-
-sudo rm -rf /home/kaapana
-
-../build/kaapana-admin-chart/deploy_platform.sh --quiet

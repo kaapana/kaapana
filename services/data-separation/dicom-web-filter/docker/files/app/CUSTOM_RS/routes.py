afc6d8128905925a082b60bd6cea63c654006ed4
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2024-11-08 16:26:00 +0100

    Add custom routes for slim viewer

--- services/data-separation/dicom-web-filter/docker/files/app/CUSTOM_RS/routes.py
+++ services/data-separation/dicom-web-filter/docker/files/app/CUSTOM_RS/routes.py
@@ -2,13 +2,12 @@ import logging
 
 import httpx
 from app import crud
-from app.config import DICOMWEB_BASE_URL
+from app.config import DEFAULT_PROJECT_ID, DICOMWEB_BASE_URL
 from app.database import get_session
-from fastapi import APIRouter, Depends, Request, Response
+from fastapi import APIRouter, Depends, Path, Request, Response
+from fastapi.responses import JSONResponse, StreamingResponse
 from sqlalchemy.ext.asyncio import AsyncSession
-
-# Set logging level
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+from starlette.status import HTTP_204_NO_CONTENT
 
 router = APIRouter()
 
@@ -196,3 +195,114 @@ async def del_series(
     else:
         return Response(status_code=403)
 
+
+# FOR SLIM VIEWER
+@router.get("/series", tags=["Custom"])
+async def get_series(request: Request, session: AsyncSession = Depends(get_session)):
+
+    if not "StudyInstanceUID" in request.query_params:
+        return JSONResponse(
+            content={"error": "StudyInstanceUID is required"}, status_code=400
+        )
+
+    study = request.query_params["StudyInstanceUID"]
+
+    # Get all series mapped to the project
+    series = set(
+        await crud.get_all_series_mapped_to_project(session, DEFAULT_PROJECT_ID)
+    )
+
+    # Remove SeriesInstanceUID from the query parameters
+    query_params = dict(request.query_params)
+    query_params["SeriesInstanceUID"] = []
+
+    # Add the series mapped to the project to the query parameters
+    for uid in series:
+        query_params["SeriesInstanceUID"].append(uid)
+
+    # Update the query parameters
+    request._query_params = query_params
+
+    async def stream_fn(request: Request):
+        async with httpx.AsyncClient() as client:
+            async with client.stream(
+                "GET",
+                f"{DICOMWEB_BASE_URL}/studies/{study}/series",
+                params=request.query_params,
+                headers=dict(request.headers),
+            ) as response:
+                async for chunk in response.aiter_bytes():
+                    yield chunk
+
+    return StreamingResponse(stream_fn(request=request))
+
+
+# FOR SLIM VIEWER
+@router.get("/studies/{study}/instances", tags=["Custom"])
+async def get_instances(
+    study: str, request: Request, session: AsyncSession = Depends(get_session)
+):
+
+    # Get all series mapped to the project
+    series = set(
+        await crud.get_all_series_mapped_to_project(session, DEFAULT_PROJECT_ID)
+    )
+
+    # Remove SeriesInstanceUID from the query parameters
+    query_params = dict(request.query_params)
+    query_params["SeriesInstanceUID"] = []
+
+    # Add the series mapped to the project to the query parameters
+    for uid in series:
+        query_params["SeriesInstanceUID"].append(uid)
+
+    # Update the query parameters
+    request._query_params = query_params
+
+    async def stream_fn(request: Request):
+        async with httpx.AsyncClient() as client:
+            async with client.stream(
+                "GET",
+                f"{DICOMWEB_BASE_URL}/studies/{study}/instances",
+                params=request.query_params,
+                headers=dict(request.headers),
+            ) as response:
+                async for chunk in response.aiter_bytes():
+                    yield chunk
+
+    return StreamingResponse(stream_fn(request=request))
+
+
+@router.get(
+    "/studies/{study}/series/{series}/instances/{instance}/bulkdata/{tag:path}",
+    tags=["Custom"],
+)
+async def get_bulkdata(
+    request: Request,
+    study: str,
+    series: str,
+    instance: str,
+    tag: str = Path(...),
+    session: AsyncSession = Depends(get_session),
+):
+
+    if not await crud.check_if_series_in_given_study_is_mapped_to_project(
+        session=session,
+        project_id=DEFAULT_PROJECT_ID,
+        study_instance_uid=study,
+        series_instance_uid=series,
+    ) and not request.scope.get("admin"):
+        return Response(status_code=HTTP_204_NO_CONTENT)
+
+    async def stream_fn(request: Request):
+        async with httpx.AsyncClient() as client:
+            async with client.stream(
+                "GET",
+                f"{DICOMWEB_BASE_URL}/studies/{study}/series/{series}/instances/{instance}/bulkdata/{tag}",
+                params=request.query_params,
+                headers=dict(request.headers),
+            ) as response:
+                async for chunk in response.aiter_bytes():
+                    yield chunk
+
+    return StreamingResponse(stream_fn(request=request))

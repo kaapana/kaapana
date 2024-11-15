6eb225ba8dd91eef39969ca26c6fcaaa9d506e91
Lorenz Feineis <lorenz.feineis@dkfz-heidelberg.de>
2024-11-15 08:51:21 +0100

    Depend list of projects on realm-role project-manager instead of keycloak group kaapana_admin.

--- services/data-separation/project-management-ui/docker/files/src/pages/index.vue
+++ services/data-separation/project-management-ui/docker/files/src/pages/index.vue
@@ -5,35 +5,38 @@
         <h4 class="text-h4 py-8">Available Projects</h4>
       </v-col>
       <v-col cols="3" class="d-flex justify-end align-center">
-        <v-btn block @click="projectDialog = true" size="large" prepend-icon="mdi-plus-box">
+        <v-btn
+          block
+          @click="projectDialog = true"
+          size="large"
+          prepend-icon="mdi-plus-box"
+        >
           Create New Projects
         </v-btn>
       </v-col>
     </v-row>
 
-    <v-alert density="compact" class="mb-6" v-model="error" icon="mdi-alert-circle"
+    <v-alert
+      density="compact"
+      class="mb-6"
+      v-model="error"
+      icon="mdi-alert-circle"
       text="Some error happened while creating the project. Please try again with different inputs."
-      title="Project Could not be created" type="error" prominent closable></v-alert>
+      title="Project Could not be created"
+      type="error"
+      prominent
+      closable
+    ></v-alert>
 
     <v-table>
       <thead>
         <tr>
           <th class="text-left"></th>
-          <th class="text-left">
-            Project ID
-          </th>
-          <th class="text-left">
-            Name
-          </th>
-          <th class="text-left">
-            Description
-          </th>
-          <th class="text-left">
-            External ID
-          </th>
-          <th class="text-center">
-            Action
-          </th>
+          <th class="text-left">Project ID</th>
+          <th class="text-left">Name</th>
+          <th class="text-left">Description</th>
+          <th class="text-left">External ID</th>
+          <th class="text-center">Action</th>
         </tr>
       </thead>
       <tbody>
@@ -44,11 +47,16 @@
           <td class="desc-col">{{ item.description }}</td>
           <td>{{ item.external_id }}</td>
           <td class="text-center">
-            <v-btn 
-              class="text-none" color="medium-emphasis" 
-              min-width="92" variant="outlined" size="small" rounded
+            <v-btn
+              class="text-none"
+              color="medium-emphasis"
+              min-width="92"
+              variant="outlined"
+              size="small"
+              rounded
               append-icon="mdi-arrow-right"
-              @click="goToProjects(item.name)">
+              @click="goToProjects(item.name)"
+            >
               View
             </v-btn>
           </td>
@@ -57,20 +65,23 @@
     </v-table>
   </v-container>
   <v-dialog v-model="projectDialog" max-width="1000">
-    <CreateNewProjectForm :onsuccess="handleProjectCreate" :oncancel="() => projectDialog = false" />
+    <CreateNewProjectForm
+      :onsuccess="handleProjectCreate"
+      :oncancel="() => (projectDialog = false)"
+    />
   </v-dialog>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import CreateNewProjectFrom from '@/components/CreateNewProjectForm.vue'
-import { aiiApiGet } from '@/common/aiiApi.service';
-import { ProjectItem, UserItem } from '@/common/types';
-import store from '@/common/store';
+import { defineComponent } from "vue";
+import CreateNewProjectFrom from "@/components/CreateNewProjectForm.vue";
+import { aiiApiGet } from "@/common/aiiApi.service";
+import { ProjectItem, UserItem } from "@/common/types";
+import store from "@/common/store";
 
 export default defineComponent({
   components: {
-    CreateNewProjectFrom
+    CreateNewProjectFrom,
   },
   props: {},
   data() {
@@ -79,36 +90,36 @@ export default defineComponent({
       projectDialog: false,
       error: false,
       projectFetched: false,
-    }
+    };
   },
   mounted() {
     // Store watch not triggering for some reasom
     // Temporary solution to check for user via
     // custom interval loop
-    const fetchProjectsRef = this.fetchProjects
-    let checkForUser = setInterval(function() {
+    const fetchProjectsRef = this.fetchProjects;
+    let checkForUser = setInterval(function () {
       const user = store.state.user;
       if (user) {
         fetchProjectsRef(user);
         clearInterval(checkForUser);
       }
-    }, 100)
+    }, 100);
   },
   watch: {
     // TODO
     // Watching the user object deeply
     // not triggering
-    'store.state.user': {
+    "store.state.user": {
       handler(newValue, oldValue) {
-        console.log('User object changed:', { newValue, oldValue });
+        console.log("User object changed:", { newValue, oldValue });
 
         // Perform your logic here, e.g., fetching projects
         if (newValue !== oldValue) {
           this.fetchProjects(newValue);
         }
       },
-      deep: true // Enables deep watching of user object
-    }
+      deep: true, // Enables deep watching of user object
+    },
   },
   methods: {
     fetchProjects: function (user: UserItem) {
@@ -117,7 +128,7 @@ export default defineComponent({
       let projects_url = `users/${user.id}/projects`;
 
       // if default-user / kaapana_admin, fetch all the projects
-      if (user.groups && user.groups.includes('kaapana_admin')) {
+      if (user.realm_roles && user.realm_roles.includes("project-manager")) {
         projects_url = `projects`;
       }
 
@@ -125,9 +136,9 @@ export default defineComponent({
         aiiApiGet(projects_url).then((projects: ProjectItem[]) => {
           this.projects = projects;
           this.projectFetched = true;
-        })
+        });
       } catch (error: unknown) {
-        console.log(error)
+        console.log(error);
       }
     },
     handleProjectCreate: function (success: boolean = true) {
@@ -143,23 +154,22 @@ export default defineComponent({
     },
     goToProjects(projectName: string) {
       this.$router.push(`/project/${projectName}`);
-    }
+    },
   },
-})
+});
 </script>
 
-
 <style scoped>
-.project-name-col{
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    max-width: 150px;
+.project-name-col {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  max-width: 150px;
 }
-.desc-col{
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    max-width: 350px;
+.desc-col {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  max-width: 350px;
 }
 </style>

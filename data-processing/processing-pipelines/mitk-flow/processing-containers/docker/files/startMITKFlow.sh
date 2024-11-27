3460ff4b803a804102dd676dc804f32442825480
Philipp Schader <philipp.schader@dkfz-heidelberg.de>
2024-11-27 16:51:39 +0100

    Refactor mitk-flow processing container

---
+++ data-processing/processing-pipelines/mitk-flow/processing-containers/docker/files/startMITKFlow.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Fixing input data permissions"
+echo "mitk" | sudo -S chown $USER -R /$WORKFLOW_DIR/$BATCH_NAME
+
+echo "Starting MTIK Flow"
+/mitk-flow/MitkFlowBench.sh /$WORKFLOW_DIR/$BATCH_NAME/tasklist.json &
+PID=$!
+
+tail -f  $USER/logfile | while read LOGLINE
+do
+	[[ "${LOGLINE}" == *"BlueBerry Workbench ready"* ]] && pkill -P $$ tail
+done
+
+echo 'Setting fullscreen mode'
+wmctrl -r 'Segmentation' -b toggle,fullscreen
+
+wait $PID

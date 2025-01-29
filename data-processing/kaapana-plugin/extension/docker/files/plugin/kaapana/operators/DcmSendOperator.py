61d5394013ec03e7bbd1b1b9b5d39fbd19d4d309
Benjamin Hamm <benjamin.hamm@dkfz-heidelberg.de>
2025-01-29 11:19:12 +0100

    New DcmSendOperator structure

--- data-processing/kaapana-plugin/extension/docker/files/plugin/kaapana/operators/DcmSendOperator.py
+++ data-processing/kaapana-plugin/extension/docker/files/plugin/kaapana/operators/DcmSendOperator.py
@@ -1,11 +1,10 @@
-from datetime import timedelta, datetime
+from datetime import datetime, timedelta
 
-from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator
 from kaapana.blueprints.kaapana_global_variables import (
-    SERVICES_NAMESPACE,
     DEFAULT_REGISTRY,
     KAAPANA_BUILD_VERSION,
 )
+from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator
 
 
 class DcmSendOperator(KaapanaBaseOperator):
@@ -13,54 +12,41 @@ class DcmSendOperator(KaapanaBaseOperator):
     Operator sends data to the platform.
 
     This operator is used for sending data to the platform.
-    For dcmsend documentation please have a look at https://support.dcmtk.org/docs/dcmsend.html.
     """
 
     def __init__(
         self,
         dag,
         name: str = "dcmsend",
-        ae_title: str = "NONE",
-        pacs_host: str = f"ctp-dicom-service.{SERVICES_NAMESPACE}.svc",
-        pacs_port: str = "11112",
-        env_vars=None,
-        level: str = "element",
+        calling_ae_title_scu: str = "",
+        called_ae_title_scp: str = "",
+        pacs_host: int = "",
+        pacs_port: str = "",
+        env_vars: dict = {},
         execution_timeout: datetime = timedelta(minutes=60),
         **kwargs,
     ):
         """
-        :param ae_title: calling Application Entity (AE) title
+        :param calling_ae_title_scu: Local Calling AET. Kaapana interprets this as the dataset name.
+        :param called_ae_title_scp: Remote Called AET. Kaapana interprets this as the project name.
         :param pacs_host: Host of PACS
         :param pacs_port: Port of PACS
         :param env_vars: Environment variables
-        :param level: 'element' or batch'
-            If batch, an operator folder next to the batch folder with .dcm files is expected.
-            If element, \*.dcm are expected in the corresponding operator with .dcm files is expected.
         :param execution_timeout: timeout for connection requests
         """
 
-        if level not in ["element", "batch"]:
-            raise NameError(
-                "level must be either 'element' or 'batch'. If batch, an operator folder next to the batch folder with .dcm files is expected. If element, *.dcm are expected in the corresponding operator with .dcm files is expected."
-            )
-
-        if env_vars is None:
-            env_vars = {}
-
-        envs = {
-            "PACS_HOST": str(pacs_host),
-            "PACS_PORT": str(pacs_port),
-            "AETITLE": str(ae_title),
-            "LEVEL": str(level),
-        }
-
-        env_vars.update(envs)
+        env_vars.update(
+            {
+                "PACS_HOST": str(pacs_host),
+                "PACS_PORT": str(pacs_port),
+                "CALLING_AE_TITLE_SCU": str(calling_ae_title_scu),
+                "CALLED_AE_TITLE_SCP": str(called_ae_title_scp),
+            }
+        )
 
         if not kwargs.get("labels"):
-            kwargs["labels"] = {"network-access": "ctp"}
-        else:
-            if not kwargs.get("labels").get("network-access"):
-                kwargs["labels"]["network-access"] = "ctp"
+            kwargs["labels"] = {}
+            kwargs["labels"]["network-access"] = "ctp"
 
         super().__init__(
             dag=dag,

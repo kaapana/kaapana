10f8a0f20bf9e0a413453c5aa8908d6a3c645fb6
Lorenz Feineis <lorenz.feineis@dkfz-heidelberg.de>
2025-03-06 14:17:47 +0100

    Return 403 if user starts a workflow that is not allowed in the current project

--- services/base/kaapana-backend/docker/files/app/workflows/routers/client.py
+++ services/base/kaapana-backend/docker/files/app/workflows/routers/client.py
@@ -639,11 +639,22 @@ def create_workflow(
 ):
     # exception handling for admin requests via fastapi's kaapana-backend/docs
     # necessary for maually restarting federated workflows via recovery_conf
-    try:
-        project = request.headers.get("Project")
-        json_schema_data.conf_data["project_form"] = json.loads(project)
-    except:
-        pass
+    project = request.headers.get("Project")
+    project = json.loads(project)
+    json_schema_data.conf_data["project_form"] = project
+    # Get all software-mappings for the project
+    # and verify that the current user is allowed to create the workflow
+    aii_response = httpx.get(
+        f"http://aii-service.services.svc:8080/projects/{project.get("name")}/software-mappings"
+    )
+    software_mappings = aii_response.json()
+    dag_id = json_schema_data.dag_id
+    if dag_id not in [mapping.get("software_uuid") for mapping in software_mappings]:
+        raise HTTPException(
+            status_code=403,
+            detail="Unauthorized to start this workflow."
+        )
+
 
     # validate incoming json_schema_data
     try:

74f333e916620dbda76daeddf2e3def956739cf8
Lorenz Feineis <lorenz.feineis@dkfz-heidelberg.de>
2025-03-18 12:05:16 +0100

    Make boot script for kaapana-backend realice dev_files mode.

--- services/base/kaapana-backend/docker/files/boot.sh
+++ services/base/kaapana-backend/docker/files/boot.sh
@@ -5,16 +5,11 @@ alembic upgrade head
 
 export PYTHONPATH="$PWD" 
 python3 scripts/create_kaapana_instance.py
-# # Production
-# echo "Running at $APPLICATION_ROOT"
-#SCRIPT_NAME=$APPLICATION_ROOT gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000 --access-logfile - --error-logfile - 
-
-# Development
-# uvicorn app.main:app --reload --host 0.0.0.0 --port 5000 --workers 4 --root-path $APPLICATION_ROOT
 
 if [ -z "${DEV_FILES}" ]; then
+    # Production
     SCRIPT_NAME=$APPLICATION_ROOT gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000 --access-logfile - --error-logfile - 
 else
+    # Development
     uvicorn app.main:app --reload --host 0.0.0.0 --port 5000 --workers 4 --root-path $APPLICATION_ROOT
-
 fi

cc61541cf7d384220e895783498ab32ef012c166
l597s <l597s@e132-pc08.inet.dkfz-heidelberg.de>
2025-04-03 10:42:42 +0200

    Make applicable models selectable in the workflow execution; Add publication form.

--- data-processing/processing-pipelines/body-and-organ-analysis/extension/docker/files/dag_body_and_organ_analysis.py
+++ data-processing/processing-pipelines/body-and-organ-analysis/extension/docker/files/dag_body_and_organ_analysis.py
@@ -12,16 +12,80 @@ from body_and_organ_analysis.BodyAndOrganAnalysisOperator import BodyAndOrganAna
 
 max_active_runs = 5
 
+
 ui_forms = {
+    "publication_form": {
+        "type": "object",
+        "properties": {
+            "boa": {
+                "title": "BOA",
+                "default": "Haubold, J., Baldini, G., Parmar, V., Schaarschmidt, B. M., Koitka, S., Kroll, L., van Landeghem, N., Umutlu, L., Forsting, M., Nensa, F., & Hosch, R. (2023). BOA: A CT-Based Body and Organ Analysis for Radiologists at the Point of Care. Investigative radiology, 10.1097/RLI.0000000000001040. Advance online publication. https://doi.org/10.1097/RLI.0000000000001040",
+                "description": "Haubold, J., Baldini, G., Parmar, V., Schaarschmidt, B. M., Koitka, S., Kroll, L., van Landeghem, N., Umutlu, L., Forsting, M., Nensa, F., & Hosch, R. (2023). BOA: A CT-Based Body and Organ Analysis for Radiologists at the Point of Care. Investigative radiology, 10.1097/RLI.0000000000001040. Advance online publication. https://doi.org/10.1097/RLI.0000000000001040",
+                "type": "string",
+                "readOnly": True,
+            },
+            "totalsegmentator": {
+                "title": "TotalSegmentator",
+                "default": "Wasserthal J, Breit H-C, Meyer MT, et al. TotalSegmentator: Robust Segmentation of 104 Anatomic Structures in CT Images. Radiol. Artif. Intell. 2023:e230024. Available at: https://pubs.rsna.org/doi/10.1148/ryai.230024.",
+                "description": "Wasserthal J, Breit H-C, Meyer MT, et al. TotalSegmentator: Robust Segmentation of 104 Anatomic Structures in CT Images. Radiol. Artif. Intell. 2023:e230024. Available at: https://pubs.rsna.org/doi/10.1148/ryai.230024.",
+                "type": "string",
+                "readOnly": True,
+            },
+            "nnunet": {
+                "title": "nnU-Net",
+                "default": "Isensee F, Jaeger PF, Kohl SAA, et al. nnU-Net: a self-configuring method for deep learning-based biomedical image segmentation. Nat. Methods. 2021;18(2):203–211. Available at: https://www.nature.com/articles/s41592-020-01008-z.",
+                "description": "Isensee F, Jaeger PF, Kohl SAA, et al. nnU-Net: a self-configuring method for deep learning-based biomedical image segmentation. Nat. Methods. 2021;18(2):203–211. Available at: https://www.nature.com/articles/s41592-020-01008-z.",
+                "type": "string",
+                "readOnly": True,
+            },
+            "confirmation": {
+                "title": "Accept",
+                "default": False,
+                "type": "boolean",
+                "readOnly": False,
+                "required": True,
+                "description": "I will cite the publication above if applicable."
+            },
+        },
+    },
     "workflow_form": {
         "type": "object",
         "properties": {
+            "models": {
+                "title": "Model selection",
+                "description": "Select all models that should be used during analysis.",
+                "type": "array",
+                "items": {
+                    "type": "string",
+                    "examples": [
+                        "body",
+                        "total",
+                        "lung_vessels",
+                        "cerebral_bleed",
+                        "hip_implant",
+                        "coronary_arteries",
+                        "pleural_pericard_effusion",
+                        "liver_vessels",
+                        "bca",
+                    ]
+                },
+                "default": ["bca"],
+                "readOnly": False,
+            },
             "single_execution": {
                 "title": "single execution",
                 "description": "Should each series be processed separately?",
                 "type": "boolean",
                 "default": True,
                 "readOnly": True,
+            },
+            "input": {
+                "title": "Input modality",
+                "default": "CT",
+                "description": "Expected input modality.",
+                "type": "string",
+                "readOnly": True,
+                "required": True,
             },
         },
     }
--- data-processing/processing-pipelines/body-and-organ-analysis/processing-containers/body-and-organ-analysis/files/run_boa.sh
+++ data-processing/processing-pipelines/body-and-organ-analysis/processing-containers/body-and-organ-analysis/files/run_boa.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
 IDENTIFIERS=$( jq -r '.data_form.identifiers[]' ${WORKFLOW_DIR}/conf/conf.json )
+MODELS=$( jq -r '.workflow_form.models[]' ${WORKFLOW_DIR}/conf/conf.json )
+
+MODELS_ARGUMENT=$(echo ${MODELS} | sed 's/ /+/g')
 
 for SERIES_UID in $IDENTIFIERS; do
     python3 body_organ_analysis \
     --input-image ${BATCHES_INPUT_DIR}/${SERIES_UID}/${OPERATOR_IN_DIR}/${SERIES_UID}.nii.gz \
     --output-dir ${WORKFLOW_DIR}/${OPERATOR_OUT_DIR}/${SERIES_UID} \
-    --models bca \
+    --models ${MODELS_ARGUMENT} \
     --verbose
     
     python3 convert_nifti_to_dicom.py ${BATCHES_INPUT_DIR}/${SERIES_UID}/get-input-data ${WORKFLOW_DIR}/${OPERATOR_OUT_DIR}/${SERIES_UID}

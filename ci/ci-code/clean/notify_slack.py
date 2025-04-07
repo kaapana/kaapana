51942cb5ec97680515fbcbbd29b229c21adf85c7
Mikulas Bankovic <mikulas.bankovic27@gmail.com>
2025-04-07 14:23:40 +0200

    Add link to the issue

--- ci/ci-code/clean/notify_slack.py
+++ ci/ci-code/clean/notify_slack.py
@@ -1,30 +1,51 @@
 import os
+
+import gitlab
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-# Load secrets from environment variables
-SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
-SLACK_CHANNEL_ID = os.getenv("SLACK_CHANNEL_ID")
 
-# Initialize Slack client
-client = WebClient(token=SLACK_BOT_TOKEN)
-
-
-def send_message(message: str):
-    """Sends a message to the fixed Slack channel."""
+def send_message(client: WebClient, channel: str, message: str):
+    """Sends a message to a given Slack channel."""
     try:
-        response = client.chat_postMessage(channel=SLACK_CHANNEL_ID, text=message)
+        response = client.chat_postMessage(channel=channel, text=message)
         return response
     except SlackApiError as e:
         print(f"Error sending message: {e.response['error']}")
 
 
-# Example usage
+def main():
+    # Load secrets and env vars
+    slack_token = os.getenv("SLACK_BOT_TOKEN")
+    slack_channel = os.getenv("SLACK_CHANNEL_ID")
+    registry_token = os.getenv("REGISTRY_TOKEN")
+    project_id = os.getenv("CI_PROJECT_ID")
+    pipeline_url = os.getenv("CI_PIPELINE_URL")
+    commit_sha = os.getenv("CI_COMMIT_SHORT_SHA")
+    branch = os.getenv("CI_COMMIT_BRANCH")
+
+    # Set up clients
+    slack_client = WebClient(token=slack_token)
+    gl = gitlab.Gitlab(
+        url="https://codebase.helmholtz.cloud",
+        private_token=registry_token,
+        ssl_verify=False,
+    )
+
+    # Logic
+    project = gl.projects.get(id=project_id)
+    issues = project.issues.list(state="opened", labels=["CI"], search=commit_sha)
+
+    if len(issues) == 1:
+        issue_url = issues[0].web_url
+        message = (
+            "*Dear Kaapana developers*,\n\n"
+            "Unfortunately, a *CI pipeline* has failed for branch: `{}`.\n"
+            "*Pipeline:* <{}>\n"
+            "*Issue:* <{}>\n"
+        ).format(branch, pipeline_url, issue_url)
+        send_message(slack_client, slack_channel, message)
+
+
 if __name__ == "__main__":
-
-    message = """Dear Kaapana developers, 
-    unfortunately a CI pipeline failed for branch: {}. Here is a link to the pipeline: {}."""
-    ci_pipeline_url = os.getenv("CI_PIPELINE_URL")
-    ci_commit_branch = os.getenv("CI_COMMIT_BRANCH")
-
-    send_message(message.format(ci_commit_branch, ci_pipeline_url))
+    main()

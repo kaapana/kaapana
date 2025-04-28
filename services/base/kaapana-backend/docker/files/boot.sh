7a09926b41fc9f22135db9e98cc4c0ae9d6539d4
Mikulas Bankovic <mikulas.bankovic27@gmail.com>
2025-04-28 13:27:06 +0200

    Remove stamping the database. This needs to be done manually for 0.4.1, and automatically afterwards.

--- services/base/kaapana-backend/docker/files/boot.sh
+++ services/base/kaapana-backend/docker/files/boot.sh
@@ -2,18 +2,15 @@
 
 RESULT=$(alembic current)
 
-if [ -z "$RESULT" ] || echo "$RESULT" | grep -q "Can't locate revision identified by"; then
+if echo "$RESULT" | grep -q "Can't locate revision identified by"; then
     echo "No valid current revision detected. Stamping DB to initial revision..."
     alembic stamp --purge 5d694eb1a7b1 # 0.4.0
 fi
 
-# Apply all migrations
 alembic upgrade head
 
 export PYTHONPATH="$PWD"
 python3 scripts/create_kaapana_instance.py
-
-
 
 if [ -z "${DEV_FILES}" ]; then
     # Production

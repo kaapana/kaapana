f6d79a83eba284a165c4a5ddd53e306c38f0b3ba
Philipp Schader <philipp.schader@dkfz-heidelberg.de>
2025-05-15 12:13:17 +0200

    Add missing comma

--- tests/ui/playwright.config.ts
+++ tests/ui/playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    baseURL: process.env.KAAPANA_TEST_INSTANCE_UI || 'https://localhost'
+    baseURL: process.env.KAAPANA_TEST_INSTANCE_UI || 'https://localhost',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',

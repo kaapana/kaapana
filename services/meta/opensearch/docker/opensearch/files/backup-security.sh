2814db8abd8cbec98efa74b09cde810361a31260
l597s <lorenz.feineis@dkfz-heidelberg.de>
2025-05-16 15:12:47 +0200

    CronJob to backup security config like roles and roles mappings from opensearch.

--- services/meta/opensearch/docker/opensearch/files/backup-security.sh
+++ services/meta/opensearch/docker/opensearch/files/backup-security.sh
@@ -1,3 +1,4 @@
 CONFIG_DIR="/usr/share/opensearch/config/"
+OPENSEARCH_HOSTNAME="opensearch-service.services.svc"
 
-/usr/share/opensearch/plugins/opensearch-security/tools/securityadmin.sh -icl -nhnv -cacert ${CONFIG_DIR}/root-ca.pem -cert ${CONFIG_DIR}/admin.pem -key ${CONFIG_DIR}/admin-key.pem -backup /kaapana/opensearch-security-backup
+/usr/share/opensearch/plugins/opensearch-security/tools/securityadmin.sh -h ${OPENSEARCH_HOSTNAME} -icl -nhnv -cacert ${CONFIG_DIR}/root-ca.pem -cert ${CONFIG_DIR}/admin.pem -key ${CONFIG_DIR}/admin-key.pem -backup /kaapana/opensearch-security-backup

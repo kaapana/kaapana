9f2a108969bd93422a710c503317f2f4ce1d2cee
Gao, Hanno <h.gao@dkfz-heidelberg.de>
2025-08-13 15:29:11 +0200

    optimized reindexing

--- data-processing/kaapana-plugin/extension/docker/files/dags/dag_service_reindex_dicom_data.py
+++ data-processing/kaapana-plugin/extension/docker/files/dags/dag_service_reindex_dicom_data.py
@@ -1,7 +1,10 @@
 from airflow.models import DAG
 from datetime import timedelta
 import pydicom
-from shutil import copyfile
+from pydicom.tag import Tag
+import json
+import os
+import glob
 from airflow.utils.dates import days_ago
 from kaapana.blueprints.kaapana_global_variables import AIRFLOW_WORKFLOW_DIR, BATCH_NAME
 from kaapana.operators.LocalWorkflowCleanerOperator import LocalWorkflowCleanerOperator
@@ -12,7 +15,6 @@ from kaapana.operators.LocalJson2MetaOperator import LocalJson2MetaOperator
 from kaapana.operators.LocalAssignDataToProjectOperator import (
     LocalAssignDataToProjectOperator,
 )
-
 
 args = {
     "ui_visible": False,
@@ -32,12 +34,29 @@ dag = DAG(
 )
 
 
-def start_reindexing(ds, **kwargs):
-    import os
-    import glob
+def _delete_pixel_data(dcm: pydicom.Dataset) -> pydicom.Dataset:
+    """Remove pixel data elements to reduce memory usage"""
+    # (0014,3080) Bad Pixel Image
+    # (7FE0,0008) Float Pixel Data
+    # (7FE0,0009) Double Float Pixel Data
+    # (7FE0,0010) Pixel Data
+    pixel_data_elements = [
+        (0x0014, 0x3080),
+        (0x7FE0, 0x0008),
+        (0x7FE0, 0x0009),
+        (0x7FE0, 0x0010),
+    ]
+    for elem in pixel_data_elements:
+        tag = Tag(*elem)
+        if tag in dcm:
+            del dcm[tag]
+    return dcm
 
+
+def start_reindexing_optimized(ds, **kwargs):
+    """Optimized reindexing that creates JSON metadata without copying DICOM files"""
     pacs_data_dir = "/kaapana/mounted/pacsdata"
-    print("Start re-index")
+    print("Start optimized re-index")
 
     dcm_dirs = []
     file_list = glob.glob(pacs_data_dir + "/fs1/**/*", recursive=True)
@@ -52,51 +71,78 @@ def start_reindexing(ds, **kwargs):
     print("Run-id: {}".format(dag_run_id))
 
     for dcm_dir in dcm_dirs:
-        dcm_file = os.path.join(dcm_dir, os.listdir(dcm_dir)[0])
-        print("DIR: {}".format(dcm_dir))
-        print("dcm-file: {}".format(dcm_file))
-        incoming_dcm = pydicom.dcmread(dcm_file)
-        seriesUID = incoming_dcm.SeriesInstanceUID
+        try:
+            dcm_file = os.path.join(dcm_dir, os.listdir(dcm_dir)[0])
+            print("DIR: {}".format(dcm_dir))
+            print("dcm-file: {}".format(dcm_file))
 
-        target_dir = os.path.join(
-            AIRFLOW_WORKFLOW_DIR,
-            dag_run_id,
-            BATCH_NAME,
-            "{}".format(seriesUID),
-            "copy-from-pacs",
-        )
-        print(target_dir)
+            dcm = pydicom.dcmread(dcm_file, stop_before_pixels=True)
 
-        if not os.path.exists(target_dir):
-            os.makedirs(target_dir)
+            # Remove any remaining pixel data elements
+            dcm = _delete_pixel_data(dcm)
 
-        copyfile(
-            dcm_file, os.path.join(target_dir, os.path.basename(dcm_file) + ".dcm")
-        )
+            # Get series UID for directory structure
+            seriesUID = dcm.SeriesInstanceUID
+
+            # Create target directory structure
+            target_dir = os.path.join(
+                AIRFLOW_WORKFLOW_DIR,
+                dag_run_id,
+                BATCH_NAME,
+                "{}".format(seriesUID),
+                "extract-metadata-direct",
+            )
+
+            print(target_dir)
+
+            if not os.path.exists(target_dir):
+                os.makedirs(target_dir)
+
+            json_dict = dcm.to_json_dict()
+
+            del dcm
+
+            json_filename = os.path.basename(dcm_file) + ".json"
+            json_path = os.path.join(target_dir, json_filename)
+
+            with open(json_path, "w", encoding="utf-8") as f:
+                json.dump(json_dict, f, indent=2, ensure_ascii=False)
+
+            print("Saved JSON metadata to: {}".format(json_path))
+
+        except Exception as e:
+            print("Error processing directory {}: {}".format(dcm_dir, str(e)))
+            continue
+
+    print("Re-indexing completed")
 
 
-copy_from_pacs = KaapanaPythonBaseOperator(
-    name="copy-from-pacs",
+extract_metadata_direct = KaapanaPythonBaseOperator(
+    name="extract-metadata-direct",
     pool="default_pool",
     pool_slots=1,
-    python_callable=start_reindexing,
+    python_callable=start_reindexing_optimized,
     dag=dag,
 )
 
-extract_metadata = LocalDcm2JsonOperator(dag=dag, input_operator=copy_from_pacs)
-add_to_dataset = LocalAddToDatasetOperator(dag=dag, input_operator=extract_metadata)
-push_json = LocalJson2MetaOperator(
-    dag=dag, input_operator=copy_from_pacs, json_operator=extract_metadata
+
+# add_to_dataset = LocalAddToDatasetOperator(dag=dag, input_operator=extract_metadata_direct)
+
+extract_metadata = LocalDcm2JsonOperator(
+    dag=dag, input_operator=extract_metadata_direct, data_type="json"
 )
+push_json = LocalJson2MetaOperator(dag=dag, json_operator=extract_metadata)
+
 assign_to_project = LocalAssignDataToProjectOperator(
     dag=dag, input_operator=extract_metadata
 )
+# clean = LocalWorkflowCleanerOperator(dag=dag, clean_workflow_dir=True)
 
-clean = LocalWorkflowCleanerOperator(dag=dag, clean_workflow_dir=True)
-
+# Updated task dependencies
 (
-    copy_from_pacs
+    extract_metadata_direct
     >> extract_metadata
-    >> (add_to_dataset, assign_to_project, push_json)
-    >> clean
+    >> (assign_to_project, push_json)
+    # >> (add_to_dataset, assign_to_project, push_json)
+    # >> clean
 )

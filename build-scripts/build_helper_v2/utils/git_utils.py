c265a196d069e3c376cc307a627f62618f97575b
xbankov <mikulas.bankovic27@gmail.com>
2025-09-03 19:36:52 +0200

    Helm charts built and pushed

--- build-scripts/build_helper_v2/utils/git_utils.py
+++ build-scripts/build_helper_v2/utils/git_utils.py
@@ -12,40 +12,42 @@ class GitUtils:
             repo_dir = dirname(repo_dir)
         assert repo_dir != "/", "Reached filesystem root without finding .git"
 
-        repo = Repo(repo_dir)
-        assert not repo.bare, "Found git repo is bare"
+        with Repo(repo_dir) as repo:
+            assert not repo.bare, "Found git repo is bare"
 
-        # Extract shared info
-        def extract_common_info(repo):
-            commit = repo.head.commit
-            timestamp = (
-                commit.committed_datetime.astimezone()
-                .replace(microsecond=0)
-                .isoformat()
-            )
-            version = repo.git.describe()
-            return commit, timestamp, version
+            # Extract shared info
+            def extract_common_info(repo):
+                commit = repo.head.commit
+                timestamp = (
+                    commit.committed_datetime.astimezone()
+                    .replace(microsecond=0)
+                    .isoformat()
+                )
+                version = repo.git.describe()
+                return commit, timestamp, version
 
-        if "modules" in repo.common_dir:
-            # If we're in a submodule, get correct submodule repo by name
-            repo_name = basename(repo.working_dir)
-            submodules = Repo(dirname(repo_dir)).submodules
-            sub_repo = next(
-                (x.module() for x in submodules if x.name == repo_name), None
-            )
-            assert sub_repo, f"Submodule {repo_name} not found"
-            commit, timestamp, version = extract_common_info(sub_repo)
-            branch = sub_repo.git.branch()
-            branch = branch.split("\n")[1].strip() if "\n" in branch else branch.strip()
-        else:
-            # Regular repo
-            commit, timestamp, version = extract_common_info(repo)
-            try:
-                branch = repo.active_branch.name
-            except TypeError:
-                branch = "DETACHED-HEAD"
+            if "modules" in repo.common_dir:
+                # If we're in a submodule, get correct submodule repo by name
+                repo_name = basename(repo.working_dir)
+                submodules = Repo(dirname(repo_dir)).submodules
+                sub_repo = next(
+                    (x.module() for x in submodules if x.name == repo_name), None
+                )
+                assert sub_repo, f"Submodule {repo_name} not found"
+                commit, timestamp, version = extract_common_info(sub_repo)
+                branch = sub_repo.git.branch()
+                branch = (
+                    branch.split("\n")[1].strip() if "\n" in branch else branch.strip()
+                )
+            else:
+                # Regular repo
+                commit, timestamp, version = extract_common_info(repo)
+                try:
+                    branch = repo.active_branch.name
+                except TypeError:
+                    branch = "DETACHED-HEAD"
 
-            # Optionally validate version (will raise if invalid)
-            _ = semver.VersionInfo.parse(version)
+                # Optionally validate version (will raise if invalid)
+                _ = semver.VersionInfo.parse(version)
 
         return version, branch, commit, timestamp

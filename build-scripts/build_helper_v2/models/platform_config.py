7ee932cd2b4d3ab2100830b7ec2a79fbf940aa7e
xbankov <mikulas.bankovic27@gmail.com>
2025-09-09 10:44:55 +0200

    No errors on build and deploy

--- build-scripts/build_helper_v2/models/platform_config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class PlatformConfig(BaseModel):
-    platform_name: str
-    platform_build_version: str
-    platform_repo_version: str
-    platform_build_branch: str
-    platform_last_commit_timestamp: str

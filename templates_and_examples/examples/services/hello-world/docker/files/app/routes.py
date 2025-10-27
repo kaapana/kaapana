260939fb3c0066fa7976b8367d7cca1713a5fdb8
Theo Stempel-Hauburger <theo.stempel-hauburger@dkfz-heidelberg.de>
2025-10-27 14:25:51 +0100

    add /metrics endpoint to hello-world example

--- templates_and_examples/examples/services/hello-world/docker/files/app/routes.py
+++ templates_and_examples/examples/services/hello-world/docker/files/app/routes.py
@@ -1,5 +1,5 @@
 import os
-from flask import render_template
+from flask import render_template, Response
 from app import app
 
 
@@ -10,3 +10,19 @@ def index():
     return render_template(
         "index.html", title="Home", hello_world_user=hello_world_user
     )
+
+
+@app.route("/metrics", methods=["GET"])
+def metrics():
+    hello_world_user = os.environ.get("HELLO_WORLD_USER", "unknown").replace('"', '\\"')
+    metrics_output = (
+        "\n".join(
+            [
+                "# HELP hello_world_app_info Basic metadata about the hello-world app.",
+                "# TYPE hello_world_app_info gauge",
+                f'hello_world_app_info{{user="{hello_world_user}"}} 1',
+            ]
+        )
+        + "\n"
+    )
+    return Response(metrics_output, mimetype="text/plain")

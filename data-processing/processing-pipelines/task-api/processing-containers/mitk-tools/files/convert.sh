cc16fac11cefb63c22b5eb271ef02bc2d2dea4a6
l597s <lorenz.feineis@dkfz-heidelberg.de>
2025-10-28 15:52:36 +0100

    Fix finding files to process

--- data-processing/processing-pipelines/task-api/processing-containers/mitk-tools/files/convert.sh
+++ data-processing/processing-pipelines/task-api/processing-containers/mitk-tools/files/convert.sh
@@ -1,7 +1,7 @@
 ROOT_INPUT_DICOM_DIR="/home/kaapana/dicom"
 ROOT_OUTPUT_NRRD_DIR="/home/kaapana/nrrd"
 
-for INPUT_DICOM_DIR in $( find ${ROOT_INPUT_DICOM_DIR} -mindepth 1 -maxdepth 1 -type d -name *.dcm ); do
+for INPUT_DICOM_DIR in $( find ${ROOT_INPUT_DICOM_DIR} -mindepth 1 -maxdepth 1 -type d); do
     IDENTIFIER=$( basename ${INPUT_DICOM_DIR} )
     mkdir -p ${ROOT_OUTPUT_NRRD_DIR}/${IDENTIFIER}
     /kaapana/app/apps/MitkFileConverter.sh -i ${INPUT_DICOM_DIR} -o ${ROOT_OUTPUT_NRRD_DIR}/${IDENTIFIER}/${IDENTIFIER}.nrrd

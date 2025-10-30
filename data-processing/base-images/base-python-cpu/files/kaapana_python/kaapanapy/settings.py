e5fecf12c9d29853bc4832948c02ed5e3c6932fa
l597s <lorenz.feineis@dkfz-heidelberg.de>
2025-10-30 08:30:40 +0100

    Add prefix KAAPANA_ to environment variable names in kaapanapy

--- data-processing/base-images/base-python-cpu/files/kaapana_python/kaapanapy/settings.py
+++ data-processing/base-images/base-python-cpu/files/kaapana_python/kaapanapy/settings.py
@@ -9,14 +9,30 @@ class KaapanaSettings(BaseSettings):
     These settings are imported in every module of the kaapana-pip library
     """
 
-    services_namespace: str = "services"
-    admin_namespace: str = "admin"
-    kaapana_log_level: str = "DEBUG"
-    timezone: str = Field("Europe/Berlin", validation_alias=AliasChoices("TZ"))
+    services_namespace: str = Field(
+        default="services",
+        validation_alias=AliasChoices(
+            "KAAPANA_SERVICES_NAMESPACE", "SERVICES_NAMESPACE"
+        ),
+    )
+    admin_namespace: str = Field(
+        default="admin",
+        validation_alias=AliasChoices("KAAPANA_ADMIN_NAMESPACE", "ADMIN_NAMESPACE"),
+    )
+    kaapana_log_level: str = Field(
+        default="DEBUG", validation_alias=AliasChoices("KAAPANA_LOG_LEVEL")
+    )
+    timezone: str = Field(
+        "Europe/Berlin",
+        validation_alias=AliasChoices("TZ", "KAAPANA_TIMEZONE", "TIMEZONE"),
+    )
 
 
 class KeycloakSettings(KaapanaSettings):
-    keycloak_url: str = "http://keycloak-external-service.admin.svc:80"
+    keycloak_url: str = Field(
+        default="http://keycloak-external-service.admin.svc:80",
+        validation_alias=AliasChoices("KAAPANA_KEYCLOAK_URL", "KEYCLOAK_URL"),
+    )
     client_secret: str = Field(
         validation_alias=AliasChoices("KAAPANA_CLIENT_SECRET", "OIDC_CLIENT_SECRET")
     )
@@ -28,11 +44,19 @@ class OpensearchSettings(KaapanaSettings):
     Settings for Opensearch module
     """
 
-    opensearch_host: str = "opensearch-service.services.svc"
-    opensearch_port: str = "9200"
+    opensearch_host: str = Field(
+        default="opensearch-service.services.svc",
+        validation_alias=AliasChoices("KAAPANA_OPENSEARCH_HOST", "OPENSEARCH_HOST"),
+    )
+    opensearch_port: str = Field(
+        default="9200",
+        validation_alias=AliasChoices("KAAPANA_OPENSEARCH_PORT", "OPENSEARCH_PORT"),
+    )
     default_index: str = Field(
         "project_admin",
-        validation_alias=AliasChoices("KAAPANA_DEFAULT_OPENSEARCH_INDEX"),
+        validation_alias=AliasChoices(
+            "KAAPANA_DEFAULT_OPENSEARCH_INDEX", "DEFAULT_INDEX"
+        ),
     )
 
 
@@ -68,13 +92,39 @@ class OperatorSettings(BaseSettings):
 
 class ServicesSettings(BaseSettings):
     # ADMIN
-    keycloak_url: Optional[str] = None
-    kube_helm_url: Optional[str] = None
+    keycloak_url: Optional[str] = Field(
+        default="http://keycloak-external-service.admin.svc:80",
+        validation_alias=AliasChoices("KAAPANA_KEYCLOAK_URL", "KEYCLOAK_URL"),
+    )
+    kube_helm_url: Optional[str] = Field(
+        default="http://kube-helm-service.admin.svc:5000",
+        validation_alias=AliasChoices("KAAPANA_KUBE_HELM_URL", "KUBE_HELM_URL"),
+    )
 
     # SERVICES
-    aii_url: str
-    dicom_web_filter_url: Optional[str] = None
-    opensearch_url: Optional[str] = None
-    kaapana_backend_url: Optional[str] = None
-    minio_url: Optional[str] = None
-    notification_url: Optional[str] = None
+    aii_url: str = Field(
+        default="http://aii-service.services.svc:8080",
+        validation_alias=AliasChoices("KAAPANA_AII_URL", "AII_URL"),
+    )
+    dicom_web_filter_url: Optional[str] = Field(
+        default="http://dicom-web-filter-service.services.svc:8080",
+        validation_alias=AliasChoices(
+            "KAAPANA_DICOM_WEB_FILTER_URL", "DICOM_WEB_FILTER_URL"
+        ),
+    )
+    opensearch_url: Optional[str] = Field(
+        default="http://opensearch-service.services.svc:9200",
+        validation_alias=AliasChoices("KAAPANA_OPENSEARCH_URL", "OPENSEARCH_URL"),
+    )
+    kaapana_backend_url: Optional[str] = Field(
+        default="http://kaapana-backend-service.services.svc:5000",
+        validation_alias=AliasChoices("KAAPANA_BACKEND_URL"),
+    )
+    minio_url: Optional[str] = Field(
+        default="http://minio-service.services.svc:9000",
+        validation_alias=AliasChoices("KAAPANA_MINIO_URL", "MINIO_URL"),
+    )
+    notification_url: Optional[str] = Field(
+        default="http://notification-service.services.svc:80",
+        validation_alias=AliasChoices("KAAPANA_NOTIFICATION_URL", "NOTIFICATION_URL"),
+    )

9a2bb4b88278ee4cbaa4b37a3ce5d50ad6d19d0c
Ünal Akünal <uenal.akuenal@dkfz-heidelberg.de>
2025-11-04 23:50:35 +0100

    dev-code-server: fix ingress path & add middleware

--- services/applications/dev-code-server/docker/files/entrypoint.sh
+++ services/applications/dev-code-server/docker/files/entrypoint.sh
@@ -5,6 +5,3 @@ set -e
 if ! ping -c1 -W1 pypi.org >/dev/null 2>&1; then
   echo "WARNING: Offline mode detected — pip and CRAN installs will not work."
 fi
-
-# Start code-server
-exec code-server --auth none --bind-addr 0.0.0.0:8080 /workspace

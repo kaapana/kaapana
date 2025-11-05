87c905873ad3eb34d53c1d6dbc5c89e0f6de338a
Ünal Akünal <uenal.akuenal@dkfz-heidelberg.de>
2025-11-05 19:27:26 +0100

    dev-code-server: add README.md

--- services/applications/dev-code-server/docker/files/entrypoint.sh
+++ services/applications/dev-code-server/docker/files/entrypoint.sh
@@ -3,5 +3,7 @@ set -e
 
 # Check if it's an offline environment
 if ! ping -c1 -W1 pypi.org >/dev/null 2>&1; then
-  echo "WARNING: Offline mode detected — pip and CRAN installs will not work."
+  echo "WARNING: Offline mode detected — functionalities might be limited." > /kaapana/OFFLINE_WARNING.txt
+else
+  echo "Online mode detected"
 fi

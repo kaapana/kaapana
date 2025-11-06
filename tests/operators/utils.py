951b7f5e45b791413297871603103a4de79e0102
l597s <lorenz.feineis@dkfz-heidelberg.de>
2025-11-06 14:52:25 +0100

    Moch kubernetes models to fix unittests

--- tests/operators/utils.py
+++ tests/operators/utils.py
@@ -32,7 +32,12 @@ def mock_modules():
     # Mock kubernetes and its submodules
     sys.modules["kubernetes"] = MagicMock()
     sys.modules["kubernetes.client"] = MagicMock()
+    sys.modules["kubernetes.client.exceptions"] = MagicMock()
+    sys.modules["kubernetes.client.api_client"] = MagicMock()
+    sys.modules["kubernetes.client.models"] = MagicMock()
+    sys.modules["kubernetes.client.rest"] = MagicMock()
     sys.modules["kubernetes.config"] = MagicMock()
+    sys.modules["kubernetes.stream"] = MagicMock()
 
     from kaapanapy.settings import KaapanaSettings
 
@@ -44,5 +49,6 @@ PLUGIN_DIR = (
     KAAPANA_DIR / "data-processing/kaapana-plugin/extension/docker/files/plugin/"
 )
 DICOM_TAG_DICT = (
-    KAAPANA_DIR / "services/flow/airflow/docker/files/scripts/dicom_tag_dict.json"
+    KAAPANA_DIR
+    / "services/flow/airflow/docker/bootstrap-airflow-image/files/scripts/dicom_tag_dict.json"
 )

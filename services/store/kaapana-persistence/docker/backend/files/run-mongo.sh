466aa83faa183645f7e0abf98d1649b2f619ac6d
Philipp Schader <philipp.schader@dkfz-heidelberg.de>
2025-12-17 16:03:51 +0100

    Deleted persistence layer

--- services/store/kaapana-persistence/docker/backend/files/run-mongo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run --rm -p 27017:27017 --name some-mongo mongo
-# docker run -d --name some-mongo \
-# 	-e MONGO_INITDB_ROOT_USERNAME=mongoadmin \
-# 	-e MONGO_INITDB_ROOT_PASSWORD=secret \
-# 	mongo
--- services/store/kaapana-persistence/docker/frontend/src/layouts/default/Default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-app>
-    <default-view />
-  </v-app>
-</template>
-
-<script lang="ts" setup>
-import DefaultView from "./View.vue";
-</script>

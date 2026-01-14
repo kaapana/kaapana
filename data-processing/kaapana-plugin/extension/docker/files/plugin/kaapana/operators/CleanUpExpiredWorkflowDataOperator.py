aa92d85887d6977da9a1b3c4084b01b3bf8ce233
Hanno Gao <h.gao@dkfz-heidelberg.de>
2026-01-14 09:55:21 +0100

    make cleanup a processing container and run for each ns

---
+++ data-processing/kaapana-plugin/extension/docker/files/plugin/kaapana/operators/CleanUpExpiredWorkflowDataOperator.py
@@ -0,0 +1,41 @@
+from datetime import timedelta
+from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator
+from kaapana.blueprints.kaapana_global_variables import (
+    DEFAULT_REGISTRY,
+    KAAPANA_BUILD_VERSION,
+)
+
+class CleanUpExpiredWorkflowDataOperator(KaapanaBaseOperator):
+    """
+    Operator to cleanup/remove the expired workflows data directories
+
+    **Inputs:**
+
+    * expired_period: Clean items that have expired since a certain period.
+
+    """
+    def __init__(self, dag, expired_period=timedelta(days=60), env_vars=None, **kwargs):
+        """
+        :param dag: DAG in which the operator has to be executed.
+        :param expired_period: Clean items that have expired since in day(s), default: 60 days
+        """
+
+
+
+        if env_vars is None:
+            env_vars = {}
+
+        envs = {
+            "EXPIRED_PERIOD": str(int(expired_period.total_seconds())),
+        }
+
+        env_vars.update(envs)
+
+        super().__init__(
+            dag=dag,
+            image=f"{DEFAULT_REGISTRY}/cleanup-expiered-workflows:{KAAPANA_BUILD_VERSION}",
+            name="clean-up",
+            image_pull_secrets=["registry-secret"],
+            env_vars=env_vars,
+            **kwargs,
+        )

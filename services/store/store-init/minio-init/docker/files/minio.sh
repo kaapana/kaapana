80eafe6194d5739072d5fc2d0ee356e204c357b2
l597s <lorenz.feineis@dkfz-heidelberg.de>
2026-01-27 14:21:59 +0100

    Migrate minio-init dockerfile to new minio cli version

--- services/store/store-init/minio-init/docker/files/minio.sh
+++ services/store/store-init/minio-init/docker/files/minio.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sleep 30s
-mc config host add kaapana http://minio-service.$SERVICES_NAMESPACE.svc:9000/ $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+mc alias set kaapana http://minio-service.$SERVICES_NAMESPACE.svc:9000/ $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
 mc mb --ignore-existing kaapana/uploads
 mc mb --ignore-existing kaapana/staticwebsiteresults
 mc mb --ignore-existing kaapana/template-analysis-scripts

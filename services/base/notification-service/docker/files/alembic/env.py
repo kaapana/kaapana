6f60f6239b56ff3ab270a9155a5c7fdb0635212c
l597s <lorenz.feineis@dkfz-heidelberg.de>
2026-02-17 13:34:00 +0100

    Adapt alembic to unique model.py file

--- services/base/notification-service/docker/files/alembic/env.py
+++ services/base/notification-service/docker/files/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from app.config import settings
-from app.notifications.models import Base
+from app.models import Base
 
 from alembic import context
 

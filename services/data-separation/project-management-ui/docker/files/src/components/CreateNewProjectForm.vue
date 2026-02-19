23bcdc6ceb4aeab205e65e9dc7daf48ab5cef156
l597s <lorenz.feineis@dkfz-heidelberg.de>
2026-02-19 09:21:46 +0100

    Limit project name to 13 characters

--- services/data-separation/project-management-ui/docker/files/src/components/CreateNewProjectForm.vue
+++ services/data-separation/project-management-ui/docker/files/src/components/CreateNewProjectForm.vue
@@ -10,7 +10,7 @@
                     <v-alert 
                         density="compact"
                         text="Project name must follow Minio, OpenSearch, and Kubernetes naming convention, and be under 16 
-                        characters. Use a short, lowercase, alphanumeric name (max 16 chars), with - as a separator. 
+                        characters. Use a short, lowercase, alphanumeric name (max 13 chars), with - as a separator. 
                         Avoid spaces and the word 'project' within the name."
                         type="info"
                         variant="tonal"
@@ -64,7 +64,7 @@ const fetching = ref(false)
 // Early validation rules for project naming from the UI
 const project_name_rules = ref<Array<(value: string) => boolean | string>>([
   (value) => !!value || 'Required.',
-  (value) => (value && value.length <= 16) || 'Max 16 characters',
+  (value) => (value && value.length <= 13) || 'Max 13 characters',
   (value) => (value == value.toLowerCase()) || 'Only lowercase alphanumeric characters are supported',
   (value) => (!value.includes(" ")) || 'Space are not allowed',
 ]);

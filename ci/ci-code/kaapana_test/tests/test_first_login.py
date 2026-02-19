0733987120d688bd13178263e93298ea2fd885e4
mikulas.bankovic <mikulas.bankovic27@gmail.com>
2026-02-19 10:26:59 +0100

    Change report paths

--- ci/ci-code/kaapana_test/tests/test_first_login.py
+++ ci/ci-code/kaapana_test/tests/test_first_login.py
@@ -19,9 +19,3 @@ async def test_first_login(driver, host):
     assert await driver.login(
         "kaapana", "admin"
     ), "Login after setting new password failed"
-
-@pytest.mark.asyncio
-async def test_invalid_login(driver, host):
-    await driver.goto(f"http://{host}")
-    ok = await driver.login("kaapana", "invalid")
-    assert not ok, "Login with invalid credentials should fail"

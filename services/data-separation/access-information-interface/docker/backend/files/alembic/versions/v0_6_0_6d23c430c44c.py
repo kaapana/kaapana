9fe692bc20ba195678aad4fb7e936a8ca427c451
Ünal Akünal <uenal.akuenal@dkfz-heidelberg.de>
2026-04-23 09:17:58 +0200

    aii: add new migration

--- services/data-separation/access-information-interface/docker/backend/files/alembic/versions/v0_6_0_6d23c430c44c.py
+++ services/data-separation/access-information-interface/docker/backend/files/alembic/versions/v0_6_0_6d23c430c44c.py
@@ -1,4 +1,4 @@
-"""v0-6-0 add is_archived to projects
+"""
 
 Revision ID: 6d23c430c44c
 Revises: 6e751c62ecfa
@@ -29,8 +29,33 @@ def upgrade() -> None:
             server_default=sa.false(),
         ),
     )
+    op.execute(
+        "ALTER TABLE projects "
+        "DROP CONSTRAINT IF EXISTS projects_kubernetes_namespace_key, "
+        "DROP CONSTRAINT IF EXISTS projects_s3_bucket_key, "
+        "DROP CONSTRAINT IF EXISTS projects_opensearch_index_key, "
+        "DROP COLUMN IF EXISTS kubernetes_namespace, "
+        "DROP COLUMN IF EXISTS s3_bucket, "
+        "DROP COLUMN IF EXISTS opensearch_index;"
+    )
 
 
 def downgrade() -> None:
     """Downgrade schema."""
+    op.add_column(
+        "projects", sa.Column("kubernetes_namespace", sa.String(), nullable=True)
+    )
+    op.add_column("projects", sa.Column("s3_bucket", sa.String(), nullable=True))
+    op.add_column(
+        "projects", sa.Column("opensearch_index", sa.String(), nullable=True)
+    )
+    op.create_unique_constraint(
+        "projects_kubernetes_namespace_key", "projects", ["kubernetes_namespace"]
+    )
+    op.create_unique_constraint(
+        "projects_s3_bucket_key", "projects", ["s3_bucket"]
+    )
+    op.create_unique_constraint(
+        "projects_opensearch_index_key", "projects", ["opensearch_index"]
+    )
     op.drop_column("projects", "is_archived")

21c9259b2e16dae18e394ab17fd726df3caa6043
mikulas.bankovic <mikulas.bankovic27@gmail.com>
2026-05-27 08:27:54 +0200

    Remove latest as default param from cache_from_tag

--- build-scripts/build_helper/build/build_config.py
+++ build-scripts/build_helper/build/build_config.py
@@ -40,7 +40,7 @@ class BuildConfig(BaseModel):
         True  # Docker BuildKit: https://docs.docker.com/develop/develop-images/build_enhancements/
     )
     enable_inline_cache: bool = True
-    cache_from_tag: Optional[str] = "latest"
+    cache_from_tag: Optional[str] = None
     parallel_processes: int
     max_build_rounds: int = 5
     max_push_retries: int = 30

45645cdf8c716ed5450c6fb746163bcff84d6785
l597s <lorenz.feineis@dkfz-heidelberg.de>
2026-05-28 16:00:47 +0200

    Create a kaapanapy shim that points to kaapana_client

---
+++ data-processing/base-images/base-python-cpu/files/kaapanapy-shim/kaapanapy/__init__.py
@@ -0,0 +1,35 @@
+# kaapanapy/__init__.py
+import importlib
+import importlib.abc
+import importlib.machinery
+import sys
+
+import kaapana_client
+
+sys.modules["kaapanapy"] = kaapana_client
+
+
+class _AliasLoader(importlib.abc.Loader):
+    def __init__(self, real_mod):
+        self._real_mod = real_mod
+
+    def create_module(self, spec):
+        return self._real_mod
+
+    def exec_module(self, module):
+        pass
+
+
+class _KaapanaPyFinder(importlib.abc.MetaPathFinder):
+    def find_spec(self, fullname, path, target=None):
+        if not fullname.startswith("kaapanapy."):
+            return None
+        real_name = fullname.replace("kaapanapy.", "kaapana_client.", 1)
+        try:
+            real_mod = importlib.import_module(real_name)
+        except ImportError:
+            return None
+        return importlib.machinery.ModuleSpec(fullname, _AliasLoader(real_mod))
+
+
+sys.meta_path.append(_KaapanaPyFinder())

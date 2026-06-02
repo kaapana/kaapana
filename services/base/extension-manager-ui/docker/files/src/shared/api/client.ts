ac4dfebea3bccd81a125a330985991f9cf85c431
Theo Stempel-Hauburger <theo.stempel-hauburger@dkfz-heidelberg.de>
2026-06-02 13:02:02 +0200

    use specific axios types

--- services/base/extension-manager-ui/docker/files/src/shared/api/client.ts
+++ services/base/extension-manager-ui/docker/files/src/shared/api/client.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import type { AxiosError, AxiosResponse } from 'axios'
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_EXTENSION_MANAGER_API_URL ?? '/extensions-api',
@@ -9,8 +10,8 @@ const apiClient = axios.create({
 })
 
 apiClient.interceptors.response.use(
-  (response: any) => response,
-  (error: any) => {
+  (response: AxiosResponse) => response,
+  (error: AxiosError) => {
     console.error('API Error:', error)
     return Promise.reject(error)
   },

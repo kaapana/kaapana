d900041bbb272faa68d3a638cf837ff96b93d829
Hanno Gao <h.gao@dkfz-heidelberg.de>
2026-06-19 17:30:53 +0200

    Bugfix/2237 undeployment is slow

--- templates_and_examples/examples/services/hello-backend/docker/files/boot.sh
+++ templates_and_examples/examples/services/hello-backend/docker/files/boot.sh
@@ -4,4 +4,4 @@ flask db init
 flask db migrate
 flask db upgrade
 
-SCRIPT_NAME=$APPLICATION_ROOT gunicorn -b :5000 --access-logfile - --error-logfile - run:app
+SCRIPT_NAME=$APPLICATION_ROOT exec gunicorn -b :5000 --access-logfile - --error-logfile - run:app

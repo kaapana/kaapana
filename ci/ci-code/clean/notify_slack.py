963223a9aa8bb59d3d415166238ce75c5dbbb218
Philipp Schader <philipp.schader@dkfz-heidelberg.de>
2026-07-17 14:04:23 +0200

    ci: enable TLS verification toward GitLab in ticket scripts

--- ci/ci-code/clean/notify_slack.py
+++ ci/ci-code/clean/notify_slack.py
@@ -29,7 +29,7 @@ def main():
     gl = gitlab.Gitlab(
         url="https://codebase.helmholtz.cloud",
         private_token=gitlab_api_token,
-        ssl_verify=False,
+        ssl_verify=True,
     )
 
     # Logic

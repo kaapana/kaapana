b50ef09eb750176946f3de3ab4d9e903d9a43043
l597s <lorenz.feineis@dkfz-heidelberg.de>
2026-08-03 09:30:56 +0200

    Separate bucket creation and policy creation; Using mc CLI only for policy creation

--- services/store/store-init/minio-init/docker/files/minio.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sleep 30s
-mc alias set kaapana http://minio-service.$SERVICES_NAMESPACE.svc:9000/ $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
-mc mb --ignore-existing kaapana/uploads
-mc mb --ignore-existing kaapana/staticwebsiteresults
-mc mb --ignore-existing kaapana/template-analysis-scripts
-
-mc admin policy create kaapana kaapanaUser /kaapana/app/user-policy.json

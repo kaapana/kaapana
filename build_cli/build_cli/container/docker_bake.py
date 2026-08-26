96c390c3400b076488ad06fe33520d8454a8d9b5
l597s <lorenz.feineis@dkfz-heidelberg.de>
2026-08-26 16:12:19 +0200

    Correctly set CACHE_ variables for the docker buildx bake context

--- build_cli/build_cli/container/docker_bake.py
+++ build_cli/build_cli/container/docker_bake.py
@@ -23,14 +23,17 @@ def run_bake(containers: set[Container], build_config: BuildConfig):
         BUILDX_BUILDER_NAME,
     ]
     command.extend([container.image_name for container in containers])
-    process = subprocess.run(["which", "docker"])
-    process = subprocess.run(["docker", "buildx", "ls"])
     process = subprocess.run(
         command,
         env={
             **os.environ,
             "REGISTRY": build_config.default_registry,
             "TAG": version,
+            "CACHE_TO": str(build_config.cache_to),
+            "CACHE_FROM": str(build_config.cache_from),
+            "CACHE_REGISTRY": build_config.cache_registry
+            or build_config.default_registry,
+            "BUILD_ONLY": str(build_config.build_only),
         },
     )
     process.check_returncode()

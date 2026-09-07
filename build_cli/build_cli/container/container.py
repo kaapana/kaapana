f590eb57ced6544bda9276fa57368d7bdff13d18
l597s <lorenz.feineis@dkfz-heidelberg.de>
2026-09-07 14:05:15 +0200

    Add named build context lib to all images

--- build_cli/build_cli/container/container.py
+++ build_cli/build_cli/container/container.py
@@ -134,7 +134,6 @@ class Container:
         missing_base_images: List[BaseImage | Container] | None = None,
         build_ignore: bool = False,
         local_image: bool = False,
-        build_lib_context: bool = False,
     ):
         self.dockerfile = dockerfile
         self.registry = registry
@@ -150,7 +149,6 @@ class Container:
         )
         self.build_ignore = build_ignore
         self.local_image = local_image
-        self.build_lib_context = build_lib_context
         self.status = Status.NOT_BUILT
         self.build_time: str | float = "-"
         self.push_time: str | float = "-"
@@ -201,7 +199,6 @@ class Container:
         repo_version = ""
         build_ignore = False
         local_image = False
-        build_lib_context = False
         base_images: set[BaseImage | Container] = set()
         args: dict[str, str] = {}
 
@@ -220,9 +217,6 @@ class Container:
             elif line.startswith("LABEL BUILD_IGNORE="):
                 val = cls._extract_label_value(line).lower()
                 build_ignore = val in {"true", "yes", "1"}
-            elif line.startswith("LABEL BUILD_LIB_CONTEXT="):
-                val = cls._extract_label_value(line).lower()
-                build_lib_context = val in {"true", "yes", "1"}
             elif line.startswith("FROM") and "#ignore" not in line:
                 base_tag = line.split("FROM", 1)[1].split()[0].strip().replace('"', "")
                 base_tag = re.sub(
@@ -278,7 +272,6 @@ class Container:
             base_images=base_images,
             build_ignore=build_ignore,
             local_image=local_image,
-            build_lib_context=build_lib_context,
         )
         return container
 
@@ -315,12 +308,14 @@ class Container:
             )
         if config.include_model_weights:
             build_args.extend(["--build-arg", "include_model_weights=true"])
-        if self.build_lib_context:
-            lib_path = config.kaapana_dir / "lib"
-            build_args.extend(["--build-context", f"lib={lib_path}"])
 
         build_args.extend(
-            ["--build-context", f"constraints={config.kaapana_dir / "constraints"}"]
+            [
+                "--build-context",
+                f"constraints={config.kaapana_dir / "constraints"}",
+                "--build-context",
+                f"lib={config.kaapana_dir / "lib"}",
+            ]
         )
         if self.image_name == "kaapana-extension-collection":
             build_args.extend(
